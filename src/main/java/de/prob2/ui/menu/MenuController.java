e33f1aaed0d6049541feb62695b51044c847aeba
dgelessus <dgelessus@users.noreply.github.com>
2016-07-05 15:35:25 +0200

    Add support for the Mac menu bar

--- src/main/java/de/prob2/ui/menu/MenuController.java
+++ src/main/java/de/prob2/ui/menu/MenuController.java
@@ -8,6 +8,7 @@ import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+import de.codecentric.centerdevice.MenuToolkit;
 import de.prob.scripting.Api;
 import de.prob2.ui.events.OpenFileEvent;
 import de.prob2.ui.modelchecking.ModelcheckingView;
@@ -15,7 +16,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -73,6 +77,40 @@ public class MenuController extends MenuBar {
 			e.printStackTrace();
 		}
 		bus.register(this);
+		
+		if (System.getProperty("os.name", "").toLowerCase().contains("mac")) {
+			// Mac-specific menu stuff
+			this.setUseSystemMenuBar(true);
+			MenuToolkit tk = MenuToolkit.toolkit();
+			
+			// Create Mac-style application menu
+			Menu applicationMenu = tk.createDefaultApplicationMenu("ProB 2");
+			this.getMenus().add(0, applicationMenu);
+			tk.setApplicationMenu(applicationMenu);
+			
+			// Move About menu item from Help to application menu
+			Menu helpMenu = this.getMenus().get(this.getMenus().size()-1);
+			MenuItem aboutItem = helpMenu.getItems().get(helpMenu.getItems().size()-1);
+			aboutItem.setText("About ProB 2");
+			helpMenu.getItems().remove(aboutItem);
+			applicationMenu.getItems().set(0, aboutItem);
+			
+			// Create Mac-style Window menu
+			Menu windowMenu = new Menu("Window");
+			windowMenu.getItems().addAll(
+				tk.createMinimizeMenuItem(),
+				tk.createZoomMenuItem(),
+				tk.createCycleWindowsItem(),
+				new SeparatorMenuItem(),
+				tk.createBringAllToFrontItem(),
+				new SeparatorMenuItem()
+			);
+			tk.autoAddWindowMenuItems(windowMenu);
+			this.getMenus().add(this.getMenus().size()-1, windowMenu);
+			
+			// Make this the global menu bar
+			tk.setGlobalMenuBar(this);
+		}
 	}
 
 }
