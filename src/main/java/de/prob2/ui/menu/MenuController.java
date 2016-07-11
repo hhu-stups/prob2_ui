83d38971dfcf8fe08151ab677597bbaca8985639
yutak001 <Yumiko.Takahashi@hhu.de>
2016-07-11 13:22:16 +0200

    Load Default/Perspectives, not yet working properly

--- src/main/java/de/prob2/ui/menu/MenuController.java
+++ src/main/java/de/prob2/ui/menu/MenuController.java
@@ -2,6 +2,7 @@ package de.prob2.ui.menu;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
@@ -9,11 +10,13 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob.scripting.Api;
+import de.prob2.ui.ProB2;
 import de.prob2.ui.events.OpenFileEvent;
 import de.prob2.ui.modelchecking.ModelcheckingView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.stage.FileChooser;
@@ -29,7 +32,18 @@ public class MenuController extends MenuBar {
 	
 	@FXML
 	private void handleLoadDefault(){
-		
+		Window stage = this.getScene().getWindow();
+		try {
+			FXMLLoader loader = ProB2.injector.getInstance(FXMLLoader.class);
+			loader.setLocation(getClass().getResource("../main.fxml"));
+			loader.load();
+			Parent root = loader.getRoot();
+			Scene scene = new Scene(root);
+			((Stage) stage).setScene(scene);
+		} catch (IOException e) {
+			System.err.println("Failed to load FXML-File!");
+			e.printStackTrace();
+		}
 	}
 	
 	@FXML
@@ -39,6 +53,17 @@ public class MenuController extends MenuBar {
 		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("FXML Files", "*.fxml"));
 		Window stage = this.getScene().getWindow();
 		File selectedFile = fileChooser.showOpenDialog(stage);
+		try {
+			FXMLLoader loader = ProB2.injector.getInstance(FXMLLoader.class);
+			loader.setLocation(new URL("file://" + selectedFile.getPath()));
+			loader.load();
+			Parent root = loader.getRoot();
+			Scene scene = new Scene(root);
+			((Stage) stage).setScene(scene);
+		} catch (IOException e) {
+			System.err.println("Failed to load FXML-File!");
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
