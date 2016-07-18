84ea66681688aaa9f8b2a560b2ad686d388ee6b2
dgelessus <dgelessus@users.noreply.github.com>
2016-07-18 16:43:53 +0200

    Remove unused AnimationPerspective variables

--- src/main/java/de/prob2/ui/AnimationPerspective.java
+++ src/main/java/de/prob2/ui/AnimationPerspective.java
@@ -1,22 +1,14 @@
 package de.prob2.ui;
 
 import com.google.inject.Inject;
-import de.prob.scripting.Api;
 import de.prob2.ui.dotty.DottyView;
 import de.prob2.ui.history.HistoryView;
 import de.prob2.ui.modelchecking.ModelcheckingController;
 import de.prob2.ui.operations.OperationsView;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Accordion;
 
 public class AnimationPerspective {
-
-	@SuppressWarnings("unused")
-	private final Api api;
-
-	@SuppressWarnings("unused")
-	private FXMLLoader loader;
 
 	@FXML
 	private Accordion small;
@@ -28,15 +20,11 @@ public class AnimationPerspective {
 
 	@Inject
 	public AnimationPerspective(
-		Api api,
-		FXMLLoader loader,
 		OperationsView opsController,
 		HistoryView historyController,
 		DottyView dottyController,
 		ModelcheckingController modelCheckStatsController
 	) {
-		this.api = api;
-		this.loader = loader;
 		this.opsController = opsController;
 		this.historyController = historyController;
 		this.dottyController = dottyController;
