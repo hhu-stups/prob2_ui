05142858b81cc7b93a7e994374a084c231005c74
Antonia <antonia.puetz@hhu.de>
2016-07-21 12:13:57 +0200

    some further small layout improvements

--- src/main/java/de/prob2/ui/modelchecking/ModelCheckStats.java
+++ src/main/java/de/prob2/ui/modelchecking/ModelCheckStats.java
@@ -208,28 +208,4 @@ public class ModelCheckStats extends AnchorPane implements IModelCheckListener {
 			});
 		}
 	}
-
-	@Override
-	public String toString() {
-		// ModelChecker modelChecker = jobs.get(id).getChecker();
-		// ModelCheckingOptions options = jobs.get(id).getOptions();
-		// AbstractElement main =
-		// modelChecker.getStateSpace().getMainComponent();
-		// List<String> optsList = new ArrayList<String>();
-		// for (Options opts : options.getPrologOptions()) {
-		// optsList.add(opts.getDescription());
-		// if (opts.getDescription().equals("recheck existing states")) {
-		// searchForNewErrors = false;
-		// }
-		// }
-		// // Platform.runLater(() -> {
-		// // String name = main == null ? "Model Check" : main.toString();
-		// // if (!optsList.isEmpty()) {
-		// // name += " with " + Joiner.on(", ").join(optsList);
-		// // }
-		// // titelText.setText(name);
-		// // titelImage.setImage(selectImage(res));
-		// // });
-		return super.toString();
-	}
 }
