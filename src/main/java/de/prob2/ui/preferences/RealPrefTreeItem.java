dcf22c4f8d6b82ada0e89ade904d5825c30abe77
dgelessus <dgelessus@users.noreply.github.com>
2016-07-28 14:55:39 +0200

    Format PreferencesStage values differently depending on type At the moment the type is guessed from the current value, this can be changed later.

--- src/main/java/de/prob2/ui/preferences/RealPrefTreeItem.java
+++ src/main/java/de/prob2/ui/preferences/RealPrefTreeItem.java
@@ -1,12 +1,19 @@
 package de.prob2.ui.preferences;
 
 public class RealPrefTreeItem extends PrefTreeItem {
-	public RealPrefTreeItem(String name, String changed, String value, String defaultValue, String description) {
-		super(name, changed, value, defaultValue, description);
+	public RealPrefTreeItem(
+		final String name,
+		final String changed,
+		final String value,
+		final Class<?> valueType,
+		final String defaultValue,
+		final String description
+	) {
+		super(name, changed, value, valueType, defaultValue, description);
 	}
 
 	public RealPrefTreeItem(String name) {
-		this(name, "", "", "", "");
+		this(name, "", "", String.class, "", "");
 	}
 
 	public RealPrefTreeItem() {
@@ -15,6 +22,7 @@ public class RealPrefTreeItem extends PrefTreeItem {
 
 	@Override
 	public void updateValue(final Preferences prefs) {
+		this.valueType.set(prefs.guessType(this.getName()));
 		this.value.set(prefs.getPreferenceValue(this.getName()));
 		this.changed.set(this.value.get().equals(this.defaultValue.get()) ? "" : "*");
 	}
