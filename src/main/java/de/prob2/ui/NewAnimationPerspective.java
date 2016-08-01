cfefdb54ada01b85e2a3bd0c24d8dcfb3a9ba9b1
yutak001 <Yumiko.Takahashi@hhu.de>
2016-08-01 16:16:57 +0200

    refactoring

--- src/main/java/de/prob2/ui/NewAnimationPerspective.java
+++ src/main/java/de/prob2/ui/NewAnimationPerspective.java
@@ -9,6 +9,7 @@ import de.prob2.ui.operations.OperationsView;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.SplitPane;
@@ -36,10 +37,10 @@ public class NewAnimationPerspective extends SplitPane{
     private ModelcheckingController modelcheck;
     @FXML
     private TitledPane modelcheckTP;
-    /*@FXML
+    @FXML
     private FlowPane test;
     @FXML
-    private TitledPane testTP;*/
+    private TitledPane testTP;
     @FXML
     private Accordion accordion;
 
@@ -51,9 +52,9 @@ public class NewAnimationPerspective extends SplitPane{
             loader.setRoot(this);
             loader.setController(this);
             loader.load();
-            /*parentProperty().addListener((ObservableValue<? extends Parent> ov, Parent previousParent, Parent nextParent)-> {
+            parentProperty().addListener((ObservableValue<? extends Parent> ov, Parent previousParent, Parent nextParent)-> {
                 onDrag();
-            });*/
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -65,66 +66,25 @@ public class NewAnimationPerspective extends SplitPane{
     }
 
     private void onDrag() {
-        operations.setOnDragDetected((MouseEvent t) -> {
-            if (!this.getChildren().contains(operations)){
-                this.getItems().add(operations);
-                accordion.getPanes().remove(operationsTP);
+        this.dragAction(operations,operationsTP);
+        this.dragAction(history,historyTP);
+        this.dragAction(dotty,dottyTP);
+        this.dragAction(modelcheck,modelcheckTP);
+    }
+
+    private void dragAction(Node node, TitledPane nodeTP){
+        node.setOnDragDetected((MouseEvent t) -> {
+            if (!this.getChildren().contains(node)){
+                this.getItems().add(node);
+                accordion.getPanes().remove(nodeTP);
                 this.removeAccordion();
             } else {
                 this.addAccordion();
-                operationsTP.setContent(operations);
-                accordion.getPanes().add(0,operationsTP);
+                nodeTP.setContent(node);
+                accordion.getPanes().add(nodeTP);
             }
             t.consume();
         });
-        history.setOnDragDetected((MouseEvent t) -> {
-            if (!this.getChildren().contains(history)){
-                this.getItems().add(history);
-                accordion.getPanes().remove(historyTP);
-                this.removeAccordion();
-            } else {
-                this.addAccordion();
-                historyTP.setContent(history);
-                accordion.getPanes().add(1,historyTP);
-            }
-            t.consume();
-        });
-        dotty.setOnDragDetected((MouseEvent t) -> {
-            if (!this.getChildren().contains(dotty)){
-                this.getItems().add(dotty);
-                accordion.getPanes().remove(dottyTP);
-                this.removeAccordion();
-            } else {
-                this.addAccordion();
-                dottyTP.setContent(dotty);
-                accordion.getPanes().add(2,dottyTP);
-            }
-            t.consume();
-        });
-        modelcheck.setOnDragDetected((MouseEvent t) -> {
-            if (!this.getChildren().contains(modelcheck)){
-                this.getItems().add(modelcheck);
-                accordion.getPanes().remove(modelcheckTP);
-                this.removeAccordion();
-            } else {
-                this.addAccordion();
-                modelcheckTP.setContent(modelcheck);
-                accordion.getPanes().add(3,modelcheckTP);
-            }
-            t.consume();
-        });
-        /*test.setOnDragDetected((MouseEvent t) -> {
-            if (!this.getChildren().contains(test)){
-                this.getItems().add(test);
-                accordion.getPanes().remove(testTP);
-                this.removeAccordion();
-            } else {
-                this.addAccordion();
-                testTP.setContent(test);
-                accordion.getPanes().add(4,testTP);
-            }
-            t.consume();
-        });*/
     }
 
     private void removeAccordion(){
