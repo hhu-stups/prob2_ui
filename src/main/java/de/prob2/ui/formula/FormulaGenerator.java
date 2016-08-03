e7c3ed708a58ec121dbb264d58d8bcfdea93d066
favu100 <Fabian.Vu@hhu.de>
2016-08-03 11:41:18 +0200

    Add Opportunity to move inside FormulaView

--- src/main/java/de/prob2/ui/formula/FormulaGenerator.java
+++ src/main/java/de/prob2/ui/formula/FormulaGenerator.java
@@ -11,12 +11,12 @@ import de.prob.animator.command.InsertFormulaForVisualizationCommand;
 import de.prob.animator.domainobjects.ExpandedFormula;
 import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.statespace.AnimationSelector;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseButton;
-import javafx.scene.layout.StackPane;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
@@ -28,6 +28,12 @@ public class FormulaGenerator {
 	private final AnimationSelector animationSelector;
 	
 	private double zoomFactor = 1;
+	
+	private double oldMousePositionX = -1;
+	private double oldMousePositionY = -1;
+	
+	private Group group;
+	private ScrollPane root;
 	
 	
 	@Inject
@@ -46,53 +52,39 @@ public class FormulaGenerator {
 			data.collapseNodes(new HashSet<>(collapsedNodes));
 			
 			graph = new FormulaGraph(new FormulaNode(data));
-			//graph.autosize();
+			setEventListeners();
 			zoomFactor = 1;		
-			
 			draw();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void collapseNode(final String id) {
-		collapsedNodes.add(id);
-	}
-
-	public void expandNode(final String id) {
-		collapsedNodes.remove(id);
-	}
-	
-	private void draw() {
-		Group group = new Group();
-		ScrollPane root = new ScrollPane(group);
-		group.getChildren().add(graph);
-		
-		Stage stage = new Stage();
-		stage.setTitle("Mathematical Expression");
-		Scene scene = new Scene(root, 1024, 768);
-		
-		/*graph.setOnMousePressed(e-> {
-			graph.setCursor(Cursor.);
-			
-		});
+	private void setEventListeners() {
 		graph.setOnMouseMoved(e-> {
-			graph.setCursor(Cursor.OPEN_HAND);
-			
-		});*/
+			graph.setCursor(Cursor.HAND);
+			oldMousePositionX = e.getSceneX();
+			oldMousePositionY = e.getSceneY();
+		});
 		
-	
+		graph.setOnMouseDragged(e-> {
+			graph.setCursor(Cursor.MOVE);
+			root.setHvalue(root.getHvalue() + (-e.getSceneX() + oldMousePositionX)/graph.getWidth());
+			root.setVvalue(root.getVvalue() + (-e.getSceneY() + oldMousePositionY)/graph.getHeight());
+			oldMousePositionX = e.getSceneX();
+			oldMousePositionY = e.getSceneY();
 
+		});
 		graph.setOnMouseClicked(e -> {
+			if(e.getClickCount() < 2) {
+				return;
+			}
 			double Xpos;
 			double Ypos;
 			double oldWidth = graph.getWidth();
 			double oldHeight = graph.getHeight();
 			ScrollBar vBar = (ScrollBar) root.getChildrenUnmodifiable().get(1);
 			ScrollBar hBar = (ScrollBar) root.getChildrenUnmodifiable().get(2);
-
-
-
 
 			if(e.getButton() == MouseButton.PRIMARY) {
 				graph.setPrefHeight(graph.getHeight() * 1.3);
@@ -122,13 +114,26 @@ public class FormulaGenerator {
 			if(e.getY()/oldHeight > 0.9) {
 				root.setVvalue(2);
 			}*/
-			
-			
-
-			
-
 			System.out.println("Real value: " + root.getHvalue() + " " + root.getVvalue());
 		});
+	}
+	
+	public void collapseNode(final String id) {
+		collapsedNodes.add(id);
+	}
+
+	public void expandNode(final String id) {
+		collapsedNodes.remove(id);
+	}
+	
+	private void draw() {
+		group = new Group();
+		root = new ScrollPane(group);
+		group.getChildren().add(graph);
+		
+		Stage stage = new Stage();
+		stage.setTitle("Mathematical Expression");
+		Scene scene = new Scene(root, 1024, 768);
 		
 		stage.setScene(scene);
 		stage.show();
