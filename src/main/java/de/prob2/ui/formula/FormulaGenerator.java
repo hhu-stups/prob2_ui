54b9419e9231c956d30bd298238f441042f25e3a
favu100 <Fabian.Vu@hhu.de>
2016-08-04 10:54:32 +0200

    Improve Zooming in FormulaView

--- src/main/java/de/prob2/ui/formula/FormulaGenerator.java
+++ src/main/java/de/prob2/ui/formula/FormulaGenerator.java
@@ -14,21 +14,17 @@ import de.prob.statespace.AnimationSelector;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseButton;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 public class FormulaGenerator {
-	private IEvalElement formula;
 	private FormulaGraph graph;
 	private final Set<String> collapsedNodes = new CopyOnWriteArraySet<>();
 	
 	private final AnimationSelector animationSelector;
-	
-	private double zoomFactor = 1;
-	
+		
 	private double oldMousePositionX = -1;
 	private double oldMousePositionY = -1;
 	
@@ -54,7 +50,6 @@ public class FormulaGenerator {
 			
 			graph = new FormulaGraph(new FormulaNode(data));
 			setEventListeners();
-			zoomFactor = 1;		
 			draw();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -80,42 +75,20 @@ public class FormulaGenerator {
 			if(e.getClickCount() < 2) {
 				return;
 			}
-			double Xpos;
-			double Ypos;
-			double oldWidth = graph.getWidth();
-			double oldHeight = graph.getHeight();
-			ScrollBar vBar = (ScrollBar) root.getChildrenUnmodifiable().get(1);
-			ScrollBar hBar = (ScrollBar) root.getChildrenUnmodifiable().get(2);
 
 			if(e.getButton() == MouseButton.PRIMARY) {
-				graph.setPrefHeight(graph.getHeight() * 1.3);
-				graph.setPrefWidth(graph.getWidth() * 1.3);
-				zoomFactor *= 1.3;
-				group.getTransforms().add(new Scale(1.3, 1.3));			
+				graph.getTransforms().add(new Scale(1.3, 1.3));			
 			} else if(e.getButton() == MouseButton.SECONDARY) {
-				graph.setPrefHeight(graph.getHeight() * 0.8);
-				graph.setPrefWidth(graph.getWidth() * 0.8);
-				zoomFactor *= 0.8;
-				group.getTransforms().add(new Scale(0.8, 0.8));
+				graph.getTransforms().add(new Scale(0.8, 0.8));
 				
 			}
+			
 			group.getChildren().clear();
 			group.getChildren().add(graph);
-
-			Xpos = e.getX()*zoomFactor/graph.getWidth();
-			Ypos = e.getY()*zoomFactor/graph.getHeight();
-
-			
+			double Xpos = e.getX()/graph.getWidth();
+			double Ypos = e.getY()/graph.getHeight();
 			root.setHvalue(Xpos);
 			root.setVvalue(Ypos);
-			
-			/*if(e.getX()/oldWidth > 0.9) {
-				root.setHvalue(2);
-			}
-			if(e.getY()/oldHeight > 0.9) {
-				root.setVvalue(2);
-			}*/
-			System.out.println("Real value: " + root.getHvalue() + " " + root.getVvalue());
 		});
 	}
 	
