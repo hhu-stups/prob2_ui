0364d5f7e133a4fb96c40c10f654813f31039e84
Antonia <antonia.puetz@hhu.de>
2016-08-12 13:05:49 +0200

    added AnimationsPane to GUI

--- src/main/java/de/prob2/ui/animations/AnimationsView.java
+++ src/main/java/de/prob2/ui/animations/AnimationsView.java
@@ -1,5 +1,27 @@
 package de.prob2.ui.animations;
 
-public class AnimationsView {
+import java.io.IOException;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+@Singleton
+public class AnimationsView extends AnchorPane {
+	
+	@Inject
+	private AnimationsView(final FXMLLoader loader) {
+		try {
+			loader.setLocation(getClass().getResource("animations_view.fxml"));
+			loader.setRoot(this);
+			loader.setController(this);
+			loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
