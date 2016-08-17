8a4f7e2b65970cf5349c5f6d51ceab600e1640e9
Antonia <antonia.puetz@hhu.de>
2016-08-17 12:30:15 +0200

    all current animations are shown in the animation view and it's possible to show the trace of one animation by selecting this

--- src/main/java/de/prob2/ui/animations/Animation.java
+++ src/main/java/de/prob2/ui/animations/Animation.java
@@ -1,33 +1,46 @@
 package de.prob2.ui.animations;
 
+import de.prob.statespace.Trace;
+
 public class Animation {
 	private String modelName;
 	private String lastOperation;
 	private String steps;
-	
-	public Animation(String modelName, String lastOperation, String steps) {
+	private Trace trace;
+	private boolean isCurrent;
+	private boolean isProtected;
+
+	public Animation(String modelName, String lastOperation, String steps, Trace trace, boolean isCurrent,
+			boolean isProtected) {
 		this.modelName = modelName;
 		this.lastOperation = lastOperation;
 		this.steps = steps;
+		this.trace = trace;
+		this.isCurrent = isCurrent;
+		this.isProtected = isProtected;
 	}
 
 	public String getModelName() {
 		return modelName;
 	}
-	
+
 	public String getLastOperation() {
 		return lastOperation;
 	}
-	
+
 	public String getSteps() {
 		return steps;
 	}
-	
-	void setLastOperation(String lastOperation) {
-		this.lastOperation = lastOperation;
+
+	public Trace getTrace() {
+		return trace;
+	}
+
+	public boolean isCurrent() {
+		return isCurrent;
 	}
 	
-	void setSteps(String steps) {
-		this.steps = steps;
+	public boolean isProtected() {
+		return isProtected;
 	}
 }
