afa8143d8ef48152e164bf5213c3e9be781f99f5
dgelessus <dgelessus@users.noreply.github.com>
2016-08-30 13:03:51 +0200

    Evaluate transition of HistoryItem before displaying, fixes PROB2UI-2

--- src/main/java/de/prob2/ui/history/HistoryItem.java
+++ src/main/java/de/prob2/ui/history/HistoryItem.java
@@ -6,20 +6,13 @@ public class HistoryItem {
 
 	public final HistoryStatus status;
 	public final Transition transition;
-	public final boolean root;
 	
 	public HistoryItem(HistoryStatus status) {
-		this.status = status;
-		this.transition = null;
-		this.root = true;
+		this(null, status);
 	}
 
 	public HistoryItem(Transition transition, HistoryStatus status) {
 		this.transition = transition;
 		this.status = status;
-		this.root = false;
 	}
-
-
-	
 }
