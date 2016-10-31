ea40d8eda165959938aa52db53f3a46db278a74b
favu100 <Fabian.Vu@hhu.de>
2016-10-31 14:15:21 +0100

    Refactor both console stages

--- src/main/java/de/prob2/ui/consoles/b/BConsoleStage.java
+++ src/main/java/de/prob2/ui/consoles/b/BConsoleStage.java
@@ -1,35 +1,23 @@
 package de.prob2.ui.consoles.b;
 
-import java.io.IOException;
-
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob2.ui.prob2fx.CurrentStage;
 
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Singleton
 public final class BConsoleStage extends Stage {
-	private static final Logger logger = LoggerFactory.getLogger(BConsoleStage.class);
 
 	@Inject
-	private BConsoleStage(FXMLLoader loader, CurrentStage currentStage, BConsole bConsole) {
-		try {
-			loader.setLocation(getClass().getResource("b_console_stage.fxml"));
-			loader.setRoot(this);
-			loader.setController(this);
-			loader.load();
-		} catch (IOException e) {
-			logger.error("loading fxml failed", e);
-		}
+	private BConsoleStage(CurrentStage currentStage, BConsole bConsole) {
+		this.setTitle("B Console");
+		this.setScene(new Scene(new StackPane(bConsole), 800, 600));
+		this.getScene().getStylesheets().add(getClass().getResource("../../../../../prob.css").toString());
 		currentStage.register(this);
-		this.getScene().setRoot(new StackPane(bConsole));
 	}
 
 }
--- src/main/java/de/prob2/ui/consoles/groovy/GroovyConsoleStage.java
+++ src/main/java/de/prob2/ui/consoles/groovy/GroovyConsoleStage.java
@@ -1,42 +1,24 @@
 package de.prob2.ui.consoles.groovy;
 
-import java.io.IOException;
-
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob2.ui.prob2fx.CurrentStage;
-
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Singleton
 public final class GroovyConsoleStage extends Stage {
-	private static final Logger logger = LoggerFactory.getLogger(GroovyConsoleStage.class);
-
-	private GroovyConsole groovyConsole;
 		
 	@Inject
-	private GroovyConsoleStage(FXMLLoader loader, CurrentStage currentStage, GroovyConsole groovyConsole) {
-		try {
-			loader.setLocation(getClass().getResource("groovy_console_stage.fxml"));
-			loader.setRoot(this);
-			loader.setController(this);
-			loader.load();
-		} catch (IOException e) {
-			logger.error("loading fxml failed", e);
-		}
-		currentStage.register(this);
-		this.getScene().setRoot(new StackPane(groovyConsole));
-	}
-	
-	@FXML
-	public void initialize() {
+	private GroovyConsoleStage(CurrentStage currentStage, GroovyConsole groovyConsole) {
+		this.setTitle("Groovy Console");
+		this.setScene(new Scene(new StackPane(groovyConsole), 800, 600));
+		this.getScene().getStylesheets().add(getClass().getResource("../../../../../prob.css").toString());
 		this.setOnCloseRequest(e -> groovyConsole.closeObjectStage());
+		currentStage.register(this);
 	}
+
 }
