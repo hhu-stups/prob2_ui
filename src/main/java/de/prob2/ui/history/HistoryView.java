a1b401ac6e7ffc9818b73d9a72a91e18c43312f9
yutak001 <Yumiko.Takahashi@hhu.de>
2017-01-10 14:32:04 +0100

    removing obsolete IComponents reference

--- src/main/java/de/prob2/ui/history/HistoryView.java
+++ src/main/java/de/prob2/ui/history/HistoryView.java
@@ -8,7 +8,6 @@ import com.google.inject.Inject;
 import de.prob.statespace.Trace;
 import de.prob.statespace.Transition;
 
-import de.prob2.ui.internal.IComponents;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentTrace;
 
@@ -22,7 +21,7 @@ import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-public final class HistoryView extends AnchorPane implements IComponents{
+public final class HistoryView extends AnchorPane {
 	private static class TransitionCell extends ListCell<HistoryItem> {
 		@Override
 		protected void updateItem(HistoryItem item, boolean empty) {
