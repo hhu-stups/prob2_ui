4ff6bc2edd4b35c9f6655a1be469c69a4307d891
dgelessus <dgelessus@users.noreply.github.com>
2017-01-17 15:44:08 +0100

    Allow viewing state and evaluation errors in StatesView

--- src/main/java/de/prob2/ui/states/ElementStateTreeItem.java
+++ src/main/java/de/prob2/ui/states/ElementStateTreeItem.java
@@ -1,7 +1,6 @@
 package de.prob2.ui.states;
 
 import java.util.Map;
-import java.util.Objects;
 
 import de.prob.animator.domainobjects.AbstractEvalResult;
 import de.prob.animator.domainobjects.EnumerationWarning;
@@ -9,25 +8,42 @@ import de.prob.animator.domainobjects.EvalResult;
 import de.prob.animator.domainobjects.EvaluationErrorResult;
 import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.animator.domainobjects.IdentifierNotInitialised;
+import de.prob.animator.domainobjects.WDError;
 import de.prob.model.representation.AbstractElement;
 import de.prob.model.representation.AbstractFormulaElement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ElementStateTreeItem extends StateTreeItem<AbstractElement> {
-	public ElementStateTreeItem(final String name, final String value, final String previousValue, final AbstractElement contents) {
-		super(name, value, previousValue, contents);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ElementStateTreeItem.class);
+	
+	private final AbstractEvalResult result;
+	private final AbstractEvalResult previousResult;
+	
+	public ElementStateTreeItem(final String name, final AbstractElement contents, final AbstractEvalResult result, final AbstractEvalResult previousResult) {
+		super(name, stringRep(result), stringRep(previousResult), contents);
+		
+		this.result = result;
+		this.previousResult = previousResult;
 	}
 	
 	private static String stringRep(final AbstractEvalResult res) {
-		Objects.requireNonNull(res);
-		if (res instanceof IdentifierNotInitialised) {
-			return "(not initialized)";
+		if (res == null) {
+			return "";
 		} else if (res instanceof EvalResult) {
 			return ((EvalResult) res).getValue();
+		} else if (res instanceof IdentifierNotInitialised) {
+			return "(not initialized)";
+		} else if (res instanceof WDError) {
+			return "(not well-defined)";
 		} else if (res instanceof EvaluationErrorResult) {
-			return ((EvaluationErrorResult) res).getResult();
+			return "Error: " + ((EvaluationErrorResult) res).getResult();
 		} else if (res instanceof EnumerationWarning) {
-			return "?(∞)";
+			return "(enumeration warning)";
 		} else {
+			LOGGER.warn("Unknown result type, falling back to toString: {}", res.getClass());
+			// noinspection ObjectToString
 			return res.getClass() + " toString: " + res;
 		}
 	}
@@ -37,17 +53,25 @@ public class ElementStateTreeItem extends StateTreeItem<AbstractElement> {
 		final Map<IEvalElement, AbstractEvalResult> values,
 		final Map<IEvalElement, AbstractEvalResult> previousValues
 	) {
-		final String value;
-		final String previousValue;
+		final AbstractEvalResult result;
+		final AbstractEvalResult previousResult;
 		if (element instanceof AbstractFormulaElement) {
 			final IEvalElement formula = ((AbstractFormulaElement)element).getFormula();
-			value = stringRep(values.get(formula));
-			previousValue = previousValues == null ? "" : stringRep(previousValues.get(formula));
+			result = values.get(formula);
+			previousResult = previousValues == null ? null : previousValues.get(formula);
 		} else {
-			value = "";
-			previousValue = "";
+			result = null;
+			previousResult = null;
 		}
 		
-		return new ElementStateTreeItem(element.toString(), value, previousValue, element);
+		return new ElementStateTreeItem(element.toString(), element, result, previousResult);
+	}
+	
+	public AbstractEvalResult getResult() {
+		return this.result;
+	}
+	
+	public AbstractEvalResult getPreviousResult() {
+		return this.previousResult;
 	}
 }
