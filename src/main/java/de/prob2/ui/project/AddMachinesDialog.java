7a4e571a3a071dbb88ce52b729350f0f195c1cb0
dgelessus <dgelessus@users.noreply.github.com>
2017-03-01 13:35:07 +0100

    Remove unnecessary cast There is nothing wrong with this code - Sonar just doesn't understand lambda parameter types correctly.

--- src/main/java/de/prob2/ui/project/AddMachinesDialog.java
+++ src/main/java/de/prob2/ui/project/AddMachinesDialog.java
@@ -81,7 +81,7 @@ public class AddMachinesDialog extends Dialog<Machine> {
 		
 		nameField.textProperty().addListener((observable, from, to) -> {
 			Button okButton = (Button) this.getDialogPane().lookupButton(okButtonType);
-			if (machineNamesSet.contains((String) to)) {
+			if (machineNamesSet.contains(to)) {
 				okButton.setDisable(true);
 				errorExplanationLabel.setText("There is already a machine named '" + to + "'");
 			} else if (to.isEmpty()) {
