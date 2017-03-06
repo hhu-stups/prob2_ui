a5e868bed4c24e840c09063136cdd24ae6d07a5b
favu100 <Fabian.Vu@hhu.de>
2017-03-06 10:46:49 +0100

    Merge conflict

--- src/main/resources/de/prob2/ui/beditor/b.js
+++ src/main/resources/de/prob2/ui/beditor/b.js
@@ -7,22 +7,18 @@ CodeMirror.defineMode("b", function() {
         },
 		token: function(stream, state) {
             if (stream.match(/\/\*/, true)) {
-                t = blexer.getNextToken();
                 state.comment = true
                 return 'b-comment'
             }
 
             if (stream.match(/\*\//, true)) {
+            	blexer.getNextToken();
                 state.comment = false
                 return 'b-comment'
             }
             if(state.comment == false) {
-                t = blexer.getNextToken();
-                if(t == null) {
-                	stream.next();
-                	return 'b-nothing';
-                }
-                if (stream.match(t.getText(), true)) {
+                var t = blexer.getNextToken();
+                if (t !== null && stream.match(t.getText(), true)) {
                     return blexer.getStyleClassFromToken(t);
                 }
                 stream.next();
