5cec13f176cf9f67434ea5826c1ac7257ff138b0
dgelessus <dgelessus@users.noreply.github.com>
2017-03-07 12:35:45 +0100

    Show current machine and/or project name in window title (PROB2UI-35)

--- src/main/java/de/prob2/ui/ProB2.java
+++ src/main/java/de/prob2/ui/ProB2.java
@@ -4,11 +4,14 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import de.prob.cli.ProBInstanceProvider;
+
 import de.prob2.ui.config.Config;
 import de.prob2.ui.internal.ProB2Module;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.persistence.UIPersistence;
 import de.prob2.ui.prob2fx.CurrentProject;
+import de.prob2.ui.prob2fx.CurrentTrace;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
@@ -18,31 +21,48 @@ import javafx.stage.Stage;
 public class ProB2 extends Application {
 	private Injector injector;
 	private Config config;
+	
+	private Stage primaryStage;
 
 	public static void main(String... args) {
 		launch(args);
 	}
+	
+	private void updateTitle() {
+		final CurrentProject currentProject = injector.getInstance(CurrentProject.class);
+		final CurrentTrace currentTrace = injector.getInstance(CurrentTrace.class);
+		
+		final StringBuilder title = new StringBuilder();
+		if (currentTrace.exists()) {
+			title.append(currentTrace.getModel().getModelFile().getName());
+			title.append(" - ");
+		}
+		if (currentProject.exists()) {
+			title.append(currentProject.getName());
+			title.append(" - ");
+		}
+		title.append("ProB 2.0");
+		
+		this.primaryStage.setTitle(title.toString());
+	}
 
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
 		ProB2Module module = new ProB2Module();
 		injector = Guice.createInjector(com.google.inject.Stage.PRODUCTION, module);
 		config = injector.getInstance(Config.class);
 		UIPersistence uiPersistence = injector.getInstance(UIPersistence.class);
 		Parent root = injector.getInstance(MainController.class);
 		Scene mainScene = new Scene(root, 1024, 768);
-		primaryStage.setTitle("ProB 2.0");
 		primaryStage.setScene(mainScene);
 		primaryStage.setOnCloseRequest(e -> Platform.exit());
 
 		CurrentProject currentProject = injector.getInstance(CurrentProject.class);
-		currentProject.addListener((observable, from, to) -> {
-			if (to == null) {
-				primaryStage.setTitle("ProB 2.0");
-			} else {
-				primaryStage.setTitle("ProB 2.0 [" + to.getName() + "]");
-			}
-		});
+		currentProject.addListener((observable, from, to) -> this.updateTitle());
+		CurrentTrace currentTrace = injector.getInstance(CurrentTrace.class);
+		currentTrace.addListener((observable, from, to) -> this.updateTitle());
+		this.updateTitle();
 
 		injector.getInstance(StageManager.class).register(primaryStage, this.getClass().getName());
 
