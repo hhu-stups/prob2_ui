3253e7e7b636ee46d403f00cd563f96c1cf683f3
dgelessus <dgelessus@users.noreply.github.com>
2017-03-14 17:09:58 +0100

    Add editable formula list to history chart stage

--- src/main/java/de/prob2/ui/chart/HistoryChartStage.java
+++ src/main/java/de/prob2/ui/chart/HistoryChartStage.java
@@ -1,7 +1,6 @@
 package de.prob2.ui.chart;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.google.inject.Inject;
@@ -10,17 +9,26 @@ import com.google.inject.Singleton;
 import de.prob.animator.domainobjects.AbstractEvalResult;
 import de.prob.animator.domainobjects.ClassicalB;
 import de.prob.animator.domainobjects.EvalResult;
+import de.prob.animator.domainobjects.EvaluationException;
 import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.animator.domainobjects.IdentifierNotInitialised;
 import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
 import de.prob.statespace.TraceElement;
 
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentTrace;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import org.slf4j.Logger;
@@ -28,75 +36,143 @@ import org.slf4j.LoggerFactory;
 
 @Singleton
 public final class HistoryChartStage extends Stage {
+	private static final class ClassicalBListCell extends ListCell<ClassicalB> {
+		private ClassicalBListCell() {
+			super();
+		}
+		
+		@Override
+		protected void updateItem(final ClassicalB item, final boolean empty) {
+			super.updateItem(item, empty);
+			
+			this.setText(item == null || empty ? null : item.getCode());
+			this.setGraphic(null);
+		}
+		
+		@Override
+		public void startEdit() {
+			super.startEdit();
+			
+			if (!this.isEditing()) {
+				return;
+			}
+			
+			final TextField textField = new TextField(this.getText());
+			textField.setOnAction(event -> {
+				textField.getStyleClass().remove("badsearch");
+				final ClassicalB formula;
+				try {
+					formula = new ClassicalB(textField.getText());
+				} catch (EvaluationException e) {
+					LOGGER.debug("Could not parse user-entered formula", e);
+					textField.getStyleClass().add("badsearch");
+					return;
+				}
+				this.commitEdit(formula);
+			});
+			textField.textProperty().addListener(observable -> textField.getStyleClass().remove("badsearch"));
+			
+			this.setText(null);
+			this.setGraphic(textField);
+		}
+	}
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(HistoryChartStage.class);
 	
 	@FXML private LineChart<Number, Number> chart;
+	@FXML private ListView<ClassicalB> formulaList;
+	@FXML private Button addButton;
+	@FXML private Button removeButton;
 	
 	private final CurrentTrace currentTrace;
-	private final List<IEvalElement> formulas;
 	
 	@Inject
 	private HistoryChartStage(final StageManager stageManager, final CurrentTrace currentTrace) {
 		super();
 		
 		this.currentTrace = currentTrace;
-		this.formulas = Arrays.asList(
-			new ClassicalB("card(active)"),
-			new ClassicalB("card(ready)"),
-			new ClassicalB("card(waiting)"),
-			new ClassicalB("PID1 : waiting"),
-			new ClassicalB("PID2 : waiting"),
-			new ClassicalB("PID3 : waiting")
-		);
 		
 		stageManager.loadFXML(this, "history_chart_stage.fxml", this.getClass().getName());
 	}
 	
 	@FXML
 	private void initialize() {
-		for (final IEvalElement ee : this.formulas) {
-			this.chart.getData().add(new XYChart.Series<>(ee.getCode(), FXCollections.observableArrayList()));
+		this.formulaList.setCellFactory(view -> new ClassicalBListCell());
+		this.formulaList.getItems().addListener((ListChangeListener<ClassicalB>)change -> {
+			while (change.next()) {
+				if (change.wasRemoved()) {
+					LOGGER.debug("Removed {} ({} to {})", change.getRemoved(), change.getFrom(), change.getFrom()+change.getRemovedSize());
+					this.chart.getData().remove(change.getFrom(), change.getFrom()+change.getRemovedSize());
+				}
+				if (change.wasAdded()) {
+					for (int i = change.getFrom(); i < change.getTo(); i++) {
+						LOGGER.debug("Added {} at {}", change.getList().get(i), i);
+						this.chart.getData().add(i, new XYChart.Series<>(change.getList().get(i).getCode(), FXCollections.observableArrayList()));
+					}
+				}
+			}
+			this.update(this.currentTrace.get());
+		});
+		
+		this.removeButton.disableProperty().bind(Bindings.isEmpty(this.formulaList.getSelectionModel().getSelectedIndices()));
+		
+		this.currentTrace.addListener((observable, from, to) -> this.update(to));
+	}
+	
+	@FXML
+	private void handleAdd() {
+		this.formulaList.getItems().add(new ClassicalB("0"));
+		this.formulaList.edit(this.formulaList.getItems().size()-1);
+	}
+	
+	@FXML
+	private void handleRemove() {
+		this.formulaList.getItems().remove(this.formulaList.getSelectionModel().getSelectedIndex());
+	}
+	
+	private void update(final Trace trace) {
+		final List<List<XYChart.Data<Number, Number>>> newDatas = new ArrayList<>();
+		for (int i = 0; i < this.chart.getData().size(); i++) {
+			newDatas.add(new ArrayList<>());
 		}
 		
-		this.currentTrace.addListener((observable, from, to) -> {
-			final List<List<XYChart.Data<Number, Number>>> newDatas = new ArrayList<>();
-			for (int i = 0; i < this.chart.getData().size(); i++) {
-				newDatas.add(new ArrayList<>());
-			}
+		if (trace != null) {
+			final StateSpace stateSpace = trace.getStateSpace();
 			
-			if (to != null) {
-				final StateSpace stateSpace = to.getStateSpace();
+			TraceElement element = trace.getCurrent();
+			// Workaround for StateSpace.eval only taking exactly a List<IEvalElement>, and not a List<ClassicalB>
+			final List<IEvalElement> formulas = new ArrayList<>(this.formulaList.getItems());
+			while (element != null) {
+				final List<AbstractEvalResult> results = stateSpace.eval(element.getCurrentState(), formulas);
 				
-				TraceElement element = to.getCurrent();
-				while (element != null) {
-					final List<AbstractEvalResult> results = stateSpace.eval(element.getCurrentState(), this.formulas);
-					
-					for (int i = 0; i < results.size(); i++) {
-						final AbstractEvalResult result = results.get(i);
-						final int value;
-						try {
-							value = resultToInt(result);
-						} catch (IllegalArgumentException e) {
-							LOGGER.debug("Not convertible to int, ignoring", e);
-							continue;
-						}
-						newDatas.get(i).add(0, new XYChart.Data<>(-1, value));
+				for (int i = 0; i < results.size(); i++) {
+					final AbstractEvalResult result = results.get(i);
+					if (result instanceof IdentifierNotInitialised) {
+						continue;
 					}
-					
-					element = element.getPrevious();
+					final int value;
+					try {
+						value = resultToInt(result);
+					} catch (IllegalArgumentException e) {
+						LOGGER.debug("Not convertible to int, ignoring", e);
+						continue;
+					}
+					newDatas.get(i).add(0, new XYChart.Data<>(-1, value));
 				}
+				
+				element = element.getPrevious();
+			}
+		}
+		
+		for (int i = 0; i < newDatas.size(); i++) {
+			final List<XYChart.Data<Number, Number>> newData = newDatas.get(i);
+			
+			for (int j = 0; j < newData.size(); j++) {
+				newData.get(j).setXValue(j);
 			}
 			
-			for (int i = 0; i < newDatas.size(); i++) {
-				final List<XYChart.Data<Number, Number>> newData = newDatas.get(i);
-				
-				for (int j = 0; j < newData.size(); j++) {
-					newData.get(j).setXValue(j);
-				}
-				
-				this.chart.getData().get(i).getData().setAll(newData);
-			}
-		});
+			this.chart.getData().get(i).getData().setAll(newData);
+		}
 	}
 	
 	private static int resultToInt(final AbstractEvalResult aer) {
