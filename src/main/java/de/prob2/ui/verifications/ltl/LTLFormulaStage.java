8b36ab49909d63319ed96a877c3418560b6555e4
favu100 <Fabian.Vu@hhu.de>
2017-04-20 15:49:04 +0200

    Update triggering unsaved changes

--- src/main/java/de/prob2/ui/verifications/ltl/LTLFormulaStage.java
+++ src/main/java/de/prob2/ui/verifications/ltl/LTLFormulaStage.java
@@ -1,10 +1,14 @@
 package de.prob2.ui.verifications.ltl;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 import de.prob2.ui.internal.StageManager;
+import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,8 +17,6 @@ import javafx.stage.Stage;
 
 public class LTLFormulaStage extends Stage {
 	
-	
-		
 	@FXML
 	private TextArea ta_formula;
 	
@@ -23,21 +25,33 @@ public class LTLFormulaStage extends Stage {
 	
 	private final CurrentTrace currentTrace;
 	
+	private final CurrentProject currentProject;
+	
 	private LTLFormulaItem item;
 	
 	private Injector injector;
 		
 	@Inject
-	private LTLFormulaStage(final StageManager stageManager, final Injector injector, final CurrentTrace currentTrace) {
+	private LTLFormulaStage(final StageManager stageManager, final Injector injector, final CurrentTrace currentTrace,
+							final CurrentProject currentProject) {
 		this.injector = injector;
 		this.currentTrace = currentTrace;
+		this.currentProject = currentProject;
 		this.item = null;
 		stageManager.loadFXML(this, "ltlformula_stage.fxml");
 	}
 	
 	@FXML
 	public void initialize() {
-		ta_formula.textProperty().addListener((observable, oldValue, newValue) -> item.setFormula(newValue));
+		ta_formula.textProperty().addListener((observable, oldValue, newValue) -> {
+			List<LTLFormulaItem> newFormulas = new ArrayList<>(currentProject.getLtlFormulas());
+			int index = newFormulas.indexOf(item);
+			item.setFormula(newValue);
+			if(index != -1) {
+				newFormulas.set(index, new LTLFormulaItem(item));
+			}
+			currentProject.refreshLTLFormulas(newFormulas);
+		});
 		checkFormulaButton.disableProperty().bind(currentTrace.existsProperty().not());
 	}
 	
