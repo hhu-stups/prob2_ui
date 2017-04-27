aef1f6ac9f3459044d89ab4c043e41142279c873
Antonia <antonia.puetz@hhu.de>
2017-04-27 10:01:24 +0200

    added FontSize class and implemented methods to change the font size

---
+++ src/main/java/de/prob2/ui/layout/FontSize.java
@@ -0,0 +1,15 @@
+package de.prob2.ui.layout;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+@Singleton
+public class FontSize extends SimpleIntegerProperty {
+
+	@Inject
+	public FontSize() {
+		this.set(12);
+	}
+}
