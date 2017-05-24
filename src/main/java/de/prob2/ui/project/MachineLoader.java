8eb526cb4eccc6b1050836712fe229d4d132379e
dgelessus <dgelessus@users.noreply.github.com>
2017-05-24 14:07:42 +0200

    Add basic progress display when loading a file (PROB2UI-97)

--- src/main/java/de/prob2/ui/project/MachineLoader.java
+++ src/main/java/de/prob2/ui/project/MachineLoader.java
@@ -22,6 +22,7 @@ import de.prob2.ui.preferences.GlobalPreferences;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.project.machines.Machine;
+import de.prob2.ui.statusbar.StatusBar;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
@@ -39,10 +40,11 @@ public class MachineLoader {
 	private final AnimationSelector animations;
 	private final CurrentTrace currentTrace;
 	private final GlobalPreferences globalPreferences;
+	private final StatusBar statusBar;
 
 	@Inject
 	public MachineLoader(final Api api, final CurrentProject currentProject, final StageManager stageManager,
-			final AnimationSelector animations, final CurrentTrace currentTrace, final GlobalPreferences globalPreferences) {
+			final AnimationSelector animations, final CurrentTrace currentTrace, final GlobalPreferences globalPreferences, final StatusBar statusBar) {
 		this.api = api;
 		this.openLock = new Object();
 		this.currentProject = currentProject;
@@ -50,6 +52,7 @@ public class MachineLoader {
 		this.animations = animations;
 		this.currentTrace = currentTrace;
 		this.globalPreferences = globalPreferences;
+		this.statusBar = statusBar;
 	}
 
 	public void loadAsync(Machine machine, Map<String, String> pref) {
@@ -57,38 +60,49 @@ public class MachineLoader {
 			try {
 				this.load(machine, pref);
 			} catch (CliError | IOException | ModelTranslationError | ProBError e) {
-				LOGGER.error("Loading machine \"" + machine.getName() + "\" failed", e);
+				LOGGER.error("Loading machine \"{}\" failed", machine.getName(), e);
 				Platform.runLater(() -> stageManager
 						.makeAlert(Alert.AlertType.ERROR, "Could not open machine \"" + machine.getName() + "\":\n" + e)
 						.showAndWait());
 			}
-		} , "File Opener Thread").start();
+		}, "Machine Loader").start();
+	}
+	
+	private void setLoadingStatus(final StatusBar.LoadingStatus loadingStatus) {
+		Platform.runLater(() -> this.statusBar.setLoadingStatus(loadingStatus));
 	}
 
 	private void load(Machine machine, Map<String, String> prefs) throws IOException, ModelTranslationError {
 		// NOTE: This method may be called from outside the JavaFX main thread,
-		// for example from openAsync.
+		// for example from loadAsync.
 		// This means that all JavaFX calls must be wrapped in
 		// Platform.runLater.
 
 		// Prevent multiple threads from loading a file at the same time
 		synchronized (this.openLock) {
-			final Path path;
-			if (currentProject.getMachines().contains(machine)) {
-				final String projectLocation = currentProject.get().getLocation().getPath();
-				path = Paths.get(projectLocation, machine.getPath().toString());
-			} else {
-				path = machine.getPath();
-			}
-			final Map<String, String> allPrefs = new HashMap<>(this.globalPreferences);
-			allPrefs.putAll(prefs);
-			final StateSpace stateSpace = api.b_load(path.toString(), allPrefs);
-			Platform.runLater(() -> {
+			try {
+				setLoadingStatus(StatusBar.LoadingStatus.REMOVING_OLD_ANIMATION);
 				if (currentTrace.exists()) {
 					this.animations.removeTrace(currentTrace.get());
 				}
+				
+				setLoadingStatus(StatusBar.LoadingStatus.LOADING_FILE);
+				final Path path;
+				if (currentProject.getMachines().contains(machine)) {
+					final String projectLocation = currentProject.get().getLocation().getPath();
+					path = Paths.get(projectLocation, machine.getPath().toString());
+				} else {
+					path = machine.getPath();
+				}
+				final Map<String, String> allPrefs = new HashMap<>(this.globalPreferences);
+				allPrefs.putAll(prefs);
+				final StateSpace stateSpace = api.b_load(path.toString(), allPrefs);
+				
+				setLoadingStatus(StatusBar.LoadingStatus.ADDING_ANIMATION);
 				this.animations.addNewAnimation(new Trace(stateSpace));
-			});
+			} finally {
+				setLoadingStatus(StatusBar.LoadingStatus.NOT_LOADING);
+			}
 		}
 	}
 }
