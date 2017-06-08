9f3102af734536c3b247508598bb121f89f9f86a
Antonia <antonia.puetz@hhu.de>
2017-06-08 15:06:51 +0200

    created basic fxml for VisualisationView

--- src/main/java/de/prob2/ui/visualisation/VisualisationView.java
+++ src/main/java/de/prob2/ui/visualisation/VisualisationView.java
@@ -3,18 +3,14 @@ package de.prob2.ui.visualisation;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import javafx.scene.Group;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
+import de.prob2.ui.internal.StageManager;
+import javafx.scene.layout.AnchorPane;
 
 @Singleton
-public class VisualisationView extends StackPane {
-	private Group group;
-	
+public class VisualisationView extends AnchorPane {
+
 	@Inject
-	public VisualisationView() {
-		group = new Group();
-		this.getChildren().add(group);
-		group.getChildren().add(new ImageView(getClass().getResource("prob_logo.gif").toExternalForm()));
+	public VisualisationView(final StageManager stageManager) {
+		stageManager.loadFXML(this, "visualisation_view.fxml");
 	}
 }
