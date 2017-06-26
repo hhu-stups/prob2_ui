89e04ed5eb12ec2d6ab2b751110f3d1b95a35af5
Antonia <antonia.puetz@hhu.de>
2017-06-26 14:07:14 +0200

    added context menu on image views with right click options (actually clicking them currently causes ProBError)

--- src/main/java/de/prob2/ui/visualisation/VisualisationView.java
+++ src/main/java/de/prob2/ui/visualisation/VisualisationView.java
@@ -3,6 +3,7 @@ package de.prob2.ui.visualisation;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -14,13 +15,17 @@ import com.google.inject.Singleton;
 import de.prob.Main;
 import de.prob.statespace.State;
 import de.prob.statespace.StateSpace;
+import de.prob2.ui.commands.ExecuteRightClickCommand;
 import de.prob2.ui.commands.GetImagesForMachineCommand;
 import de.prob2.ui.commands.GetImagesForStateCommand;
+import de.prob2.ui.commands.GetRightClickOptionsForStateVisualizationCommand;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -31,7 +36,7 @@ import javafx.scene.layout.StackPane;
 @Singleton
 public class VisualisationView extends AnchorPane {
 	private static final Logger LOGGER = LoggerFactory.getLogger(VisualisationView.class);
-	
+
 	@FXML
 	private StackPane probLogoStackPane;
 	@FXML
@@ -55,7 +60,7 @@ public class VisualisationView extends AnchorPane {
 	@FXML
 	public void initialize() {
 		visualisationScrollPane.visibleProperty().bind(probLogoStackPane.visibleProperty().not());
-		
+
 		currentTrace.currentStateProperty().addListener((observable, from, to) -> {
 			visualisationGridPane.getChildren().clear();
 
@@ -67,7 +72,7 @@ public class VisualisationView extends AnchorPane {
 				Map<Integer, String> images = getImagesForMachineCommand.getImages();
 				if (!images.isEmpty()) {
 					try {
-						showImages(to, stateSpace, images);
+						showImages(to, images);
 						probLogoStackPane.setVisible(false);
 					} catch (FileNotFoundException e) {
 						LOGGER.warn("Failed to open images for visualisation", e);
@@ -85,9 +90,10 @@ public class VisualisationView extends AnchorPane {
 
 	}
 
-	private void showImages(State state, StateSpace stateSpace, Map<Integer, String> images) throws FileNotFoundException {
+	private void showImages(State state, Map<Integer, String> images)
+			throws FileNotFoundException {
 		GetImagesForStateCommand getImagesForStateCommand = new GetImagesForStateCommand(state.getId());
-		stateSpace.execute(getImagesForStateCommand);
+		state.getStateSpace().execute(getImagesForStateCommand);
 		int[][] imageMatrix = getImagesForStateCommand.getMatrix();
 		int rowNr = getImagesForStateCommand.getRows();
 		int columnNr = getImagesForStateCommand.getColumns();
@@ -97,32 +103,57 @@ public class VisualisationView extends AnchorPane {
 				String imageURL = images.get(imageMatrix[r][c]);
 				Image image = getImage(imageURL);
 				ImageView imageView = new ImageView(image);
+				ContextMenu contextMenu = getContextMenu(state, r, c);
+				imageView.setOnContextMenuRequested(e -> contextMenu.show(imageView, e.getScreenX(), e.getScreenY()));
 				visualisationGridPane.add(imageView, c, r);
 			}
 		}
 	}
 
+	private ContextMenu getContextMenu(State state, int row, int column) {
+		StateSpace stateSpace = state.getStateSpace();
+		ContextMenu contextMenu = new ContextMenu();
+		GetRightClickOptionsForStateVisualizationCommand getOptionsCommand = 
+				new GetRightClickOptionsForStateVisualizationCommand(state.getId(), row, column);
+		stateSpace.execute(getOptionsCommand);
+		List<String> options = getOptionsCommand.getOptions();
+		for(String opt: options) {
+			final MenuItem item = new MenuItem(opt);
+			ExecuteRightClickCommand executeCommand = new ExecuteRightClickCommand(state.getId(), row, column, opt);
+			item.setOnAction(e -> stateSpace.execute(executeCommand));
+			contextMenu.getItems().add(item);
+		}
+		if(options.isEmpty()) {
+			final MenuItem item = new MenuItem("no right click options defined");
+			item.setDisable(true);
+			contextMenu.getItems().add(item);
+		}
+		return contextMenu;
+	}
+
 	private Image getImage(String imageURL) throws FileNotFoundException {
 		String imagePath;
 		final String projectFolder = currentProject.get().getLocation().getPath();
-		//look in machine folder
+		// look in machine folder
 		File machineFile = new File(currentProject.getCurrentRunconfiguration().getMachine().getPath().toString());
 		String machineFolder = Paths.get(projectFolder, machineFile.getParent()).toString();
 		imagePath = Paths.get(machineFolder, imageURL).toString();
 		File imageInMachineFolder = new File(imagePath);
-		//look in project folder
+		// look in project folder
 		if (!imageInMachineFolder.exists()) {
 			imagePath = Paths.get(projectFolder, imageURL).toString();
 			File imageInProjectFolder = new File(imagePath);
-			if(!imageInProjectFolder.exists()) {
-				//look in ProB folder
+			if (!imageInProjectFolder.exists()) {
+				// look in ProB folder
 				String probFolder = Main.getProBDirectory();
 				imagePath = Paths.get(probFolder, imageURL).toString();
 				File imageInProbFolder = new File(imagePath);
-				if(!imageInProbFolder.exists()) {
+				if (!imageInProbFolder.exists()) {
 					String imageName = new File(imagePath).getName();
-					throw new FileNotFoundException("Image " + imageName + " not found in machine folder (" + imageInMachineFolder.getParent() 
-							+ ") and project folder (" + imageInProjectFolder.getParent() + ") and ProB folder (" + imageInProbFolder.getParent() + ")");
+					throw new FileNotFoundException(
+							"Image " + imageName + " not found in machine folder (" + imageInMachineFolder.getParent()
+									+ ") and project folder (" + imageInProjectFolder.getParent()
+									+ ") and ProB folder (" + imageInProbFolder.getParent() + ")");
 				}
 			}
 		}
