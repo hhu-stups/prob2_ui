ee42a059cac61d916b318c0c684b68638821740e
dgelessus <dgelessus@users.noreply.github.com>
2017-07-10 09:25:04 +0200

    Make it clear which errors are caused by history chart evaluation

--- src/main/java/de/prob2/ui/chart/HistoryChartStage.java
+++ src/main/java/de/prob2/ui/chart/HistoryChartStage.java
@@ -362,16 +362,16 @@ public final class HistoryChartStage extends Stage {
 					return Integer.parseInt(value);
 				} catch (NumberFormatException e) {
 					if (showErrors) {
-						stageManager.makeAlert(Alert.AlertType.ERROR, "Not a valid integer: " + e.getMessage()).show();
+						stageManager.makeAlert(Alert.AlertType.ERROR, "Could not evaluate formula for history chart: Not a valid integer: " + e.getMessage()).show();
 					}
-					throw new IllegalArgumentException("Not a valid integer", e);
+					throw new IllegalArgumentException("Could not evaluate formula for history chart: Not a valid integer", e);
 				}
 			}
 		} else {
 			if (showErrors) {
-				stageManager.makeAlert(Alert.AlertType.ERROR, "Invalid result: " + aer).show();
+				stageManager.makeAlert(Alert.AlertType.ERROR, "Could not evaluate formula for history chart: Invalid result: " + aer).show();
 			}
-			throw new IllegalArgumentException("Expected an EvalResult, not " + aer.getClass().getName());
+			throw new IllegalArgumentException("Could not evaluate formula for history chart: Expected an EvalResult, not " + aer.getClass().getName());
 		}
 	}
 }
