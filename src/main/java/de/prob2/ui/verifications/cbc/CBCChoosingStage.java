d99d550a8c0ac34069b4cac8ca25e0e0923b9b32
favu100 <Fabian.Vu@hhu.de>
2017-07-12 10:07:34 +0200

    Add CBCSequence and CBCDeadlock

--- src/main/java/de/prob2/ui/verifications/cbc/CBCChoosingStage.java
+++ src/main/java/de/prob2/ui/verifications/cbc/CBCChoosingStage.java
@@ -15,6 +15,13 @@ public class CBCChoosingStage extends Stage {
 	@FXML
 	private RadioButton rbInvariant;
 	
+	@FXML
+	private RadioButton rbSequence;
+	
+	@FXML
+	private RadioButton rbDeadlock;
+	
+	
 	private final Injector injector;
 	
 	@Inject
@@ -28,6 +35,10 @@ public class CBCChoosingStage extends Stage {
 	public void choose() {
 		if(rbInvariant.isSelected()) {
 			injector.getInstance(CBCInvariants.class).showAndWait();
+		} else if(rbSequence.isSelected()) {
+			injector.getInstance(CBCSequence.class).showAndWait();
+		} else {
+			injector.getInstance(CBCDeadlock.class).showAndWait();
 		}
 	}
 
---
+++ src/main/java/de/prob2/ui/verifications/cbc/CBCSequence.java
@@ -0,0 +1,29 @@
+package de.prob2.ui.verifications.cbc;
+
+
+import javax.inject.Inject;
+
+import de.prob2.ui.internal.StageManager;
+import de.prob2.ui.prob2fx.CurrentTrace;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class CBCSequence extends Stage {
+	
+	@FXML
+	private ChoiceBox<String> cbOperations;
+	
+	private final CurrentTrace currentTrace;
+
+	@Inject
+	private CBCSequence(final StageManager stageManager, final CurrentTrace currentTrace) {
+		this.currentTrace = currentTrace;
+		stageManager.loadFXML(this, "cbc_sequence.fxml");
+		this.initModality(Modality.APPLICATION_MODAL);
+		
+	}
+	
+	
+}
