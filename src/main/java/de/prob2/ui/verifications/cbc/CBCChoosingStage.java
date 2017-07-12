4f9f0ecc776b3b72d17571893ea9e5f1b912bd63
favu100 <Fabian.Vu@hhu.de>
2017-07-12 09:26:47 +0200

    Add CBC Choice View and CBC Invariants

--- src/main/java/de/prob2/ui/verifications/cbc/CBCChoosingStage.java
+++ src/main/java/de/prob2/ui/verifications/cbc/CBCChoosingStage.java
@@ -2,22 +2,33 @@ package de.prob2.ui.verifications.cbc;
 
 import javax.inject.Inject;
 
+import com.google.inject.Injector;
+
 import de.prob2.ui.internal.StageManager;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class CBCChoosingStage extends Stage {
 	
+	@FXML
+	private RadioButton rbInvariant;
+	
+	private final Injector injector;
+	
 	@Inject
-	private CBCChoosingStage(final StageManager stageManager) {
+	private CBCChoosingStage(final StageManager stageManager, final Injector injector) {
+		this.injector = injector;
 		stageManager.loadFXML(this, "cbc_checking_choice.fxml");
 		this.initModality(Modality.APPLICATION_MODAL);
 	}
 	
 	@FXML
 	public void choose() {
-		
+		if(rbInvariant.isSelected()) {
+			injector.getInstance(CBCInvariants.class).showAndWait();
+		}
 	}
 
 }
---
+++ src/main/java/de/prob2/ui/verifications/cbc/CBCInvariants.java
@@ -0,0 +1,53 @@
+package de.prob2.ui.verifications.cbc;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import de.prob.model.representation.AbstractElement;
+import de.prob.model.representation.BEvent;
+import de.prob.model.representation.Machine;
+import de.prob.statespace.Trace;
+import de.prob2.ui.internal.StageManager;
+import de.prob2.ui.prob2fx.CurrentTrace;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class CBCInvariants extends Stage {
+	
+	@FXML
+	private ChoiceBox<String> cbOperations;
+	
+	private final CurrentTrace currentTrace;
+
+	@Inject
+	private CBCInvariants(final StageManager stageManager, final CurrentTrace currentTrace) {
+		this.currentTrace = currentTrace;
+		stageManager.loadFXML(this, "cbc_invariants.fxml");
+		this.initModality(Modality.APPLICATION_MODAL);
+		
+	}
+	
+	@FXML
+	public void initialize() {
+		this.update(currentTrace.get());
+		currentTrace.addListener((observable, from, to)-> {
+			update(to);
+		});
+	}
+	
+	private void update(Trace trace) {
+		ArrayList<String> events = new ArrayList<>();
+		AbstractElement mainComponent = currentTrace.getStateSpace().getMainComponent();
+		if (mainComponent instanceof Machine) {
+			for (BEvent e : mainComponent.getChildrenOfType(BEvent.class)) {
+				events.add(e.getName());
+			}
+		}
+		cbOperations.getItems().setAll(events);
+	}
+	
+	
+}
