eec5bc3832b224d15d88cf955f652522b0103385
favu100 <Fabian.Vu@hhu.de>
2017-08-02 11:24:24 +0200

    Add opportunity to add CBCFormulas through CBCInvariants

--- src/main/java/de/prob2/ui/verifications/cbc/CBCView.java
+++ src/main/java/de/prob2/ui/verifications/cbc/CBCView.java
@@ -2,15 +2,20 @@ package de.prob2.ui.verifications.cbc;
 
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import de.prob2.ui.helpsystem.HelpButton;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.project.machines.Machine;
 import de.prob2.ui.verifications.MachineTableView;
 import de.prob2.ui.verifications.MachineTableView.CheckingType;
+import de.prob2.ui.verifications.ltl.formula.LTLFormulaItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 import javax.inject.Inject;
@@ -28,17 +33,27 @@ public class CBCView extends AnchorPane {
 	private TableView<CBCFormulaItem> tvFormula;
 	
 	@FXML
+	private TableColumn<LTLFormulaItem, FontAwesomeIconView> formulaStatusColumn;
+	
+	@FXML
+	private TableColumn<LTLFormulaItem, String> formulaNameColumn;
+	
+	@FXML
+	private TableColumn<LTLFormulaItem, String> formulaDescriptionColumn;
+	
+	@FXML
 	private Button addFormulaButton;
 	
-	private CurrentTrace currentTrace;
+	private final CurrentTrace currentTrace;
 
-	private Injector injector;
+	private final Injector injector;
 	
 	@Inject
 	public CBCView(final StageManager stageManager, final CurrentTrace currentTrace, final Injector injector) {
 		this.currentTrace = currentTrace;
 		this.injector = injector;
 		stageManager.loadFXML(this, "cbc_view.fxml");
+
 	}
 	
 	@FXML
@@ -46,6 +61,19 @@ public class CBCView extends AnchorPane {
 		helpButton.setHelpContent("HelpMain.html");
 		tvMachines.setCheckingType(CheckingType.CBC);
 		addFormulaButton.disableProperty().bind(currentTrace.existsProperty().not());
+		formulaStatusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+		formulaNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		formulaDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+		tvMachines.getSelectionModel().selectedItemProperty().addListener((observable, from, to) -> {
+			if(to != null) {
+				tvFormula.itemsProperty().bind(to.cbcFormulasProperty());
+			}
+		});
+		currentTrace.existsProperty().addListener((observable, oldValue, newValue) -> {
+			if(tvMachines.getSelectionModel().getSelectedIndex() < 0) {
+				tvMachines.getSelectionModel().select(0);
+			}
+		});
 	}
 	
 	@FXML
