ab6424ea22d6bdccd14578223b974c273780d7cd
dgelessus <dgelessus@users.noreply.github.com>
2017-08-30 12:35:52 +0200

    Make console header and prompt translatable

--- src/main/java/de/prob2/ui/consoles/b/BConsole.java
+++ src/main/java/de/prob2/ui/consoles/b/BConsole.java
@@ -1,17 +1,16 @@
 package de.prob2.ui.consoles.b;
 
+import java.util.ResourceBundle;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.prob2.ui.consoles.Console;
 
 @Singleton
 public final class BConsole extends Console {
-		
 	@Inject
-	private BConsole(BInterpreter interpreter) {
-		super("ProB 2.0 B Console");
+	private BConsole(BInterpreter interpreter, ResourceBundle bundle) {
+		super(bundle.getString("consoles.b.header"), bundle.getString("consoles.b.prompt"));
 		this.interpreter = interpreter;
-		this.appendText(header + " \n > ");
 	}
-		
 }
