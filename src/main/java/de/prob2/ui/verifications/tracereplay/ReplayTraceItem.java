ff0a38e7627065344d39e5e492306cb206030842
Antonia Pütz <antonia.puetz@hhu.de>
2017-10-13 10:07:43 +0200

    implemented check machine and basic Trace replay

--- src/main/java/de/prob2/ui/verifications/tracereplay/ReplayTraceItem.java
+++ src/main/java/de/prob2/ui/verifications/tracereplay/ReplayTraceItem.java
@@ -3,41 +3,56 @@ package de.prob2.ui.verifications.tracereplay;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import de.prob2.ui.verifications.tracereplay.ReplayTrace.Status;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.paint.Color;
 
 public class ReplayTraceItem {
 
-	private FontAwesomeIconView statusIcon;
+	private ObjectProperty<FontAwesomeIconView> statusIcon;
 	private final String name;
+	private final ReplayTrace trace;
 
 	public ReplayTraceItem(ReplayTrace trace, String name) {
 		this.name = name;
+		this.trace = trace;
+		this.statusIcon = new SimpleObjectProperty<>();
 		this.setStatus(trace.getStatus().get());
 		trace.getStatus().addListener((observable, from, to) -> setStatus(to));
 	}
 
 	private void setStatus(Status status) {
+		FontAwesomeIconView icon;
 		switch (status) {
 		case SUCCESSFUL:
-			this.statusIcon = new FontAwesomeIconView(FontAwesomeIcon.CHECK);
-			this.statusIcon.setFill(Color.GREEN);
+			icon = new FontAwesomeIconView(FontAwesomeIcon.CHECK);
+			icon.setFill(Color.GREEN);
 			break;
 		case FAILED:
-			this.statusIcon = new FontAwesomeIconView(FontAwesomeIcon.REMOVE);
-			this.statusIcon.setFill(Color.RED);
+			icon = new FontAwesomeIconView(FontAwesomeIcon.REMOVE);
+			icon.setFill(Color.RED);
 			break;
 		default:
-			this.statusIcon = new FontAwesomeIconView(FontAwesomeIcon.QUESTION_CIRCLE);
-			this.statusIcon.setFill(Color.BLUE);
+			icon = new FontAwesomeIconView(FontAwesomeIcon.QUESTION_CIRCLE);
+			icon.setFill(Color.BLUE);
 			break;
 		}
+		this.statusIcon.set(icon);
+	}
+
+	public FontAwesomeIconView getStatusIcon() {
+		return statusIcon.get();
 	}
 	
-	public FontAwesomeIconView getStatusIcon() {
+	public ObjectProperty<FontAwesomeIconView> statusIconProperty() {
 		return statusIcon;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public ReplayTrace getTrace() {
+		return trace;
 	}
 }
--- src/main/java/de/prob2/ui/verifications/tracereplay/TraceReplayView.java
+++ src/main/java/de/prob2/ui/verifications/tracereplay/TraceReplayView.java
@@ -2,15 +2,22 @@ package de.prob2.ui.verifications.tracereplay;
 
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.layout.FontSize;
 import de.prob2.ui.prob2fx.CurrentProject;
+import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.project.machines.Machine;
+import de.prob2.ui.verifications.tracereplay.ReplayTrace.Status;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -20,6 +27,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 @Singleton
 public class TraceReplayView extends ScrollPane {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TraceReplayView.class);
 
 	@FXML
 	private TableView<ReplayTraceItem> traceTableView;
@@ -27,15 +36,15 @@ public class TraceReplayView extends ScrollPane {
 	private TableColumn<ReplayTraceItem, FontAwesomeIconView> statusColumn;
 	@FXML
 	private TableColumn<ReplayTraceItem, String> nameColumn;
-	@FXML 
+	@FXML
 	private Button checkButton;
-	@FXML 
+	@FXML
 	private Button cancelButton;
 
 	private final CurrentProject currentProject;
 	private final TraceLoader traceLoader;
 	private final Injector injector;
-	
+
 	@Inject
 	private TraceReplayView(final StageManager stageManager, final CurrentProject currentProject,
 			final TraceLoader traceLoader, final Injector injector) {
@@ -47,24 +56,50 @@ public class TraceReplayView extends ScrollPane {
 
 	@FXML
 	private void initialize() {
-		statusColumn.setCellValueFactory(new PropertyValueFactory<>("statusIcon"));
-		statusColumn.setStyle( "-fx-alignment: CENTER;");
-		nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
+		statusColumn.setCellValueFactory(new PropertyValueFactory<ReplayTraceItem, FontAwesomeIconView>("statusIcon"));
+		statusColumn.setStyle("-fx-alignment: CENTER;");
+		nameColumn.setCellValueFactory(new PropertyValueFactory<ReplayTraceItem, String>("name"));
+
 		currentProject.currentMachineProperty().addListener((observable, from, to) -> updateTraceTableView(to));
-		
+
 		FontSize fontsize = injector.getInstance(FontSize.class);
 		((FontAwesomeIconView) (checkButton.getGraphic())).glyphSizeProperty().bind(fontsize.multiply(2.0));
 		((FontAwesomeIconView) (cancelButton.getGraphic())).glyphSizeProperty().bind(fontsize.multiply(2.0));
+		statusColumn.minWidthProperty().bind(fontsize.multiply(6.0));
+		statusColumn.maxWidthProperty().bind(fontsize.multiply(6.0));
 	}
 
 	private void updateTraceTableView(Machine machine) {
 		traceTableView.getItems().clear();
+		checkButton.setDisable(true);
 		if (machine != null) {
 			for (File traceFile : machine.getTraces()) {
 				ReplayTrace trace = traceLoader.loadTrace(traceFile);
 				traceTableView.getItems().add(new ReplayTraceItem(trace, traceFile.getName()));
 			}
+			checkButton.setDisable(false);
 		}
+	}
+
+	@FXML
+	private void checkMachine() {
+		for (ReplayTraceItem traceItem : traceTableView.getItems()) {
+			replayTrace(traceItem.getTrace());
+		}
+	}
+
+	private void replayTrace(ReplayTrace trace) {
+		StateSpace stateSpace = injector.getInstance(CurrentTrace.class).getStateSpace();
+		Trace t = new Trace(stateSpace);
+
+		try {
+			for (ReplayTransition transition : trace.getTransitionList()) {
+				t = t.addTransitionWith(transition.getName(), transition.getParameters());
+			}
+		} catch (IllegalArgumentException e) {
+			trace.setStatus(Status.FAILED);
+			return;
+		}
+		trace.setStatus(Status.SUCCESSFUL);
 	}
 }
