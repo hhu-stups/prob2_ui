4f39b06938e396b43ba767ac645cdacdda35c1d5
yutak001 <Yumiko.Takahashi@hhu.de>
2017-11-15 12:14:08 +0100

    fixing some errors

--- help.sh
+++ help.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 cd src/main/resources/help
+count=`ls -1 *.md 2>/dev/null | wc -l`
+if [ $count != 0 ]; then
+	echo "Found .md file. Converting..."
+	rm -f *.md.hmtl
+	for f in *.md; do pandoc "${f}" -f markdown_github -t html -s -o "${f}.html"; done
+	for f in *.md.html; do sed -i s/.md/.md.html/g "${f}"; done
+	echo "done"
+else
+	echo "No .md files found."
+fi
 for d in */ ; do
     cd "${d}"
     echo "entering ${d}"
