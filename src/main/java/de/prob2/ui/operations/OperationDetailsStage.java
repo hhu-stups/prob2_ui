b2b133e15581271ec898fb0c190dfb270707e5cd
dgelessus <dgelessus@users.noreply.github.com>
2017-12-11 12:02:53 +0100

    Change operation details view to use a tree instead of a list

--- src/main/java/de/prob2/ui/operations/OperationDetailsStage.java
+++ src/main/java/de/prob2/ui/operations/OperationDetailsStage.java
@@ -1,8 +1,7 @@
 package de.prob2.ui.operations;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import com.google.inject.Inject;
@@ -12,47 +11,26 @@ import de.prob2.ui.internal.StageManager;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 
 class OperationDetailsStage extends Stage {
-	private enum ValueType {
-		PARAM("operationDetails.type.parameter"),
-		RETURN_VALUE("operationDetails.type.returnValue"),
-		CONSTANT("operationDetails.type.constant"),
-		VARIABLE("operationDetails.type.variable"),
-		;
-		
-		private final String key;
-		
-		private ValueType(final String key) {
-			this.key = key;
-		}
-		
-		public String getKey() {
-			return this.key;
-		}
-	}
-	
 	private static final class ValueItem {
-		private final ValueType type;
 		private final String name;
 		private final String value;
 		
-		ValueItem(final ValueType type, final String name, final String value ) {
+		ValueItem(final String name, final String value) {
 			super();
 			
-			this.type = type;
+			Objects.requireNonNull(name, "name");
+			Objects.requireNonNull(value, "value");
+			
 			this.name = name;
 			this.value = value;
 		}
-		
-		public ValueType getType() {
-			return type;
-		}
-		
 		
 		public String getName() {
 			return name;
@@ -63,7 +41,7 @@ class OperationDetailsStage extends Stage {
 		}
 	}
 	
-	private final class ValueCell extends ListCell<ValueItem> {
+	private final class ValueCell extends TreeCell<ValueItem> {
 		private ValueCell() {
 			super();
 		}
@@ -75,17 +53,17 @@ class OperationDetailsStage extends Stage {
 			if (item == null || empty) {
 				this.setText(null);
 			} else {
-				this.setText(String.format(
-					bundle.getString("operationDetails.format"),
-					bundle.getString(item.getType().getKey()),
-					item.getName(),
-					item.getValue()
-				));
+				this.setText(item.getName());
 			}
 		}
 	}
 	
-	@FXML private ListView<ValueItem> valuesListView;
+	@FXML private TreeView<ValueItem> valuesTreeView;
+	@FXML private TreeItem<ValueItem> rootItem;
+	@FXML private TreeItem<ValueItem> parametersItem;
+	@FXML private TreeItem<ValueItem> returnValuesItem;
+	@FXML private TreeItem<ValueItem> constantsItem;
+	@FXML private TreeItem<ValueItem> variablesItem;
 	@FXML private TextArea textArea;
 	
 	private final ResourceBundle bundle;
@@ -105,43 +83,53 @@ class OperationDetailsStage extends Stage {
 	
 	@FXML
 	private void initialize() {
-		this.valuesListView.setCellFactory(lv -> new ValueCell());
-		this.valuesListView.getSelectionModel().selectedItemProperty().addListener((observable, from, to) -> this.textArea.setText(to == null ? null : to.getValue()));
+		this.valuesTreeView.setCellFactory(lv -> new ValueCell());
+		this.valuesTreeView.getSelectionModel().selectedItemProperty().addListener((observable, from, to) -> this.textArea.setText(to == null ? null : to.getValue().getValue()));
+		
+		this.rootItem.setValue(new ValueItem("Values (this root item should be invisible)", ""));
+		this.parametersItem.setValue(new ValueItem(bundle.getString("operationDetails.groups.parameters"), ""));
+		this.returnValuesItem.setValue(new ValueItem(bundle.getString("operationDetails.groups.returnValues"), ""));
+		this.constantsItem.setValue(new ValueItem(bundle.getString("operationDetails.groups.constants"), ""));
+		this.variablesItem.setValue(new ValueItem(bundle.getString("operationDetails.groups.variables"), ""));
 		
 		this.item.addListener((observable, from, to) -> {
-			this.valuesListView.getItems().clear();
+			this.rootItem.getChildren().clear();
+			this.rootItem.getChildren().add(this.parametersItem);
+			this.rootItem.getChildren().add(this.returnValuesItem);
+			this.rootItem.getChildren().add(this.constantsItem);
+			this.rootItem.getChildren().add(this.variablesItem);
+			this.rootItem.getChildren().forEach(ti -> ti.getChildren().clear());
 			if (to == null) {
 				this.setTitle(bundle.getString("operationDetails.title"));
 			} else {
 				this.setTitle(String.format(bundle.getString("operationDetails.titleWithName"), to.getName()));
+				
 				final List<String> paramNames = to.getParameterNames();
 				final List<String> paramValues = to.getParameterValues();
 				for (int i = 0; i < paramValues.size(); i++) {
-					String name = i < paramNames.size() ? paramNames.get(i): "#" + (i + 1);
+					String name = i < paramNames.size() ? paramNames.get(i) : "#" + (i + 1);
 					final String param = paramValues.get(i);
-					this.valuesListView.getItems().add(new ValueItem(ValueType.PARAM, name, param));
+					this.parametersItem.getChildren().add(new TreeItem<>(new ValueItem(name, param)));
 				}
+				
 				final List<String> outputParamNames = to.getOutputParameterNames();
 				final List<String> returnValues = to.getReturnValues();
 				for (int i = 0; i < returnValues.size(); i++) {
-					final String name = i < outputParamNames.size() ? outputParamNames.get(i): "#" + (i + 1);
+					final String name = i < outputParamNames.size() ? outputParamNames.get(i) : "#" + (i + 1);
 					final String retval = returnValues.get(i);
-					this.valuesListView.getItems().add(new ValueItem(ValueType.RETURN_VALUE, name, retval));
+					this.returnValuesItem.getChildren().add(new TreeItem<>(new ValueItem(name, retval)));
 				}
 				
-				addToValuesListView(ValueType.CONSTANT, to.getConstants());
-				addToValuesListView(ValueType.VARIABLE, to.getVariables());
+				to.getConstants().forEach((key, value) ->
+					this.constantsItem.getChildren().add(new TreeItem<>(new ValueItem(key, value)))
+				);
+				
+				to.getVariables().forEach((key, value) ->
+					this.variablesItem.getChildren().add(new TreeItem<>(new ValueItem(key, value)))
+				);
 			}
+			this.rootItem.getChildren().removeIf(ti -> ti.getChildren().isEmpty());
 		});
-	}
-	
-	private void addToValuesListView(ValueType type, Map<String, String> map) {
-		for (Entry<String, String> element : map.entrySet()) {
-			String name = element.getKey();
-			String value = element.getValue();
-			this.valuesListView.getItems().add(new ValueItem(type, name, value));
-		}
-		
 	}
 
 	public ObjectProperty<OperationItem> itemProperty() {
