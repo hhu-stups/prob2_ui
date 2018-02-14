6d0f2f6dfa4ebccdb57cccd19c51701d7b8db487
yutak001 <Yumiko.Takahashi@hhu.de>
2018-02-14 11:43:36 +0100

    refactoring

--- src/main/java/de/prob2/ui/helpsystem/HelpButton.java
+++ src/main/java/de/prob2/ui/helpsystem/HelpButton.java
@@ -20,8 +20,6 @@ import org.slf4j.LoggerFactory;
 public class HelpButton extends Button{
 	private Injector injector;
 	private File helpContent;
-	private Map<Class<?>, String> germanHelpMap = new HashMap<>();
-	private Map<Class<?>, String> englishHelpMap = new HashMap<>();
 
 	@Inject
 	private HelpButton(StageManager stageManager, Injector injector) {
@@ -29,18 +27,12 @@ public class HelpButton extends Button{
 		stageManager.loadFXML(this, "helpbutton.fxml");
 	}
 
-	@FXML
-	private void initialize() {
-		// this needs to be updated if new translations of help are added and/or if new help buttons are added
-		prepareMap(germanHelpMap, this.getClass().getClassLoader().getResourceAsStream("help/help_de.txt"));
-		prepareMap(englishHelpMap, this.getClass().getClassLoader().getResourceAsStream("help/help_en.txt"));
-	}
-
-	private void prepareMap(Map map, InputStream stream) {
+	private Map prepareMap(InputStream stream) {
+		Map map = new HashMap<Class<?>, String>();
 		Scanner scanner = new Scanner(stream);
 		while (scanner.hasNext()) {
 			String s = scanner.nextLine();
-			int splitIndex = s.indexOf(",");
+			int splitIndex = s.indexOf(',');
 			String className = s.substring(0, splitIndex);
 			String htmlFileName = s.substring(splitIndex + 1);
 			try {
@@ -49,6 +41,7 @@ public class HelpButton extends Button{
 				LoggerFactory.getLogger(HelpButton.class).error("No class with this name found", e);
 			}
 		}
+		return map;
 	}
 
 	@FXML
@@ -62,25 +55,13 @@ public class HelpButton extends Button{
 	}
 
 	public void setHelpContent(Class<?> clazz) {
-		switch (injector.getInstance(HelpSystemStage.class).help.helpSubdirectoryString) {
-			case "help_de":
-				setHelp(clazz,
-						Main.getProBDirectory() +
-								"prob2ui" + File.separator +
-								"help" + File.separator +
-								"help_de" + File.separator,
-						germanHelpMap);
-				break;
-			case "help_en":
-			default:
-				setHelp(clazz,
-						Main.getProBDirectory() +
-								"prob2ui" + File.separator +
-								"help" + File.separator +
-								"help_en" + File.separator,
-						englishHelpMap);
-				break;
-		}
+		String helpSubdirectory = injector.getInstance(HelpSystemStage.class).help.helpSubdirectoryString;
+		setHelp(clazz,
+				Main.getProBDirectory() +
+						"prob2ui" + File.separator +
+						"help" + File.separator +
+						helpSubdirectory + File.separator,
+				prepareMap(this.getClass().getClassLoader().getResourceAsStream("help/"+helpSubdirectory+".txt")));
 	}
 
 	private void setHelp(Class<?> clazz, String main, Map<Class<?>, String> map) {
