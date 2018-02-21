d2926bf643b065f6cde96c4759d86a46e8f5ccfc
dohan <dohan001@hhu.de>
2018-02-21 15:59:58 +0100

    consider operation output values while replaying a trace

--- src/main/java/de/prob2/ui/verifications/tracereplay/TraceChecker.java
+++ src/main/java/de/prob2/ui/verifications/tracereplay/TraceChecker.java
@@ -8,12 +8,17 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob.animator.command.GetOperationByPredicateCommand;
+import de.be4.classicalb.core.parser.exceptions.BCompoundException;
+import de.prob.animator.command.GetMachineOperationInfos.OperationInfo;
 import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.check.tracereplay.PersistentTransition;
 import de.prob.formula.PredicateBuilder;
+import de.prob.statespace.LoadedMachine;
 import de.prob.statespace.StateSpace;
 import de.prob.statespace.Trace;
 import de.prob.statespace.Transition;
+import de.prob.translator.Translator;
+import de.prob.translator.types.BObject;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
@@ -26,6 +31,8 @@ import javafx.beans.property.SimpleMapProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
 import javafx.collections.SetChangeListener;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 @Singleton
 public class TraceChecker {
@@ -55,64 +62,118 @@ public class TraceChecker {
 		replayTrace(replayTraces.get(traceFile), setCurrentAnimation);
 	}
 
-	public void replayTrace(ReplayTrace trace, final boolean setCurrentAnimation) {
+	public void replayTrace(ReplayTrace replayTrace, final boolean setCurrentAnimation) {
 		Thread replayThread = new Thread(() -> {
-			trace.setStatus(Status.NOT_CHECKED);
+			replayTrace.setStatus(Status.NOT_CHECKED);
 
 			StateSpace stateSpace = currentTrace.getStateSpace();
 
-			Trace t = new Trace(stateSpace);
-			t.setExploreStateByDefault(false);
+			Trace trace = new Trace(stateSpace);
+			trace.setExploreStateByDefault(false);
 			boolean traceReplaySuccess = true;
-			try {
-				for (PersistentTransition transition : trace.getStoredTrace().getTransitionList()) {
-					String predicate = new PredicateBuilder().addMap(transition.getParameters())
-							//TODO destination state variables are currently not supported 
-							//.addMap(transition.getDestinationStateVariables())
-							// TODO output parameters are currently not supported by ExecuteOperationByPredicate
-							// .addMap(transition.getOuputParameters()) 
-							.toString();
-					final IEvalElement pred = stateSpace.getModel().parseFormula(predicate);
-					final GetOperationByPredicateCommand command = new GetOperationByPredicateCommand(stateSpace,
-							t.getCurrentState().getId(), transition.getOperationName(), pred, 1);
-					stateSpace.execute(command);
-					if (command.hasErrors()) {
-						throw new IllegalArgumentException(
-								"Executing operation " + transition.getOperationName() + " with predicate " + predicate
-										+ " produced errors: " + Joiner.on(", ").join(command.getErrors()));
-					}
-					List<Transition> possibleTransitions = command.getNewTransitions();
-					if (possibleTransitions.isEmpty()) {
-						traceReplaySuccess = false;
-						break;
-					}
-					t = t.add(possibleTransitions.get(0));
-					if (Thread.currentThread().isInterrupted()) {
-						currentJobThreads.remove(Thread.currentThread());
-						return;
-					}
+			for (PersistentTransition persistentTransition : replayTrace.getStoredTrace().getTransitionList()) {
+				Transition trans = replayPersistentTransition(stateSpace, trace, persistentTransition,
+						setCurrentAnimation);
+				if (trans != null) {
+					trace = trace.add(trans);
+				} else {
+					break;
 				}
-			} catch (IllegalArgumentException | de.prob.exception.ProBError e) {
-				traceReplaySuccess = false;
-				trace.setError(e);
+
+				if (Thread.currentThread().isInterrupted()) {
+					currentJobThreads.remove(Thread.currentThread());
+					return;
+				}
 			}
 
-			trace.setStatus(traceReplaySuccess ? Status.SUCCESSFUL : Status.FAILED);
-			t.setExploreStateByDefault(true);
+			replayTrace.setStatus(traceReplaySuccess ? Status.SUCCESSFUL : Status.FAILED);
+			trace.setExploreStateByDefault(true);
 			if (setCurrentAnimation) {
 				// set the current trace in both cases
-				t.getCurrentState().explore();
-				currentTrace.set(t);
-				
-				if (trace.getError() != null) {
-					Platform.runLater(() -> stageManager.makeExceptionAlert("", trace.getError()).showAndWait());
+				trace.getCurrentState().explore();
+				currentTrace.set(trace);
+
+				if (replayTrace.getError() != null) {
+					Platform.runLater(() -> stageManager.makeExceptionAlert("", replayTrace.getError()).showAndWait());
 				}
-				
+
 			}
 			currentJobThreads.remove(Thread.currentThread());
 		}, "Trace Replay Thread");
 		currentJobThreads.add(replayThread);
 		replayThread.start();
+	}
+
+	private Transition replayPersistentTransition(StateSpace stateSpace, Trace t,
+			PersistentTransition persistentTransition, boolean setCurrentAnimation) {
+		String predicate = new PredicateBuilder().addMap(persistentTransition.getParameters())
+				// TODO destination state variables are currently
+				// not supported
+				// .addMap(transition.getDestinationStateVariables())
+				// TODO output parameters are currently not
+				// supported by ExecuteOperationByPredicate
+				// .addMap(transition.getOuputParameters())
+				.toString();
+		final IEvalElement pred = stateSpace.getModel().parseFormula(predicate);
+		final GetOperationByPredicateCommand command = new GetOperationByPredicateCommand(stateSpace,
+				t.getCurrentState().getId(), persistentTransition.getOperationName(), pred, 1);
+		stateSpace.execute(command);
+		if (command.hasErrors()) {
+			String errorMessage = "Executing operation " + persistentTransition.getOperationName() + " with predicate "
+					+ predicate + " produced errors: " + Joiner.on(", ").join(command.getErrors());
+			Platform.runLater(() -> getReplayErrorAlert(errorMessage).showAndWait());
+			return null;
+		}
+		List<Transition> possibleTransitions = command.getNewTransitions();
+		if (possibleTransitions.isEmpty()) {
+			String errorMessage = "Executing operation " + persistentTransition.getOperationName() + " with predicate "
+					+ predicate + " produced errors: Operation not possible.";
+			Platform.runLater(() -> getReplayErrorAlert(errorMessage).showAndWait());
+			return null;
+		}
+		Transition trans = possibleTransitions.get(0);
+		String operationName = trans.getName();
+		OperationInfo machineOperationInfo = stateSpace.getLoadedMachine().getMachineOperationInfo(operationName);
+		if (machineOperationInfo != null) {
+			List<String> outputParameterNames = machineOperationInfo.getOutputParameterNames();
+			try {
+				List<BObject> translatedReturnValues = trans.getTranslatedReturnValues();
+				for (int i = 0; i < outputParameterNames.size(); i++) {
+					String outputParamName = outputParameterNames.get(i);
+					BObject paramValueFromTransition = translatedReturnValues.get(i);
+					if (persistentTransition.getOuputParameters().containsKey(outputParamName)) {
+						String stringValue = persistentTransition.getOuputParameters().get(outputParamName);
+						BObject bValue = Translator.translate(stringValue);
+						if (!bValue.equals(paramValueFromTransition)) {
+							// TODO do we need further checks here
+							// because the value translator does not
+							// support enum value properly
+							if (setCurrentAnimation) {
+								String errorMessage = String.format(
+										"Can not replay operation '%s'. The value of the ouput parameter '%s' does not match.\n"
+												+ "Value from trace file: %s\nComputed value: %s",
+										operationName, outputParamName, bValue.toString(), paramValueFromTransition);
+								Platform.runLater(() -> {
+									Platform.runLater(() -> getReplayErrorAlert(errorMessage).showAndWait());
+								});
+							}
+							return null;
+						}
+					}
+
+				}
+			} catch (BCompoundException e) {
+				Platform.runLater(() -> stageManager.makeExceptionAlert("", e.getFirstException()).showAndWait());
+				return null;
+			}
+		}
+		return trans;
+	}
+
+	private Alert getReplayErrorAlert(String errorMessage) {
+		Alert alert = stageManager.makeAlert(AlertType.ERROR, errorMessage);
+		alert.setHeaderText("Replay Error");
+		return alert;
 	}
 
 	void cancelReplay() {
