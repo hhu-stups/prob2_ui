7114c4bc299b59559d58b34874ffc5c7e82bd2ed
miwer106 <michellewerth@hotmail.com>
2018-03-07 09:31:49 +0100

    Update report bug stage

--- src/main/java/de/prob2/ui/menu/ReportBugStage.java
+++ src/main/java/de/prob2/ui/menu/ReportBugStage.java
@@ -3,22 +3,17 @@ package de.prob2.ui.menu;
 import java.util.ResourceBundle;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import de.prob2.ui.internal.StageManager;
 import javafx.scene.Scene;
-import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-@Singleton
 public class ReportBugStage extends Stage {
 	@Inject
 	public ReportBugStage(StageManager stageManager, ResourceBundle bundle) {
 		WebView webView = new WebView();
-		WebEngine webEngine = webView.getEngine();
-		webEngine.setJavaScriptEnabled(true);
-		webEngine.load("https://probjira.atlassian.net/secure/RapidBoard.jspa?rapidView=8");
-		
+		String script = getClass().getResource("bugreportbutton.html").toExternalForm();
+		webView.getEngine().load(script);
 		this.setTitle(bundle.getString("reportBug.title"));
 		this.setScene(new Scene(webView));
 		stageManager.register(this, this.getClass().getName());
