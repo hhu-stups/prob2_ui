e84c9468366560f44943369fd8b556062d357788
dohan <dohan001@hhu.de>
2018-07-24 13:53:37 +0200

    truncate formulas in OperationView

--- src/main/java/de/prob2/ui/operations/OperationItem.java
+++ src/main/java/de/prob2/ui/operations/OperationItem.java
@@ -17,6 +17,7 @@ import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.exception.ProBError;
 import de.prob.statespace.LoadedMachine;
 import de.prob.statespace.OperationInfo;
+import de.prob.statespace.StateSpace;
 import de.prob.statespace.Trace;
 import de.prob.statespace.Transition;
 
@@ -36,18 +37,10 @@ public class OperationItem {
 	private final Map<String, String> constants;
 	private final Map<String, String> variables;
 
-	public OperationItem(
-		final Trace trace,
-		final Transition transition,
-		final String name,
-		final Status status,
-		final List<String> parameterNames,
-		final List<String> parameterValues,
-		final List<String> returnParameterNames,
-		final List<String> returnParameterValues,
-		final Map<String, String> constants,
-		final Map<String, String> variables
-	) {
+	public OperationItem(final Trace trace, final Transition transition, final String name, final Status status,
+			final List<String> parameterNames, final List<String> parameterValues,
+			final List<String> returnParameterNames, final List<String> returnParameterValues,
+			final Map<String, String> constants, final Map<String, String> variables) {
 		this.trace = Objects.requireNonNull(trace);
 		this.transition = transition;
 		this.name = Objects.requireNonNull(name);
@@ -60,7 +53,8 @@ public class OperationItem {
 		this.variables = Objects.requireNonNull(variables);
 	}
 
-	private static LinkedHashMap<String, String> getNextStateValues(Transition transition, List<IEvalElement> formulas) {
+	private static LinkedHashMap<String, String> getNextStateValues(Transition transition,
+			List<IEvalElement> formulas) {
 		// It seems that there is no way to easily find out the
 		// constant/variable values which a specific $setup_constants or
 		// $initialise_machine transition would set.
@@ -84,7 +78,8 @@ public class OperationItem {
 	}
 
 	public static OperationItem forTransition(final Trace trace, final Transition transition) {
-		final LoadedMachine loadedMachine = trace.getStateSpace().getLoadedMachine();
+		final StateSpace stateSpace = trace.getStateSpace();
+		final LoadedMachine loadedMachine = stateSpace.getLoadedMachine();
 		OperationInfo opInfo;
 		try {
 			opInfo = loadedMachine.getMachineOperationInfo(transition.getName());
@@ -92,49 +87,59 @@ public class OperationItem {
 			// fallback solution if getMachineOperationInfo throws a ProBError
 			opInfo = null;
 		}
-		
+
 		final Map<String, String> constants;
 		final Map<String, String> variables;
 		switch (transition.getName()) {
-			case "$setup_constants":
-				constants = getNextStateValues(transition, loadedMachine.getConstantEvalElements());
+		case "$setup_constants":
+			constants = getNextStateValues(transition, getConstantsAsTruncatedEvalElements(stateSpace));
+			variables = Collections.emptyMap();
+			break;
+
+		case "$initialise_machine":
+			variables = getNextStateValues(transition, getVariablesAsTruncatedEvalElements(stateSpace));
+			constants = Collections.emptyMap();
+			break;
+
+		default:
+			constants = Collections.emptyMap();
+			if (opInfo == null) {
 				variables = Collections.emptyMap();
-				break;
-			
-			case "$initialise_machine":
-				variables = getNextStateValues(transition, loadedMachine.getVariableEvalElements());
-				constants = Collections.emptyMap();
-				break;
-			
-			default:
-				constants = Collections.emptyMap();
-				if (opInfo == null) {
-					variables = Collections.emptyMap();
-				} else {
-					variables = getNextStateValues(transition, opInfo.getNonDetWrittenVariables().stream()
-						.map(var -> trace.getStateSpace().getModel().parseFormula(var, FormulaExpand.TRUNCATE))
-						.collect(Collectors.toList()));
-				}
+			} else {
+				variables = getNextStateValues(transition,
+						opInfo.getNonDetWrittenVariables().stream()
+								.map(var -> trace.getStateSpace().getModel().parseFormula(var, FormulaExpand.TRUNCATE))
+								.collect(Collectors.toList()));
+			}
 		}
-		
+
 		final List<String> paramNames = opInfo == null ? Collections.emptyList() : opInfo.getParameterNames();
 		final List<String> outputNames = opInfo == null ? Collections.emptyList() : opInfo.getOutputParameterNames();
-		
-		return new OperationItem(
-			trace, transition, transition.getName(), Status.ENABLED,
-			paramNames, transition.getParameterValues(),
-			outputNames, transition.getReturnValues(),
-			constants, variables
-		);
+
+		return new OperationItem(trace, transition, transition.getName(), Status.ENABLED, paramNames,
+				transition.getParameterValues(), outputNames, transition.getReturnValues(), constants, variables);
 	}
 
-	public static OperationItem forDisabled(final Trace trace, final String name, final Status status, final List<String> parameters) {
-		return new OperationItem(
-			trace, null, name, status,
-			Collections.emptyList(), parameters,
-			Collections.emptyList(), Collections.emptyList(),
-			Collections.emptyMap(), Collections.emptyMap()
-		);
+	private static List<IEvalElement> getConstantsAsTruncatedEvalElements(StateSpace sp) {
+		List<IEvalElement> constantEvalElements = new ArrayList<>();
+		for (String string : sp.getLoadedMachine().getConstantNames()) {
+			constantEvalElements.add(sp.getModel().parseFormula(string, FormulaExpand.TRUNCATE));
+		}
+		return constantEvalElements;
+	}
+
+	private static List<IEvalElement> getVariablesAsTruncatedEvalElements(StateSpace sp) {
+		List<IEvalElement> variableEvalElements = new ArrayList<>();
+		for (String string : sp.getLoadedMachine().getVariableNames()) {
+			variableEvalElements.add(sp.getModel().parseFormula(string, FormulaExpand.TRUNCATE));
+		}
+		return variableEvalElements;
+	}
+
+	public static OperationItem forDisabled(final Trace trace, final String name, final Status status,
+			final List<String> parameters) {
+		return new OperationItem(trace, null, name, status, Collections.emptyList(), parameters,
+				Collections.emptyList(), Collections.emptyList(), Collections.emptyMap(), Collections.emptyMap());
 	}
 
 	public Trace getTrace() {
@@ -186,43 +191,38 @@ public class OperationItem {
 	}
 
 	public boolean isSkip() {
-		return this.getTransition() != null && this.getTransition().getSource().equals(this.getTransition().getDestination());
+		return this.getTransition() != null
+				&& this.getTransition().getSource().equals(this.getTransition().getDestination());
 	}
 
 	@Override
 	public String toString() {
-		return MoreObjects.toStringHelper(this)
-			.add("trace", this.getTrace())
-			.add("transition", this.getTransition())
-			.add("name", this.getName())
-			.add("status", this.getStatus())
-			.add("parameterNames", this.getParameterNames())
-			.add("parameterValues", this.getParameterValues())
-			.add("returnParameterNames", this.getReturnParameterNames())
-			.add("returnParameterValues", this.getReturnParameterValues())
-			.add("constants", this.getConstants())
-			.add("variables", this.getVariables())
-			.toString();
+		return MoreObjects.toStringHelper(this).add("trace", this.getTrace()).add("transition", this.getTransition())
+				.add("name", this.getName()).add("status", this.getStatus())
+				.add("parameterNames", this.getParameterNames()).add("parameterValues", this.getParameterValues())
+				.add("returnParameterNames", this.getReturnParameterNames())
+				.add("returnParameterValues", this.getReturnParameterValues()).add("constants", this.getConstants())
+				.add("variables", this.getVariables()).toString();
 	}
 
 	private static String getPrettyName(final String name) {
 		switch (name) {
-			case "$setup_constants":
-				return "SETUP_CONSTANTS";
-	
-			case "$initialise_machine":
-				return "INITIALISATION";
-	
-			default:
-				return name;
+		case "$setup_constants":
+			return "SETUP_CONSTANTS";
+
+		case "$initialise_machine":
+			return "INITIALISATION";
+
+		default:
+			return name;
 		}
 	}
 
 	public String toPrettyString() {
 		StringBuilder sb = new StringBuilder(getPrettyName(this.getName()));
-		
+
 		final List<String> args = new ArrayList<>();
-		
+
 		final List<String> paramNames = this.getParameterNames();
 		final List<String> paramValues = this.getParameterValues();
 		if (paramNames.isEmpty()) {
@@ -234,25 +234,24 @@ public class OperationItem {
 				args.add(paramNames.get(i) + '=' + paramValues.get(i));
 			}
 		}
-		
-		if(this.name.equals("$setup_constants") || this.name.equals("$initialise_machine")) {
+
+		if (this.name.equals("$setup_constants") || this.name.equals("$initialise_machine")) {
 			this.getConstants().forEach((key, value) -> args.add(key + ":=" + value));
 			this.getVariables().forEach((key, value) -> args.add(key + ":=" + value));
 		}
 
-		
 		if (!args.isEmpty()) {
 			sb.append('(');
 			sb.append(String.join(", ", args));
 			sb.append(')');
 		}
-		
+
 		final List<String> returnValues = this.getReturnParameterValues();
 		if (!returnValues.isEmpty()) {
 			sb.append(" → ");
 			sb.append(String.join(", ", returnValues));
 		}
-		
+
 		return sb.toString();
 	}
 }
