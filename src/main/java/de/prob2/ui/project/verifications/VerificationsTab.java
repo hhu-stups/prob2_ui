b888dd8f166dcbd7773b11010159fa5674d4c9d4
yutak001 <Yumiko.Takahashi@hhu.de>
2018-08-31 10:17:29 +0200

    making status help button work

--- src/main/java/de/prob2/ui/project/verifications/VerificationsTab.java
+++ src/main/java/de/prob2/ui/project/verifications/VerificationsTab.java
@@ -2,15 +2,21 @@ package de.prob2.ui.project.verifications;
 
 import com.google.inject.Inject;
 
+import de.prob2.ui.helpsystem.HelpButton;
 import de.prob2.ui.internal.StageManager;
+import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 
 public class VerificationsTab extends Tab {
-	
+	@FXML HelpButton helpButton;
+
 	@Inject
 	private VerificationsTab(final StageManager stageManager) {
 		stageManager.loadFXML(this, "verifications_tab.fxml");
 	}
 	
-	
+	@FXML
+	public void initialize() {
+		helpButton.setHelpContent(this.getClass());
+	}
 }
