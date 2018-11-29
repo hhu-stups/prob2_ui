ac7f2a22c2fedd75c1818409ead776fb37e8dfa9
Antonia Pütz <antonia.puetz@hhu.de>
2018-11-29 22:17:09 +0100

    refactor MagicGraphFX and add Java Doc comments

--- src/main/java/de/prob2/ui/visualisation/magiclayout/MagicGraphFX.java
+++ src/main/java/de/prob2/ui/visualisation/magiclayout/MagicGraphFX.java
@@ -45,12 +45,11 @@ public class MagicGraphFX implements MagicGraphI {
 	@Override
 	public Node generateMagicGraph(State state) {
 		Model model = new Model();
-		graph = new Graph(model);
-
 		if (state != null) {
-			addStateValuesToModel(state, model);
+			model = addStateValuesToModel(state);
 		}
 
+		graph = new Graph(model);
 		graph.layout(new RandomLayout());
 		return graph;
 	}
@@ -58,31 +57,28 @@ public class MagicGraphFX implements MagicGraphI {
 	@Override
 	public void updateMagicGraph(State state) {
 		// add state values to a new temporary model
-		Model tempModel = new Model();
+		Model newModel = new Model();
 		if (state != null) {
-			addStateValuesToModel(state, tempModel);
+			newModel = addStateValuesToModel(state);
 		}
 
-		// merge temporary model to old model
-		Model model = graph.getModel();
+		// merge new model to old model
+		Model graphModel = graph.getModel();
+		Set<Vertex> verticesBefore = new HashSet<>(graphModel.getVertices());
+		Set<Edge> edgesBefore = new HashSet<>(graphModel.getEdges());
 
-		Set<Edge> edgesBefore = new HashSet<>(model.getEdges());
-		tempModel.getEdges().forEach(edge -> {
-			Vertex source = getVertex(edge.getSource().getCaption(), model);
-			Vertex target = getVertex(edge.getTarget().getCaption(), model);
-			edgesBefore.remove(getEdge(source, target, edge.getCaption(), model));
-		});
-		edgesBefore.forEach(edge -> model.removeEdge(edge));
-		tempModel.getEdges().forEach(edge -> {
-			Vertex source = getVertex(edge.getSource().getCaption(), model);
-			Vertex target = getVertex(edge.getTarget().getCaption(), model);
-			model.addEdge(getEdge(source, target, edge.getCaption(), model));
-		});
+		// transform the vertices and edges of the new model to their
+		// corresponding edges and vertices in the given graph model
+		Model transformedNewModel = transformModel(newModel, graphModel);
 
-		Set<Vertex> verticesBefore = new HashSet<>(model.getVertices());
-		tempModel.getVertices().forEach(vertex -> verticesBefore.remove(getVertex(vertex.getCaption(), model)));
-		verticesBefore.forEach(vertex -> model.removeVertex(vertex));
-		tempModel.getVertices().forEach(vertex -> model.addVertex(getVertex(vertex.getCaption(), model)));
+		// determine which vertices and edges have to be removed and remove them
+		verticesBefore.removeAll(transformedNewModel.getVertices());
+		verticesBefore.forEach(vertex -> graphModel.removeVertex(vertex));
+		edgesBefore.removeAll(transformedNewModel.getEdges());
+		edgesBefore.forEach(edge -> graphModel.removeEdge(edge));
+
+		// add the new vertices and edges
+		combineModel(graphModel, transformedNewModel);
 
 		graph.update();
 	}
@@ -92,13 +88,12 @@ public class MagicGraphFX implements MagicGraphI {
 		magicNodes.forEach(node -> {
 			try {
 				BObject bObject = Translator.translate(node.getExpression());
-				Model modelToStyle = getModel(node.getName(), bObject);
+				Model modelToStyle = transformModel(getModel(node.getName(), bObject), graph.getModel());
 
 				Vertex.Style style = new Vertex.Style(node.getNodeColor(), node.getLineColor(), node.getLineWidth(),
 						node.getLineType(), node.getTextColor());
 
 				modelToStyle.getVertices().forEach(vertex -> {
-					vertex = getVertex(vertex.getCaption(), graph.getModel());
 					vertex.setStyle(style);
 					vertex.setType(toVertexType(node.getShape()));
 				});
@@ -113,17 +108,12 @@ public class MagicGraphFX implements MagicGraphI {
 			try {
 				if (!magicEdge.getExpression().equals("NOT-INITIALISED: ")) {
 					BObject bObject = Translator.translate(magicEdge.getExpression());
-					Model modelToStyle = getModel(magicEdge.getName(), bObject);
+					Model modelToStyle = transformModel(getModel(magicEdge.getName(), bObject), graph.getModel());
 
 					Edge.Style style = new Edge.Style(magicEdge.getLineColor(), magicEdge.getLineWidth(),
 							magicEdge.getLineType(), magicEdge.getTextColor(), magicEdge.getTextSize());
 
-					modelToStyle.getEdges().forEach(edge -> {
-						Vertex source = getVertex(edge.getSource().getCaption(), graph.getModel());
-						Vertex target = getVertex(edge.getTarget().getCaption(), graph.getModel());
-						edge = getEdge(source, target, edge.getCaption(), graph.getModel());
-						edge.setStyle(style);
-					});
+					modelToStyle.getEdges().forEach(edge -> edge.setStyle(style));
 				}
 			} catch (BCompoundException e) {
 				stageManager.makeExceptionAlert(e, "",
@@ -133,7 +123,17 @@ public class MagicGraphFX implements MagicGraphI {
 		});
 	}
 
-	private void addStateValuesToModel(State state, Model model) {
+	/**
+	 * Creates a model containing vertices and edges which represent the sets,
+	 * constants and variables of the specified state.
+	 * 
+	 * @param state
+	 * @return the model containing vertices and edges representing the sets,
+	 *         constants and variables of the specified state
+	 */
+	private Model addStateValuesToModel(State state) {
+		Model model = new Model();
+
 		List<IEvalElement> setEvalElements = state.getStateSpace().getLoadedMachine().getSetEvalElements();
 		Map<String, BObject> translatedSetsMap = translateMap(state.evalFormulas(setEvalElements));
 		translatedSetsMap.forEach((string, obj) -> combineModel(model, getModel(string, obj)));
@@ -145,8 +145,19 @@ public class MagicGraphFX implements MagicGraphI {
 		Map<IEvalElement, AbstractEvalResult> variableResultMap = state.getVariableValues();
 		Map<String, BObject> translatedVariableMap = translateMap(variableResultMap);
 		translatedVariableMap.forEach((string, obj) -> combineModel(model, getModel(string, obj)));
+
+		return model;
 	}
 
+	/**
+	 * Uses the {@link Translator} to translate the values of the specified map to
+	 * {@link BObject}s.
+	 * 
+	 * @param evalMap the map to be translated
+	 * @return a map which contains the string representation of the
+	 *         {@link IEvalElement}s as keys and the {@link AbstractEvalResult}s
+	 *         translated to {@link BObject}s as values
+	 */
 	private Map<String, BObject> translateMap(Map<IEvalElement, AbstractEvalResult> evalMap) {
 		Map<String, BObject> translatedMap = new HashMap<>();
 
@@ -165,21 +176,57 @@ public class MagicGraphFX implements MagicGraphI {
 		return translatedMap;
 	}
 
+	/**
+	 * Creates a model which represents the specified {@link BObject}.
+	 * 
+	 * @param caption
+	 * @param bObject
+	 * @return a model representing the specified {@link BObject}
+	 */
 	private Model getModel(String caption, BObject bObject) {
 		Model model = new Model();
 		if (!(bObject instanceof Collection<?>)) {
-			model.addEdge(getEdge(getVertex(caption, model), getVertex(bObject.toString(), model), "", model));
+			model.addEdge(new Edge(new Vertex(caption), new Vertex(bObject.toString()), ""));
 		} else if (bObject instanceof de.prob.translator.types.Tuple) {
 			de.prob.translator.types.Tuple tuple = (de.prob.translator.types.Tuple) bObject;
-			model.addEdge(getEdge(getVertex(tuple.getFirst().toString(), model),
-					getVertex(tuple.getSecond().toString(), model), caption, model));
+			model.addEdge(new Edge(new Vertex(tuple.getFirst().toString()), new Vertex(tuple.getSecond().toString()),
+					caption));
 		} else {
 			((Collection<?>) bObject).forEach(element -> combineModel(model, getModel(caption, (BObject) element)));
 		}
 		return model;
 	}
 
-	private Model combineModel(Model model1, Model model2) {
+	/**
+	 * Checks for each vertex/edge of the {@code fromModel} if the {@code toModel}
+	 * contains a vertex/edge with the same properties. If such a vertex/edge exists
+	 * it is added to the transformed model, otherwise the vertex/edge of the
+	 * {@code fromModel} is added to the transformed model unchanged.
+	 * 
+	 * @param fromModel model to be transformed
+	 * @param toModel   model to be transformed to
+	 * @return the transformed model
+	 */
+	private Model transformModel(Model fromModel, Model toModel) {
+		Model transformedModel = new Model();
+		fromModel.getVertices().forEach(vertex -> transformedModel.addVertex(getVertex(vertex.getCaption(), toModel)));
+		fromModel.getEdges().forEach(edge -> {
+			Vertex source = getVertex(edge.getSource().getCaption(), toModel);
+			Vertex target = getVertex(edge.getTarget().getCaption(), toModel);
+			transformedModel.addEdge(getEdge(source, target, edge.getCaption(), toModel));
+		});
+		return transformedModel;
+	}
+
+	/**
+	 * Combines the two specified models into a single one. Adds all vertices and
+	 * edges of {@code model2} to {@code model1}. Vertices and Edges with the same
+	 * properties are treated as equal and therefore not added twice.
+	 * 
+	 * @param model1 the model to which {@code model2} is added
+	 * @param model2 the model to be added to {@code model1}
+	 */
+	private void combineModel(Model model1, Model model2) {
 		model2.getVertices().forEach(vertex -> model1.addVertex(getVertex(vertex.getCaption(), model1)));
 		model2.getEdges().forEach(edge -> {
 			// does model1 already contain the source and/or target vertex?
@@ -188,9 +235,18 @@ public class MagicGraphFX implements MagicGraphI {
 
 			model1.addEdge(getEdge(source, target, edge.getCaption(), model1));
 		});
-		return model1;
 	}
 
+	/**
+	 * Checks if the specified model contains an edge with the specified properties.
+	 * 
+	 * @param source
+	 * @param target
+	 * @param caption
+	 * @param model
+	 * @return an edge with the same properties from the specified model or a new
+	 *         edge if the model doesn't contain such an edge
+	 */
 	private Edge getEdge(Vertex source, Vertex target, String caption, Model model) {
 		for (Edge edge : model.getEdges()) {
 			if (edge.getSource().equals(source) && edge.getTarget().equals(target)
@@ -201,6 +257,15 @@ public class MagicGraphFX implements MagicGraphI {
 		return new Edge(source, target, caption);
 	}
 
+	/**
+	 * Checks if the specified model contains a vertex with the specified
+	 * properties.
+	 * 
+	 * @param caption
+	 * @param model
+	 * @return a vertex with the same properties from the specified model or a new
+	 *         vertex if the model doesn't contain such a vertex
+	 */
 	private Vertex getVertex(String caption, Model model) {
 		for (Vertex vertex : model.getVertices()) {
 			if (vertex.getCaption().equals(caption)) {
@@ -210,6 +275,13 @@ public class MagicGraphFX implements MagicGraphI {
 		return new Vertex(caption);
 	}
 
+	/**
+	 * Transforms the specified shape from {@link MagicShape} to {@link Vertex.Type}
+	 * 
+	 * @param shape the {@link MagicShape} to be transformed
+	 * @return the {@link Vertex.Type} which corresponds to the specified
+	 *         {@link MagicShape}
+	 */
 	private Vertex.Type toVertexType(MagicShape shape) {
 		switch (shape) {
 		case CIRCLE:
