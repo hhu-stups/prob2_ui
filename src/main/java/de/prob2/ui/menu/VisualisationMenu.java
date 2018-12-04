71da6e8911b94796f463d2fba19cfa0f203d53c8
Antonia Pütz <antonia.puetz@hhu.de>
2018-12-04 17:30:44 +0100

    remove unused currentTrace from Visualisation Menu

--- src/main/java/de/prob2/ui/menu/VisualisationMenu.java
+++ src/main/java/de/prob2/ui/menu/VisualisationMenu.java
@@ -3,12 +3,12 @@ package de.prob2.ui.menu;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
+
 import de.prob2.ui.chart.HistoryChartStage;
 import de.prob2.ui.dynamic.dotty.DotView;
 import de.prob2.ui.dynamic.table.ExpressionTableView;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentProject;
-import de.prob2.ui.prob2fx.CurrentTrace;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
@@ -20,16 +20,17 @@ import javafx.stage.Stage;
  */
 @Singleton
 public class VisualisationMenu extends Menu {
-	@FXML private MenuItem graphVisualization;
-	@FXML private MenuItem tableVisualization;
-	
+	@FXML
+	private MenuItem graphVisualization;
+	@FXML
+	private MenuItem tableVisualization;
+
 	private final Injector injector;
-	private final CurrentTrace currentTrace;
 	private final CurrentProject currentProject;
 
 	@Inject
-	public VisualisationMenu(final StageManager stageManager, final Injector injector, final CurrentTrace currentTrace, final CurrentProject currentProject) {
-		this.currentTrace = currentTrace;
+	public VisualisationMenu(final StageManager stageManager, final Injector injector,
+			final CurrentProject currentProject) {
 		this.currentProject = currentProject;
 		this.injector = injector;
 		stageManager.loadFXML(this, "visualisationMenu.fxml");
@@ -40,17 +41,16 @@ public class VisualisationMenu extends Menu {
 		this.graphVisualization.disableProperty().bind(currentProject.currentMachineProperty().isNull());
 		this.tableVisualization.disableProperty().bind(currentProject.currentMachineProperty().isNull());
 	}
-	
+
 	@FXML
 	private void openGraphVisualisation() {
 		injector.getInstance(DotView.class).show();
 	}
-	
+
 	@FXML
 	private void openTableVisualisation() {
 		injector.getInstance(ExpressionTableView.class).show();
 	}
-
 
 	@FXML
 	private void handleHistoryChart() {
