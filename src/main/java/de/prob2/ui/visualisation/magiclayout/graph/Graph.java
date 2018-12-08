22c34a60d082eef9d25286a4df69bc9542431dfb
Antonia Pütz <antonia.puetz@hhu.de>
2018-12-08 10:31:09 +0100

    fix exception caused by trying to layout empty graph

--- src/main/java/de/prob2/ui/visualisation/magiclayout/graph/Graph.java
+++ src/main/java/de/prob2/ui/visualisation/magiclayout/graph/Graph.java
@@ -11,38 +11,40 @@ public class Graph extends Pane {
 	public Graph(Model model) {
 		this.model = model;
 	}
-	
+
 	public Model getModel() {
 		return model;
 	}
-	
+
 	public void layout(Layout layout) {
-		this.layout =  layout;
-		
+		this.layout = layout;
+
 		this.getChildren().clear();
 		model.getVertices().forEach(vertex -> this.getChildren().add(vertex));
 		model.getEdges().forEach(edge -> this.getChildren().add(edge));
-		
-		layout.drawGraph(this);
+
+		if (this.getChildren().size() > 0) {
+			layout.drawGraph(this);
+		}
 		model.finishUpdate();
 	}
-	
-	public void update() {	
+
+	public void update() {
 		model.getRemovedVertices().forEach(vertex -> this.getChildren().remove(vertex));
 		model.getRemovedEdges().forEach(edge -> this.getChildren().remove(edge));
-		
+
 		model.getAddedVertices().forEach(vertex -> this.getChildren().add(vertex));
 		model.getAddedEdges().forEach(edge -> this.getChildren().add(edge));
-		
-		layout.updateGraph(this);
+
+		if (this.getChildren().size() > 0) {
+			layout.updateGraph(this);
+		}
 		model.finishUpdate();
 	}
-	
+
 	public boolean vertexAt(double x, double y, double offsetX, double offsetY) {
 		for (Vertex vertex : model.getVertices()) {
-			if (x > (vertex.getLeftX() - offsetX) 
-					&& x < vertex.getRightX() 
-					&& y > (vertex.getTopY() - offsetY)
+			if (x > (vertex.getLeftX() - offsetX) && x < vertex.getRightX() && y > (vertex.getTopY() - offsetY)
 					&& y < vertex.getBottomY()) {
 				return true;
 			}
