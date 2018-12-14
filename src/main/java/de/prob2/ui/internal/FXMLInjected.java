3b4af9fd1854b3b6fc3c0b17617dd38790a0ef45
dgelessus <dgelessus@users.noreply.github.com>
2018-12-14 12:01:07 +0100

    Add Javadoc to @FXMLInjected annotation

--- src/main/java/de/prob2/ui/internal/FXMLInjected.java
+++ src/main/java/de/prob2/ui/internal/FXMLInjected.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the annotated class should be injected using Guice if it is used in an FXML file, instead of using the default JavaFX builder mechanism.
+ * This is needed for our custom JavaFX view subclasses, which have to be created through Guice for the dependency injection to work properly.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface FXMLInjected {}
