2b7da4dc60518e4f9b4f2dd0cafa6752b6293b06
dgelessus <dgelessus@users.noreply.github.com>
2018-12-14 12:49:10 +0100

    Remove Console.ConfigData class which only has a single field

--- src/main/java/de/prob2/ui/consoles/b/BConsole.java
+++ src/main/java/de/prob2/ui/consoles/b/BConsole.java
@@ -7,6 +7,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob2.ui.config.Config;
+import de.prob2.ui.config.ConfigData;
 import de.prob2.ui.config.ConfigListener;
 import de.prob2.ui.consoles.Console;
 import de.prob2.ui.prob2fx.CurrentTrace;
@@ -35,13 +36,15 @@ public final class BConsole extends Console {
 		
 		config.addListener(new ConfigListener() {
 			@Override
-			public void loadConfig(final de.prob2.ui.config.ConfigData configData) {
-				applySettings(configData.bConsoleSettings);
+			public void loadConfig(final ConfigData configData) {
+				if (configData.bConsoleInstructions != null) {
+					loadInstructions(configData.bConsoleInstructions);
+				}
 			}
 			
 			@Override
-			public void saveConfig(final de.prob2.ui.config.ConfigData configData) {
-				configData.bConsoleSettings = getSettings();
+			public void saveConfig(final ConfigData configData) {
+				configData.bConsoleInstructions = saveInstructions();
 			}
 		});
 	}
