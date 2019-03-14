eb787022aa9529fda8756d48eb68d963e158a784
dgelessus <dgelessus@users.noreply.github.com>
2019-03-14 12:47:23 +0100

    Add ControlsFX library as a replacement for FontAwesomeFX (PROB2UI-320)

---
+++ src/main/java/de/prob2/ui/layout/BindableGlyph.java
@@ -0,0 +1,36 @@
+package de.prob2.ui.layout;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+import org.controlsfx.glyphfont.Glyph;
+
+public class BindableGlyph extends Glyph {
+	private final DoubleProperty bindableFontSize;
+	
+	public BindableGlyph() {
+		super();
+		
+		this.bindableFontSize = new SimpleDoubleProperty(this, "bindableFontSize", 1.0);
+		this.bindableFontSize.addListener((o, from, to) -> this.setFontSize(to.doubleValue()));
+	}
+	
+	public BindableGlyph(final String fontFamily, final Object icon) {
+		this();
+		
+		this.setFontFamily(fontFamily);
+		this.setIcon(icon);
+	}
+	
+	public DoubleProperty bindableFontSizeProperty() {
+		return this.bindableFontSize;
+	}
+	
+	public double getBindableFontSize() {
+		return this.bindableFontSizeProperty().get();
+	}
+	
+	public void setBindableFontSize(final double bindableFontSize) {
+		this.bindableFontSizeProperty().set(bindableFontSize);
+	}
+}
