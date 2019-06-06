247bc316fba9153554ab743f3e82f90a2dcdae63
dgelessus <dgelessus@users.noreply.github.com>
2019-06-06 12:39:07 +0200

    Add a custom TabPane subclass that respects min sizes of its contents

---
+++ src/main/java/de/prob2/ui/internal/BetterTabPane.java
@@ -0,0 +1,80 @@
+package de.prob2.ui.internal;
+
+import javafx.geometry.Side;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Region;
+
+/**
+ * <p>Slightly modified version of {@link TabPane} that calculates a proper minimum size based on its contents.</p>
+ */
+public final class BetterTabPane extends TabPane {
+	public BetterTabPane() {
+		this((Tab[])null);
+	}
+	
+	public BetterTabPane(final Tab... tabs) {
+		super(tabs);
+	}
+	
+	private boolean isHorizontal() {
+		final Side tabPosition = this.getSide();
+		return Side.TOP.equals(tabPosition) || Side.BOTTOM.equals(tabPosition);
+	}
+	
+	private Region getTabHeaderArea() {
+		final Node tabHeaderArea = this.lookup(".tab-header-area");
+		return tabHeaderArea instanceof Region ? (Region)tabHeaderArea : null;
+	}
+	
+	@Override
+	protected double computeMinWidth(final double height) {
+		final double defaultMinWidth = super.computeMinWidth(height);
+		if (defaultMinWidth != 0.0) {
+			return defaultMinWidth;
+		}
+		
+		double highestMinWidth = 0.0;
+		for (final Tab tab : this.getTabs()) {
+			highestMinWidth = Math.max(highestMinWidth, this.snapSize(tab.getContent().minWidth(-1)));
+		}
+		
+		final boolean isHorizontal = this.isHorizontal();
+		final Region tabHeaderArea = this.getTabHeaderArea();
+		final double tabHeaderAreaSize;
+		if (tabHeaderArea == null) {
+			tabHeaderAreaSize = 0.0;
+		} else {
+			tabHeaderAreaSize = snapSize(isHorizontal ? tabHeaderArea.prefWidth(-1) : tabHeaderArea.prefHeight(-1));
+		}
+		
+		double prefWidth = isHorizontal ? Math.max(highestMinWidth, tabHeaderAreaSize) : highestMinWidth + tabHeaderAreaSize;
+		return snapSize(prefWidth) + snappedRightInset() + snappedLeftInset();
+	}
+	
+	@Override
+	protected double computeMinHeight(final double width) {
+		final double defaultMinHeight = super.computeMinHeight(width);
+		if (defaultMinHeight != 0.0) {
+			return defaultMinHeight;
+		}
+		
+		double highestMinHeight = 0.0;
+		for (final Tab tab : this.getTabs()) {
+			highestMinHeight = Math.max(highestMinHeight, this.snapSize(tab.getContent().minHeight(-1)));
+		}
+		
+		final boolean isHorizontal = this.isHorizontal();
+		final Region tabHeaderArea = this.getTabHeaderArea();
+		final double tabHeaderAreaSize;
+		if (tabHeaderArea == null) {
+			tabHeaderAreaSize = 0.0;
+		} else {
+			tabHeaderAreaSize = snapSize(isHorizontal ? tabHeaderArea.prefHeight(-1) : tabHeaderArea.prefWidth(-1));
+		}
+		
+		double prefWidth = isHorizontal ? highestMinHeight + snapSize(tabHeaderAreaSize) : Math.max(highestMinHeight, tabHeaderAreaSize);
+		return snapSize(prefWidth) + snappedTopInset() + snappedBottomInset();
+	}
+}
