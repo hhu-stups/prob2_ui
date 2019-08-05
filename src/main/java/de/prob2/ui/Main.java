70bb9d39be60b490e1bba7391d488a3a4916bc39
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2019-08-05 14:13:58 +0200

    Fix application plugin for Java 11/12

---
+++ src/main/java/de/prob2/ui/Main.java
@@ -0,0 +1,11 @@
+package de.prob2.ui;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String [] args) {
+        Application.launch(ProB2.class, args);
+    }
+
+}
