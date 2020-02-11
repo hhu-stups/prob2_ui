baefd443cc8dcdd60abd8b21d410c6fc65acd6b8
dgelessus <dgelessus@users.noreply.github.com>
2020-02-11 17:08:20 +0100

    Remove some duplicate code in HelpSystemStage

--- src/main/java/de/prob2/ui/helpsystem/HelpSystemStage.java
+++ src/main/java/de/prob2/ui/helpsystem/HelpSystemStage.java
@@ -6,8 +6,6 @@ import java.util.ResourceBundle;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import de.prob.Main;
-import de.prob2.ui.ProB2;
 import de.prob2.ui.internal.StageManager;
 
 import javafx.application.Platform;
@@ -21,12 +19,7 @@ public class HelpSystemStage extends Stage {
 		this.setTitle(bundle.getString("helpsystem.stage.title"));
 		this.setScene(new Scene(help));
 		stageManager.register(this, this.getClass().getName());
-		String defaultDir;
-		if (help.isJar)
-			defaultDir = Main.getProBDirectory() + "prob2ui" + File.separator + "help" + File.separator + help.helpSubdirectoryString + File.separator;
-		else
-			defaultDir = ProB2.class.getClassLoader().getResource("help" + File.separator + help.helpSubdirectoryString + File.separator).toString();
-		File defaultPage = new File(defaultDir + "ProB2UI.html");
+		File defaultPage = new File(help.getHelpSubdirectoryPath() + "ProB2UI.html");
 		setContent(defaultPage,"");
 	}
 
