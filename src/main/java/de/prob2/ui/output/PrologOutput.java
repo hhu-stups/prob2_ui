71da9f7b9c39fee83504b476e07171df51734130
yutak001 <yumiko.takahashi@uni-duesseldorf.de>
2020-03-25 14:24:40 +0100

    Adding ANSI code handling for Prolog Output

--- src/main/java/de/prob2/ui/output/PrologOutput.java
+++ src/main/java/de/prob2/ui/output/PrologOutput.java
@@ -9,20 +9,27 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.prob.cli.ProBInstance;
 import javafx.application.Platform;
-import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.slf4j.LoggerFactory;
 
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Singleton
-public class PrologOutput extends TextArea {
+public class PrologOutput extends TextFlow {
 
 	private static class PrologOutputAppender extends OutputStream {
 		StringBuilder sb;
-		TextArea textArea;
-		private PrologOutputAppender(TextArea textArea) {
+		TextFlow textFlow;
+		private PrologOutputAppender(TextFlow textFlow) {
 			this.sb = new StringBuilder();
-			this.textArea = textArea;
+			this.textFlow = textFlow;
 		}
 
 		@Override
@@ -32,25 +39,79 @@ public class PrologOutput extends TextArea {
 				//This avoids the UI from hanging up.
 				sb.append((char) b);
 				if(b == 10) {
-					this.textArea.appendText(sb.toString());
+					Text output = handleOutput(sb.toString());
+					this.textFlow.getChildren().add(output);
 					this.sb = new StringBuilder();
 				}
 			});
+		}
+
+		private Text handleOutput(String s) {
+			Paint fontColor = Color.BLACK;
+			Text output = new Text();
+			boolean underline = false;
+			boolean visible = true;
+			FontWeight weight = FontWeight.NORMAL;
+			while (s.charAt(1) == '[') {
+				// removing escape
+				s = s.substring(1);
+				// setting font color and attributes for output and removing remaining ANSI code from string
+				if (s.startsWith("[31m")) {
+					s = s.replace("[31m", "");
+					fontColor = Color.RED;
+				} else if (s.startsWith("[32m")) {
+					s = s.replace("[32m", "");
+					fontColor = Color.GREEN;
+				} else if (s.startsWith("[33m")) {
+					s = s.replace("[33m", "");
+					fontColor = Color.YELLOW;
+				} else if (s.startsWith("[34m")) {
+					s = s.replace("[34m", "");
+					fontColor = Color.BLUE;
+				} else if (s.startsWith("[35m")) {
+					s = s.replace("[35m", "");
+					fontColor = Color.MAGENTA;
+				} else if (s.startsWith("[36m")) {
+					s = s.replace("[36m", "");
+					fontColor = Color.CYAN;
+				} else if (s.startsWith("[37m")) {
+					s = s.replace("[37m", "");
+					fontColor = Color.WHITE;
+				} else if (s.startsWith("[1m")) {
+					s = s.replace("[1m", "");
+					weight = FontWeight.BOLD;
+				} else if (s.startsWith("[4m")) {
+					s = s.replace("[4m", "");
+					underline = true;
+				} else if (s.startsWith("[5m")) {
+					s = s.replace("[5m", "");
+					// not supported
+				} else if (s.startsWith("[7m")) {
+					s = s.replace("[7m", "");
+					// not supported
+				} else if (s.startsWith("[8m")) {
+					s = s.replace("[8m", "");
+					visible = false;
+				}
+			}
+			output.setFont(Font.font(output.getFont().getFamily(), weight, output.getFont().getSize()));
+			output.setText(s);
+			output.setFill(fontColor);
+			output.setUnderline(underline);
+			output.setVisible(visible);
+			return output;
 		}
 	}
 
 	@Inject
 	public PrologOutput() {
-		this.setEditable(false);
-		this.setWrapText(true);
-		this.setScrollTop(Double.MAX_VALUE);
 		PrologOutputAppender prologOutputAppender = new PrologOutputAppender(this);
 		LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 
 		Thread thread = new Thread(() -> {
 			PatternLayoutEncoder encoder = new PatternLayoutEncoder();
 			encoder.setContext(context);
-			encoder.setPattern("%replace(%msg){'\\[0m', ''}%n");
+			encoder.setPattern("%replace(%msg){'\\u001b\\[0m', ''}%n");
 			encoder.start();
 
 			OutputStreamAppender<ILoggingEvent> outputStreamAppender = new OutputStreamAppender<>();
