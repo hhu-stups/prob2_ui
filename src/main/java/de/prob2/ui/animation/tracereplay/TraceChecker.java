c66eac445742fe100fb1875b6ff61e1e28726f63
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2020-07-07 13:18:09 +0200

    Update TraceChecker

--- src/main/java/de/prob2/ui/animation/tracereplay/TraceChecker.java
+++ src/main/java/de/prob2/ui/animation/tracereplay/TraceChecker.java
@@ -1,37 +1,23 @@
 package de.prob2.ui.animation.tracereplay;
 
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
-
-import de.hhu.stups.prob.translator.BValue;
-import de.hhu.stups.prob.translator.Translator;
-import de.hhu.stups.prob.translator.exceptions.TranslationException;
 import de.prob.animator.command.GetOperationByPredicateCommand;
-import de.prob.animator.domainobjects.FormulaExpand;
-import de.prob.animator.domainobjects.IEvalElement;
+import de.prob.check.tracereplay.ITraceChecker;
 import de.prob.check.tracereplay.PersistentTrace;
 import de.prob.check.tracereplay.PersistentTransition;
+import de.prob.check.tracereplay.TraceReplay;
 import de.prob.formula.PredicateBuilder;
-import de.prob.statespace.OperationInfo;
 import de.prob.statespace.StateSpace;
 import de.prob.statespace.Trace;
-import de.prob.statespace.Transition;
 import de.prob2.ui.internal.FXMLInjected;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.tracediff.TraceDiffStage;
 import de.prob2.ui.verifications.Checked;
-
 import javafx.application.Platform;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
@@ -40,9 +26,17 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Region;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 @FXMLInjected
 @Singleton
-public class TraceChecker {
+public class TraceChecker implements ITraceChecker {
 
 	private final CurrentTrace currentTrace;
 	private final Injector injector;
@@ -74,70 +68,105 @@ public class TraceChecker {
 		if(persistentTrace == null) {
 			return;
 		}
-
 		replayTrace.setChecked(Checked.NOT_CHECKED);
+		StateSpace stateSpace = currentTrace.getStateSpace();
+		Map<String, Object> replayInformation = new HashMap<>();
+		replayInformation.put("replayTrace", replayTrace);
 		Thread replayThread = new Thread(() -> {
-			StateSpace stateSpace = currentTrace.getStateSpace();
-			Trace trace = new Trace(stateSpace);
-			trace.setExploreStateByDefault(false);
-			Checked status = Checked.SUCCESS;
-			final List<PersistentTransition> transitionList = persistentTrace.getTransitionList();
-			for (int i = 0; i < transitionList.size(); i++) {
-				final int finalI = i;
-				Platform.runLater(() -> replayTrace.setProgress((double) finalI / transitionList.size()));
-				Transition trans = replayPersistentTransition(replayTrace, trace, transitionList.get(i),
-						setCurrentAnimation);
-				if (trans != null) {
-					trace = trace.add(trans);
-				} else {
-					status = Checked.FAIL;
-					break;
-				}
-
-				if (Thread.currentThread().isInterrupted()) {
-					currentJobThreads.remove(Thread.currentThread());
-					return;
-				}
-			}
-
-			final Checked finalStatus = status;
-			Platform.runLater(() -> {
-				replayTrace.setChecked(finalStatus);
-				replayTrace.setProgress(-1);
-			});
-			trace.setExploreStateByDefault(true);
+			Trace trace = TraceReplay.replayTrace(persistentTrace, stateSpace, setCurrentAnimation, replayInformation, this);
 			if (setCurrentAnimation) {
 				// set the current trace if no error has occured. Otherwise leave the decision to the user
-				trace.getCurrentState().explore();
-				final Trace copyTrace = trace;
 				if (replayTrace.getErrorMessageBundleKey() != null) {
-					if (isNewTrace) {
-						Platform.runLater(() -> {
-							Alert alert = new Alert(Alert.AlertType.WARNING);
-							alert.initOwner(stageManager.getCurrent());
-							alert.setContentText(String.format(injector.getInstance(ResourceBundle.class).getString("animation.tracereplay.alerts.traceReplayError.newTraceContent"), lineNumber(replayTrace), replayTrace.getErrorMessageParams()[0].toString(), replayTrace.getErrorMessageParams()[1].toString()));
-							alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-							alert.show();
-						});
-						isNewTrace = false;
-					} else {
-						Platform.runLater(() -> {
-							TraceReplayErrorAlert alert = new TraceReplayErrorAlert(injector, replayTrace.getErrorMessageBundleKey(), replayTrace.getErrorMessageParams());
-							alert.initOwner(stageManager.getCurrent());
-							boolean isEqual = currentTrace.get().getTransitionList().equals(copyTrace.getTransitionList());
-							alert.setErrorMessage(false, isEqual, copyTrace.getTransitionList().size(), persistentTrace.getTransitionList().size());
-							setAlertButtons(isEqual, copyTrace, persistentTrace, alert);
-						});
-					}
+					showTraceReplayFailed(trace, replayInformation);
 				} else {
 					currentTrace.set(trace);
 				}
-
 			}
 			currentJobThreads.remove(Thread.currentThread());
 		}, "Trace Replay Thread");
 		currentJobThreads.add(replayThread);
 		replayThread.start();
+	}
+
+	public void updateProgress(double value, Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+		Platform.runLater(() -> replayTrace.setProgress(value));
+	}
+
+	public void setResult(boolean success, Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+		Platform.runLater(() -> {
+			if(success) {
+				replayTrace.setChecked(Checked.SUCCESS);
+			} else {
+				replayTrace.setChecked(Checked.FAIL);
+			}
+			replayTrace.setProgress(-1);
+		});
+	}
+
+	public void interrupt() {
+		currentJobThreads.remove(Thread.currentThread());
+	}
+
+	public void showCommandErrors(Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+		GetOperationByPredicateCommand command = (GetOperationByPredicateCommand) replayInformation.get("command");
+		PersistentTransition persistentTransition = (PersistentTransition) replayInformation.get("persistentTransition");
+		PredicateBuilder predicateBuilder = (PredicateBuilder) replayInformation.get("predicateBuilder");
+		replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage");
+		replayTrace.setErrorMessageParams(persistentTransition.getOperationName(), predicateBuilder, String.join(", ", command.getErrors()));
+	}
+
+	public void showNoOperationPossible(Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+        PersistentTransition persistentTransition = (PersistentTransition) replayInformation.get("persistentTransition");
+        PredicateBuilder predicateBuilder = (PredicateBuilder) replayInformation.get("predicateBuilder");
+		replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage.operationNotPossible");
+		replayTrace.setErrorMessageParams(persistentTransition.getOperationName(), predicateBuilder);
+	}
+
+	private void showTraceReplayFailed(Trace trace, Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+		final Trace copyTrace = trace;
+		if (isNewTrace) {
+			Platform.runLater(() -> {
+				Alert alert = new Alert(Alert.AlertType.WARNING);
+				alert.initOwner(stageManager.getCurrent());
+				alert.setContentText(String.format(injector.getInstance(ResourceBundle.class).getString("animation.tracereplay.alerts.traceReplayError.newTraceContent"), lineNumber(replayTrace), replayTrace.getErrorMessageParams()[0].toString(), replayTrace.getErrorMessageParams()[1].toString()));
+				alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+				alert.show();
+			});
+			isNewTrace = false;
+		} else {
+			PersistentTrace persistentTrace = replayTrace.getPersistentTrace();
+			Platform.runLater(() -> {
+				TraceReplayErrorAlert alert = new TraceReplayErrorAlert(injector, replayTrace.getErrorMessageBundleKey(), replayTrace.getErrorMessageParams());
+				alert.initOwner(stageManager.getCurrent());
+				boolean isEqual = currentTrace.get().getTransitionList().equals(copyTrace.getTransitionList());
+				alert.setErrorMessage(false, isEqual, copyTrace.getTransitionList().size(), persistentTrace.getTransitionList().size());
+				setAlertButtons(isEqual, copyTrace, persistentTrace, alert);
+			});
+		}
+	}
+
+	public void showTraceReplayError(Exception e) {
+		Platform.runLater(
+				() -> stageManager
+						.makeExceptionAlert(e,
+								"animation.tracereplay.alerts.traceReplayError.header",
+								"animation.tracereplay.alerts.traceReplayError.content")
+						.showAndWait());
+	}
+
+	public void showMismatchOutputParameters(Map<String, Object> replayInformation) {
+		ReplayTrace replayTrace = (ReplayTrace) replayInformation.get("replayTrace");
+        String operationName = (String) replayInformation.get("operationName");
+        String outputParamName = (String) replayInformation.get("outputParamName");
+        String bValue = (String) replayInformation.get("bValue");
+        String paramValueFromTransition = (String) replayInformation.get("paramValue");
+		replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage.mismatchingOutputValues");
+		replayTrace.setErrorMessageParams(operationName, outputParamName, bValue, paramValueFromTransition);
 	}
 
 	private void setAlertButtons(boolean isEqual, Trace copyTrace, PersistentTrace persistentTrace, Alert alert) {
@@ -163,74 +192,6 @@ public class TraceChecker {
 			traceDiffStage.show();
 			alert.close();
 		}
-	}
-
-	private Transition replayPersistentTransition(ReplayTrace replayTrace, Trace t,
-			PersistentTransition persistentTransition, boolean setCurrentAnimation) {
-		StateSpace stateSpace = t.getStateSpace();
-		PredicateBuilder predicateBuilder = new PredicateBuilder().addMap(persistentTransition.getParameters());
-		predicateBuilder.addMap(persistentTransition.getDestinationStateVariables());
-		final IEvalElement pred = stateSpace.getModel().parseFormula(predicateBuilder.toString(), FormulaExpand.EXPAND);
-		final GetOperationByPredicateCommand command = new GetOperationByPredicateCommand(stateSpace,
-				t.getCurrentState().getId(), persistentTransition.getOperationName(), pred, 1);
-		stateSpace.execute(command);
-		if (command.hasErrors()) {
-			replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage");
-			replayTrace.setErrorMessageParams(persistentTransition.getOperationName(), predicateBuilder, String.join(", ", command.getErrors()));
-			return null;
-		}
-		List<Transition> possibleTransitions = command.getNewTransitions();
-		if (possibleTransitions.isEmpty()) {
-			replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage.operationNotPossible");
-			replayTrace.setErrorMessageParams(persistentTransition.getOperationName(), predicateBuilder);
-			return null;
-		}
-		Transition trans = possibleTransitions.get(0);
-		if (!checkOutputParams(replayTrace, trans, persistentTransition, setCurrentAnimation)) {
-			return null;
-		}
-		return trans;
-	}
-
-	private boolean checkOutputParams(ReplayTrace replayTrace, Transition trans,
-			PersistentTransition persistentTransition, boolean setCurrentAnimation) {
-		String operationName = trans.getName();
-		OperationInfo machineOperationInfo = trans.getStateSpace().getLoadedMachine().getMachineOperationInfo(operationName);
-		final Map<String, String> ouputParameters = persistentTransition.getOuputParameters();
-		if (machineOperationInfo != null && ouputParameters != null) {
-			List<String> outputParameterNames = machineOperationInfo.getOutputParameterNames();
-			try {
-				List<BValue> translatedReturnValues = trans.getTranslatedReturnValues();
-				for (int i = 0; i < outputParameterNames.size(); i++) {
-					String outputParamName = outputParameterNames.get(i);
-					BValue paramValueFromTransition = translatedReturnValues.get(i);
-					if (ouputParameters.containsKey(outputParamName)) {
-						String stringValue = ouputParameters.get(outputParamName);
-						BValue bValue = Translator.translate(stringValue);
-						if (!bValue.equals(paramValueFromTransition)) {
-							// do we need further checks here?
-							// because the value translator does not
-							// support enum values properly
-							if (setCurrentAnimation) {
-								replayTrace.setErrorMessageBundleKey("animation.tracereplay.traceChecker.errorMessage.mismatchingOutputValues");
-								replayTrace.setErrorMessageParams(operationName, outputParamName, bValue.toString(), paramValueFromTransition);
-							}
-							return false;
-						}
-					}
-
-				}
-			} catch (TranslationException e) {
-				Platform.runLater(
-						() -> stageManager
-								.makeExceptionAlert(e,
-										"animation.tracereplay.alerts.traceReplayError.header",
-										"animation.tracereplay.alerts.traceReplayError.content")
-								.showAndWait());
-				return false;
-			}
-		}
-		return true;
 	}
 
 	void cancelReplay() {
