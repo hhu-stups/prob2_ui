6a4c8eca505d3225e94b9134a366550b9e4bb4e1
Michael Leuschel <leuschel@uni-duesseldorf.de>
2020-09-19 09:39:30 +0200

    moved Open VisB command to Visualisation menu

--- src/main/java/de/prob2/ui/menu/AdvancedMenu.java
+++ src/main/java/de/prob2/ui/menu/AdvancedMenu.java
@@ -9,7 +9,7 @@ import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.output.PrologOutputStage;
 import de.prob2.ui.plugin.PluginMenuStage;
 import de.prob2.ui.plugin.ProBPluginManager;
-import de.prob2.ui.visb.VisBStage;
+//import de.prob2.ui.visb.VisBStage;
 import de.prob2.ui.visualisation.fx.VisualisationController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
@@ -95,14 +95,6 @@ public class AdvancedMenu extends Menu {
 	void detachVisualisation() {
 		LOGGER.debug("Detach menu-item called.");
 		visualisationController.detachVisualisation();
-	}
-
-	@FXML
-	void openVisB(){
-		LOGGER.debug("Open Visb.");
-		final Stage visBStage = injector.getInstance(VisBStage.class);
-		visBStage.show();
-		visBStage.toFront();
 	}
 
 }
