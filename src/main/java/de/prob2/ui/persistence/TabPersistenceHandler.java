f343bb895c6c69b31120340ab69c57a718308bd6
dgelessus <dgelessus@users.noreply.github.com>
2020-09-28 18:52:04 +0200

    Replace property and listeners in TabPersistenceHandler
    There's no need to store the current tab ID in a property - it can
    always be easily retrieved from the tab pane, and when the current tab
    ID is changed, the tab in question can be selected directly. This way
    it's no longer necessary to keep the property and the tab selection
    in sync manually.

--- src/main/java/de/prob2/ui/persistence/TabPersistenceHandler.java
+++ src/main/java/de/prob2/ui/persistence/TabPersistenceHandler.java
@@ -2,9 +2,6 @@ package de.prob2.ui.persistence;
 
 import java.util.HashMap;
 
-
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
@@ -12,33 +9,22 @@ public class TabPersistenceHandler {
 	
 	private final TabPane pane;
 	
-	private final StringProperty currentTab;
-	
 	private final HashMap<String, Tab> tabMap;
 
 	public TabPersistenceHandler(TabPane pane) {
 		this.pane = pane;
-		this.currentTab = new SimpleStringProperty(this, "currentTab", null);
-		this.pane.getSelectionModel().selectedItemProperty().addListener((observable, from, to) -> this.setCurrentTab(to.getId()));
-		this.setCurrentTab(this.pane.getSelectionModel().getSelectedItem().getId());
 		this.tabMap = new HashMap<>();
 		
 		for(Tab tab : pane.getTabs()) {
 			tabMap.put(tab.getId(), tab);
 		}
-		this.currentTabProperty().addListener((observable, from, to) -> this.pane.getSelectionModel().select(tabMap.get(to)));
-	}
-	
-	public StringProperty currentTabProperty() {
-		return this.currentTab;
 	}
 	
 	public String getCurrentTab() {
-		return this.currentTabProperty().get();
+		return this.pane.getSelectionModel().getSelectedItem().getId();
 	}
 	
 	public void setCurrentTab(String tab) {
-		this.currentTabProperty().set(tab);
+		this.pane.getSelectionModel().select(tabMap.get(tab));
 	}
-	
 }
