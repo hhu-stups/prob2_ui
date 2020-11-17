5e8d90674f5136635fa79fb7baebf18250c0ee5c
favu100 <Fabian.Vu@hhu.de>
2020-11-17 16:19:15 +0100

    Implement automatic simulation

--- src/main/java/de/prob2/ui/simulation/Simulator.java
+++ src/main/java/de/prob2/ui/simulation/Simulator.java
@@ -1,14 +1,63 @@
 package de.prob2.ui.simulation;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.prob.statespace.Trace;
+import de.prob2.ui.prob2fx.CurrentTrace;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Singleton
 public class Simulator {
 
-	private int timer; // in ms
+	private Timer timer;
 
-	public Simulator(int timer) {
-		this.timer = timer;
+	private int interval; // in ms
+
+	private final CurrentTrace currentTrace;
+
+	private final BooleanProperty runningProperty;
+
+	@Inject
+	public Simulator(final CurrentTrace currentTrace) {
+		this.currentTrace = currentTrace;
+		this.runningProperty = new SimpleBooleanProperty(false);
 	}
 
-	public void setTimer(int timer) {
-		this.timer = timer;
+	public void initSimulator(int interval) {
+		this.interval = interval;
+		this.timer = new Timer();
+	}
+
+	public void setInterval(int interval) {
+		this.interval = interval;
+	}
+
+	public void run() {
+		runningProperty.set(true);
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				Trace trace = currentTrace.get().randomAnimation(1);
+				currentTrace.set(trace);
+			}
+		};
+		timer.schedule(task, interval,1000);
+	}
+
+	public void stop() {
+		timer.cancel();
+		runningProperty.set(false);
+	}
+
+	public BooleanProperty runningPropertyProperty() {
+		return runningProperty;
+	}
+
+	public boolean isRunning() {
+		return runningProperty.get();
 	}
 }
--- src/main/java/de/prob2/ui/simulation/SimulatorStage.java
+++ src/main/java/de/prob2/ui/simulation/SimulatorStage.java
@@ -3,15 +3,51 @@ package de.prob2.ui.simulation;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import de.prob2.ui.internal.StageManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 
 @Singleton
 public class SimulatorStage extends Stage {
 
+	@FXML
+	private TextField tfTimer;
+
+	@FXML
+	private Button btSimulate;
+
+	private final Simulator simulator;
+
 	@Inject
-	public SimulatorStage(final StageManager stageManager) {
+	public SimulatorStage(final StageManager stageManager, final Simulator simulator) {
+		this.simulator = simulator;
 		stageManager.loadFXML(this, "simulator_stage.fxml");
+	}
+
+	@FXML
+	public void initialize() {
+		simulator.runningPropertyProperty().addListener((observable, from, to) -> {
+			if(to) {
+				btSimulate.setText("Stop");
+			} else {
+				btSimulate.setText("Start");
+			}
+		});
+	}
+
+
+	@FXML
+	public void simulate() {
+		if(!simulator.isRunning()) {
+			int timer = Integer.parseInt(tfTimer.getText());
+			simulator.initSimulator(timer);
+			simulator.run();
+		} else {
+			simulator.stop();
+		}
 	}
 
 }
