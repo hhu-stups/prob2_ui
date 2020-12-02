02ea023957bb1564a2d730dcae78678bd2893620
Yumiko Jansing <yumiko.takahashi@uni-duesseldorf.de>
2020-12-02 16:44:40 +0100

    Error message will now be wrapped

--- src/main/java/de/prob2/ui/error/MessageCell.java
+++ src/main/java/de/prob2/ui/error/MessageCell.java
@@ -6,6 +6,7 @@ import de.prob.animator.domainobjects.ErrorItem;
 
 import javafx.scene.control.OverrunStyle;
 import javafx.scene.control.TreeTableCell;
+import javafx.scene.text.Text;
 
 final class MessageCell extends TreeTableCell<Object, Object> {
 	@Inject
@@ -14,15 +15,27 @@ final class MessageCell extends TreeTableCell<Object, Object> {
 	@Override
 	protected void updateItem(final Object item, final boolean empty) {
 		super.updateItem(item, empty);
-		
+
 		if (empty) {
 			this.setText(null);
+			this.setGraphic(null);
 		} else if (item instanceof String) {
 			this.setText((String)item);
 			this.setTextOverrun(OverrunStyle.LEADING_ELLIPSIS);
+			this.setGraphic(null);
+			// TODO Wrap text here to?
+			/*Text text = new Text((String) item);
+			text.wrappingWidthProperty().bind(this.getTableColumn().widthProperty());
+			this.setWrapText(true);
+			this.setGraphic(text);*/
 		} else if (item instanceof ErrorItem) {
-			this.setText(((ErrorItem)item).getMessage());
-			this.setTextOverrun(OverrunStyle.CENTER_ELLIPSIS);
+			/*this.setText(((ErrorItem)item).getMessage());
+			this.setTextOverrun(OverrunStyle.CENTER_ELLIPSIS);*/
+			this.setText(null);
+			Text text = new Text(((ErrorItem) item).getMessage());
+			text.wrappingWidthProperty().bind(this.getTableColumn().widthProperty().subtract(5));
+			this.setWrapText(true);
+			this.setGraphic(text);
 		} else {
 			throw new AssertionError("Invalid table element type: " + item.getClass());
 		}
