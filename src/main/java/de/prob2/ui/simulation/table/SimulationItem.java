9893aa44c62cedc46f73b5a358750557266704bb
favu100 <Fabian.Vu@hhu.de>
2020-12-08 17:19:13 +0100

    Update SimulationItem

--- src/main/java/de/prob2/ui/simulation/table/SimulationItem.java
+++ src/main/java/de/prob2/ui/simulation/table/SimulationItem.java
@@ -2,7 +2,11 @@ package de.prob2.ui.simulation.table;
 
 public class SimulationItem {
 
-    // Timing: starting condition, ending time, ending condition
+    // Timing: needs simulation with ending condition, user can check timing value (playable)
+	// Model Checking: uses B machine + simulation configuration to apply model checking on a reduced state space (counter-examples playable)
+	// Probabilistic Model Checking: Same as Model Checking, carries probability (counter-examples playable)
+	// Hypothesis Test: Number of Simulations, Store traces with result, Apply different kind of hypothesis test on results (playable)
+	// Trace Replay: requires a trace (playable)
 
     /*
     starting condition to start timer for timing - also part of configuration file to search for starting state of simulation
@@ -10,7 +14,7 @@ public class SimulationItem {
     */
 
     public enum SimulationType {
-        STANDARD, TIMING, MODEL_CHECKING, PROBABILISTIC_MODEL_CHECKING, HYPOTHESIS_TEST, TRACE_REPLAY
+        TIMING, MODEL_CHECKING, PROBABILISTIC_MODEL_CHECKING, HYPOTHESIS_TEST, TRACE_REPLAY
     }
 
     private SimulationType type;
