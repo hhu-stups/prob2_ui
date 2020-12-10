f488e41727b5b4e83a0605971e0851b869e69cfa
SeeBasTStick <sebastian.stock@hhu.de>
2020-12-10 14:07:34 +0100

    refactoring for init and the case were no solutions is found

--- src/main/java/de/prob2/ui/animation/tracereplay/TraceModificationChecker.java
+++ src/main/java/de/prob2/ui/animation/tracereplay/TraceModificationChecker.java
@@ -12,7 +12,10 @@ import de.prob.scripting.ModelTranslationError;
 import de.prob.statespace.StateSpace;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentProject;
+import de.prob2.ui.prob2fx.CurrentTrace;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -25,20 +28,18 @@ public class TraceModificationChecker {
 	final TraceChecker traceChecker;
 	private final TraceJsonFile traceJsonFile;
 	private final PersistentTrace persistentTrace;
-	private final StateSpace stateSpace;
 	private final Injector injector;
 	private final CurrentProject currentProject;
 	private final StageManager stageManager;
 	private final TraceManager traceManager;
 	private final Path path;
 
-	public TraceModificationChecker(TraceManager traceManager,Path traceJsonFilePath, StateSpace stateSpace,
+	public TraceModificationChecker(TraceManager traceManager, Path traceJsonFilePath, StateSpace stateSpace,
 									Injector injector, CurrentProject currentProject, StageManager stageManager) throws IOException, ModelTranslationError {
 		this.path = traceJsonFilePath;
 		this.traceManager = traceManager;
 		traceJsonFile = traceManager.load(traceJsonFilePath);
 		this.persistentTrace = traceJsonFile.getTrace();
-		this.stateSpace = stateSpace;
 		this.injector = injector;
 		this.currentProject = currentProject;
 		this.stageManager = stageManager;
@@ -46,10 +47,17 @@ public class TraceModificationChecker {
 
 		Path oldPath = currentProject.getLocation().resolve(Paths.get( ((TraceMetaData) traceJsonFile.getMetaData()).getPath()));
 		Path newPath = currentProject.getLocation().resolve(currentProject.getCurrentMachine().getLocation());
-		traceChecker = new TraceChecker(persistentTrace,
-				new HashMap<>(traceJsonFile.getMachineOperationInfos()),
-				new HashMap<>(stateSpace.getLoadedMachine().getOperations()), new HashSet<>(traceJsonFile.getVariableNames()),
-				new HashSet<>(stateSpace.getLoadedMachine().getVariableNames()), oldPath.toString(), newPath.toString(), injector);
+
+		if(java.nio.file.Files.exists(path) && newPath!=oldPath) {
+			traceChecker = new TraceChecker(persistentTrace.getTransitionList(),
+					new HashMap<>(traceJsonFile.getMachineOperationInfos()),
+					new HashMap<>(stateSpace.getLoadedMachine().getOperations()), new HashSet<>(traceJsonFile.getVariableNames()),
+					new HashSet<>(stateSpace.getLoadedMachine().getVariableNames()), oldPath.toString(), newPath.toString(), injector);
+		}else {
+			traceChecker = new TraceChecker(persistentTrace.getTransitionList(),
+					new HashMap<>(traceJsonFile.getMachineOperationInfos()),
+					new HashSet<>(stateSpace.getLoadedMachine().getVariableNames()), newPath.toString(), injector);
+		}
 
 	}
 
@@ -58,33 +66,50 @@ public class TraceModificationChecker {
 	public List<Path> checkTrace(){
 
 		final List<Path> result = new ArrayList<>();
-		result.add(path);
 
-		TraceModificationAlert dialog = new TraceModificationAlert(injector, stageManager, this);
-		stageManager.register(dialog);
+		if(traceChecker.getTraceModifier().isDirty()) {
+
+			final List<PersistentTrace> persistentTraces = new ArrayList<>();
 
 
-		Optional<List<PersistentTrace>> bla = dialog.showAndWait();
-
-		final List<PersistentTrace> persistentTraces = new ArrayList<>(bla.get());
-
-		persistentTraces.remove(persistentTrace);
-
-		persistentTraces.forEach(element -> {
-
-			String filename = Files.getNameWithoutExtension(path.toString());
-			String modified = filename + "_edited." + Files.getFileExtension(path.toString());
-
-			Path saveAt = currentProject.getLocation().resolve(Paths.get(modified));
-
-			result.add(saveAt);
-			try {
-				traceManager.save(saveAt,
-						traceJsonFile.changeTrace(element));
-			} catch (IOException e) {
-				e.printStackTrace();
+			if(traceChecker.getTraceModifier().getChangelogPhase3II().isEmpty()) {
+				ResourceBundle resourceBundle = injector.getInstance(ResourceBundle.class);
+				Alert alert = new Alert(Alert.AlertType.CONFIRMATION, resourceBundle.getString("traceModification.alert.traceNotReplayable") , ButtonType.YES, ButtonType.NO);
+				alert.setTitle("No suitable configuration found.");
+				Optional<ButtonType> dialogResult = alert.showAndWait();
+				if(dialogResult.get()==ButtonType.YES){
+					result.add(path);
+				}
+			}else {
+				TraceModificationAlert dialog = new TraceModificationAlert(injector, stageManager, this, persistentTrace);
+				stageManager.register(dialog);
+				Optional<List<PersistentTrace>> dialogResult = dialog.showAndWait();
+				persistentTraces.addAll(dialogResult.get());
 			}
-		});
+
+
+			if (persistentTraces.remove(persistentTrace)) {
+				result.add(path);
+			}
+
+			persistentTraces.forEach(element -> {
+
+				String filename = Files.getNameWithoutExtension(path.toString());
+				String modified = filename + "_edited_for_" + currentProject.getCurrentMachine().getName() +"." + Files.getFileExtension(path.toString());
+
+				Path saveAt = currentProject.getLocation().resolve(Paths.get(modified));
+
+				result.add(saveAt);
+				try {
+					traceManager.save(saveAt,
+							traceJsonFile.changeTrace(element));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}else{
+			result.add(path);
+		}
 
 		return result;
 
