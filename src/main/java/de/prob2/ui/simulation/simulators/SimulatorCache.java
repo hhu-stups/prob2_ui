b819b3a94a40cb78e27c378737f5928abcaed201
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-01-26 17:30:00 +0100

    Refactor SimulatorCache

--- src/main/java/de/prob2/ui/simulation/simulators/SimulatorCache.java
+++ src/main/java/de/prob2/ui/simulation/simulators/SimulatorCache.java
@@ -12,74 +12,45 @@ import java.util.stream.Collectors;
 
 public class SimulatorCache {
 
-    private final Map<String, Map<String, Double>> probabilityCache = new HashMap<>();
-
     private final Map<String, Map<String, String>> valuesCache = new HashMap<>();
 
     private final Map<String, Map<String, List<Transition>>> transitionCache = new HashMap<>();
 
-    public double readProbabilityWithCaching(State bState, String opName, String probability) {
-        String stateID = bState.getId();
-
-        double opProbability;
-        if(probabilityCache.containsKey(stateID)) {
-            if(probabilityCache.get(stateID).containsKey(opName)) {
-                opProbability = probabilityCache.get(stateID).get(opName);
-            } else {
-                AbstractEvalResult evalResult = SimulationHelperFunctions.evaluateForSimulation(bState, probability);
-                opProbability = Double.parseDouble(evalResult.toString());
-                probabilityCache.get(stateID).put(opName, opProbability);
-            }
-        } else {
-            probabilityCache.put(stateID, new HashMap<>());
-            AbstractEvalResult evalResult = SimulationHelperFunctions.evaluateForSimulation(bState, probability);
-            opProbability = Double.parseDouble(evalResult.toString());
-            probabilityCache.get(stateID).put(opName, opProbability);
-        }
-        return opProbability;
-    }
-
     public String readValueWithCaching(State bState, String expression) {
         String stateID = bState.getId();
         String value;
-        if(valuesCache.containsKey(stateID)) {
-            if(valuesCache.get(stateID).containsKey(expression)) {
-                value = valuesCache.get(stateID).get(expression);
-            } else {
-                AbstractEvalResult evalResult = SimulationHelperFunctions.evaluateForSimulation(bState, expression);
-                value = evalResult.toString();
-                valuesCache.get(stateID).put(expression, value);
+
+        if(!valuesCache.containsKey(stateID) || !valuesCache.get(stateID).containsKey(expression)) {
+            // loads values in cache if necessary
+            if(!valuesCache.containsKey(stateID)) {
+                valuesCache.put(stateID, new HashMap<>());
             }
-        } else {
-            valuesCache.put(stateID, new HashMap<>());
             AbstractEvalResult evalResult = SimulationHelperFunctions.evaluateForSimulation(bState, expression);
             value = evalResult.toString();
             valuesCache.get(stateID).put(expression, value);
         }
-        return value;
-    }
-
-    private void loadTransitionsInCache(State currentState, String opName) {
-        String stateID = currentState.getId();
-        if(!transitionCache.containsKey(stateID)) {
-            transitionCache.put(stateID, new HashMap<>());
-        }
-        transitionCache.get(stateID).put(opName, currentState.getOutTransitions().stream()
-                .filter(trans -> trans.getName().equals(opName))
-                .collect(Collectors.toList()));
+        // finally get value from cache
+        return valuesCache.get(stateID).get(expression);
     }
 
     public List<Transition> readTransitionsWithCaching(State bState, String opName) {
         String stateID = bState.getId();
-
         if(!transitionCache.containsKey(stateID) || !transitionCache.get(stateID).containsKey(opName)) {
-            loadTransitionsInCache(bState, opName);
+            // loads transitions in cache if necessary
+            if(!transitionCache.containsKey(stateID)) {
+                transitionCache.put(stateID, new HashMap<>());
+            }
+            transitionCache.get(stateID).put(opName, bState.getOutTransitions().stream()
+                    .filter(trans -> trans.getName().equals(opName))
+                    .collect(Collectors.toList()));
         }
-
-        return transitionCache.get(bState.getId()).get(opName);
+        // finally get transitions from cache
+        return transitionCache.get(stateID).get(opName);
     }
 
-	public Map<String, Map<String, List<Transition>>> getTransitionCache() {
-		return transitionCache;
-	}
+    public void clear() {
+        valuesCache.clear();
+        transitionCache.clear();
+    }
+
 }
