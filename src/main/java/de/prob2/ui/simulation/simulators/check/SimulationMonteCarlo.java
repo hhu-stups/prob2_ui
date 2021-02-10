3369bc41e36aca3f9c857a02777362bf59a962c2
Vu, Fabian (favu100) <fabian.vu@uni-duesseldorf.de>
2021-02-10 17:11:14 +0100

    Fix a bug in SimulationMonteCarlo

--- src/main/java/de/prob2/ui/simulation/simulators/check/SimulationMonteCarlo.java
+++ src/main/java/de/prob2/ui/simulation/simulators/check/SimulationMonteCarlo.java
@@ -77,7 +77,7 @@ public class SimulationMonteCarlo extends Simulator {
 			return "TRUE".equals(evalResult);
 		} else if(additionalInformation.containsKey("ENDING_TIME")) {
 			int endingTime = (int) additionalInformation.get("ENDING_TIME");
-			return endingTime >= time.get();
+			return time.get() >= endingTime;
 		}
 		return false;
 	}
