fa7a275c23c9c580e31058ba83115fd5cf083818
dgelessus <dgelessus@users.noreply.github.com>
2021-07-02 14:38:38 +0200

    Use standard message field instead of custom one in VisBException

--- src/main/java/de/prob2/ui/visb/exceptions/VisBException.java
+++ src/main/java/de/prob2/ui/visb/exceptions/VisBException.java
@@ -3,13 +3,7 @@ package de.prob2.ui.visb.exceptions;
 public class VisBException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	private String message;
-
 	public VisBException(String message){
-		this.message = message;
-	}
-
-	public String getMessage(){
-		return message;
+		super(message);
 	}
 }
