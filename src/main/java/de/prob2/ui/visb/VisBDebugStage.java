67fd9b2afd4da00152793a5afb67b53d324a9ab3
dgelessus <dgelessus@users.noreply.github.com>
2021-07-09 17:28:07 +0200

    Simplify map of items to events in VisBDebugStage
    When building the map, there's no need to check that an item with the
    same ID as the event exists - the only IDs that are looked up in the
    map are item IDs.

--- src/main/java/de/prob2/ui/visb/VisBDebugStage.java
+++ src/main/java/de/prob2/ui/visb/VisBDebugStage.java
@@ -1,7 +1,14 @@
 package de.prob2.ui.visb;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 import com.google.inject.Injector;
+
 import de.prob.animator.domainobjects.VisBEvent;
 import de.prob.animator.domainobjects.VisBHover;
 import de.prob.animator.domainobjects.VisBItem;
@@ -11,22 +18,15 @@ import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.visb.ui.ListViewEvent;
 import de.prob2.ui.visb.ui.ListViewItem;
 import de.prob2.ui.visb.visbobjects.VisBVisualisation;
+
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
-
 import static de.prob2.ui.internal.JavascriptFunctionInvoker.buildInvocation;
 import static de.prob2.ui.internal.JavascriptFunctionInvoker.wrapAsString;
-
 
 @Singleton
 public class VisBDebugStage extends Stage {
@@ -48,7 +48,7 @@ public class VisBDebugStage extends Stage {
 	@FXML
 	private ListView<VisBEvent> visBEvents;
 
-	private final Map<String, VisBEvent> itemsToEvent;
+	private final Map<String, VisBEvent> eventsById;
 
 	@Inject
 	public VisBDebugStage(final StageManager stageManager, final CurrentTrace currentTrace, final CurrentProject currentProject, final ResourceBundle bundle, final VisBController visBController, final Injector injector) {
@@ -59,7 +59,7 @@ public class VisBDebugStage extends Stage {
 		this.bundle = bundle;
 		this.visBController = visBController;
 		this.injector = injector;
-		this.itemsToEvent = new HashMap<>();
+		this.eventsById = new HashMap<>();
 		this.stageManager.loadFXML(this, "visb_debug_stage.fxml");
 	}
 
@@ -75,7 +75,7 @@ public class VisBDebugStage extends Stage {
 				applyHighlighting(to);
 			}
 		};
-		this.visBItems.setCellFactory(lv -> new ListViewItem(stageManager, currentTrace, bundle, injector, itemsToEvent));
+		this.visBItems.setCellFactory(lv -> new ListViewItem(stageManager, currentTrace, bundle, injector, eventsById));
 		this.visBEvents.setCellFactory(lv -> new ListViewEvent(stageManager, bundle, injector));
 		this.currentTrace.addListener((observable, from, to) -> refresh());
 		this.currentProject.currentMachineProperty().addListener((observable, from, to) -> refresh());
@@ -85,8 +85,8 @@ public class VisBDebugStage extends Stage {
 
 	private void removeHighlighting(VisBItem item) {
 		String id = item.getId();
-		if(itemsToEvent.containsKey(id)) {
-			for (VisBHover hover : itemsToEvent.get(id).getHovers()) {
+		if(eventsById.containsKey(id)) {
+			for (VisBHover hover : eventsById.get(id).getHovers()) {
 				String invocation = buildInvocation("changeAttribute", wrapAsString(hover.getHoverID()), wrapAsString(hover.getHoverAttr()), wrapAsString(hover.getHoverLeaveVal()));
 				injector.getInstance(VisBStage.class).runScript(invocation);
 			}
@@ -95,8 +95,8 @@ public class VisBDebugStage extends Stage {
 
 	private void applyHighlighting(VisBItem item) {
 		String id = item.getId();
-		if(itemsToEvent.containsKey(id)) {
-			for (VisBHover hover : itemsToEvent.get(id).getHovers()) {
+		if(eventsById.containsKey(id)) {
+			for (VisBHover hover : eventsById.get(id).getHovers()) {
 				String invocation = buildInvocation("changeAttribute", wrapAsString(hover.getHoverID()), wrapAsString(hover.getHoverAttr()), wrapAsString(hover.getHoverEnterVal()));
 				injector.getInstance(VisBStage.class).runScript(invocation);
 			}
@@ -112,16 +112,8 @@ public class VisBDebugStage extends Stage {
 		if (visBVisualisation != null) {
 			this.visBEvents.setItems(FXCollections.observableArrayList(visBVisualisation.getVisBEvents()));
 			this.visBItems.setItems(FXCollections.observableArrayList(visBVisualisation.getVisBItems()));
-			fillItemsToEvent(visBVisualisation.getVisBItems(), visBVisualisation.getVisBEvents());
-		}
-	}
-
-	private void fillItemsToEvent(List<VisBItem> visBItems, List<VisBEvent> visBEvents) {
-		for(VisBItem item : visBItems) {
-			for(VisBEvent event : visBEvents) {
-				if(item.getId().equals(event.getId())) {
-					itemsToEvent.put(item.getId(), event);
-				}
+			for (final VisBEvent event : visBVisualisation.getVisBEvents()) {
+				eventsById.put(event.getId(), event);
 			}
 		}
 	}
@@ -129,7 +121,7 @@ public class VisBDebugStage extends Stage {
 	public void clear(){
 		this.visBEvents.setItems(null);
 		this.visBItems.setItems(null);
-		this.itemsToEvent.clear();
+		this.eventsById.clear();
 	}
 
 	private void refresh() {
