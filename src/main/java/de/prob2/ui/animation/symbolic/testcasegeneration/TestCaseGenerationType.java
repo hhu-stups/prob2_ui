0e31e2fd556cac9771a233a66233a2739dcff67a
dgelessus <dgelessus@users.noreply.github.com>
2021-08-16 14:55:02 +0200

    Remove outdated comment from TestCaseGenerationType

--- src/main/java/de/prob2/ui/animation/symbolic/testcasegeneration/TestCaseGenerationType.java
+++ src/main/java/de/prob2/ui/animation/symbolic/testcasegeneration/TestCaseGenerationType.java
@@ -1,9 +1,6 @@
 package de.prob2.ui.animation.symbolic.testcasegeneration;
 
 public enum TestCaseGenerationType {
-	// Note: When adding/changing/removing enum constants here,
-	// make sure to also adjust TestCaseGenerationItem,
-	// especially the internal class TestCaseGenerationItem.AdditionalInformation and its subclasses.
 	MCDC("MCDC Testing"),
 	COVERED_OPERATIONS("Covered Operations Testing")
 	;
