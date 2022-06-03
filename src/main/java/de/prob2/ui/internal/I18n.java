0ab491202eb75375a3d76d5d2647ac10ec21488c
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2022-06-03 12:28:09 +0200

    Preparation for locale unification

--- src/main/java/de/prob2/ui/internal/I18n.java
+++ src/main/java/de/prob2/ui/internal/I18n.java
@@ -1,9 +1,11 @@
 package de.prob2.ui.internal;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import com.google.inject.Inject;
+import com.google.inject.Injector;
 import com.google.inject.Singleton;
 
 import org.slf4j.Logger;
@@ -14,16 +16,44 @@ public final class I18n {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(I18n.class);
 
-	private final ResourceBundle bundle;
+	private final Injector injector;
+	private volatile ResourceBundle bundle;
 
 	@Inject
-	private I18n(ResourceBundle bundle) {
-		this.bundle = bundle;
+	private I18n(Injector injector/*, UIState uiState*/) {
+		this.injector = injector;
+
+		// this code allows changing the locale at runtime
+		/*uiState.localeOverrideProperty().addListener((observable, oldValue, newValue) -> {
+			if (oldValue != newValue) {
+				Locale.setDefault(newValue);
+				reset();
+			}
+		});*/
+	}
+
+	private synchronized void reset() {
+		this.bundle = null;
+	}
+
+	private synchronized void init() {
+		if (this.bundle == null) {
+			this.bundle = ResourceBundle.getBundle("de.prob2.ui.prob2", locale());
+		}
+	}
+
+	private Locale locale() {
+		return injector.getInstance(Locale.class);
+	}
+
+	public synchronized ResourceBundle bundle() {
+		init();
+		return bundle;
 	}
 
 	public String translate(String key) {
 		try {
-			return bundle.getString(key);
+			return bundle().getString(key);
 		} catch (Exception e) {
 			LOGGER.error("Error while translating key '{}'", key, e);
 			return key;
--- src/main/java/de/prob2/ui/internal/ProB2Module.java
+++ src/main/java/de/prob2/ui/internal/ProB2Module.java
@@ -1,7 +1,15 @@
 package de.prob2.ui.internal;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.*;
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
 import de.codecentric.centerdevice.MenuToolkit;
 import de.prob.MainModule;
 import de.prob2.ui.ProB2;
@@ -9,47 +17,39 @@ import de.prob2.ui.config.RuntimeOptions;
 import de.prob2.ui.menu.RevealInExplorer;
 import de.prob2.ui.visualisation.magiclayout.MagicGraphFX;
 import de.prob2.ui.visualisation.magiclayout.MagicGraphI;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.JavaFXBuilderFactory;
 import javafx.util.BuilderFactory;
-
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 public class ProB2Module extends AbstractModule {
 
 	public static final boolean IS_MAC = System.getProperty("os.name", "").toLowerCase(Locale.ROOT).contains("mac");
 	public static final boolean IS_WINDOWS = System.getProperty("os.name", "").toLowerCase(Locale.ROOT).contains("win");
 
+	private static final BuilderFactory javafxDefaultBuilderFactory = new JavaFXBuilderFactory();
+
 	private final ProB2 application;
 	private final RuntimeOptions runtimeOptions;
-	private final BuilderFactory javafxDefaultBuilderFactory;
 
 	public ProB2Module(final ProB2 application, final RuntimeOptions runtimeOptions) {
 		super();
 		this.application = application;
 		this.runtimeOptions = runtimeOptions;
-		this.javafxDefaultBuilderFactory = new JavaFXBuilderFactory();
-	}
-
-	@Override
-	protected void configure() {
-		install(new MainModule());
-		install(new DataPathsModule());
-
-		bind(Locale.class).toProvider(Locale::getDefault);
-		bind(Application.class).toInstance(this.application);
-		bind(ProB2.class).toInstance(this.application);
-		bind(RuntimeOptions.class).toInstance(this.runtimeOptions);
-
-		bind(MagicGraphI.class).to(MagicGraphFX.class);
 	}
 
 	@Provides
-	@Singleton
-	private static ResourceBundle provideResourceBundle(final Locale locale) {
-		return ResourceBundle.getBundle("de.prob2.ui.prob2", locale);
+	private static Locale locale(/*UIState uiState*/) {
+		return Locale.getDefault();
+		// this code allows changing the locale at runtime
+		/*Locale localeOverride = uiState.getLocaleOverride();
+		return localeOverride != null ? localeOverride : Locale.getDefault();*/
+	}
+
+	@Provides
+	private static ResourceBundle resourceBundle(I18n i18n) {
+		return i18n.bundle();
 	}
 
 	@Provides
@@ -75,7 +75,12 @@ public class ProB2Module extends AbstractModule {
 	}
 
 	@Provides
-	public FXMLLoader provideLoader(final Injector injector, ResourceBundle bundle) {
+	private static ObjectMapper provideObjectMapper() {
+		return new ObjectMapper().registerModule(new ProB2UIJacksonModule());
+	}
+
+	@Provides
+	private static FXMLLoader provideLoader(final Injector injector, ResourceBundle bundle) {
 		FXMLLoader fxmlLoader = new FXMLLoader();
 		fxmlLoader.setBuilderFactory(type -> {
 			if (injector.getExistingBinding(Key.get(type)) != null || type.isAnnotationPresent(FXMLInjected.class)) {
@@ -89,8 +94,15 @@ public class ProB2Module extends AbstractModule {
 		return fxmlLoader;
 	}
 
-	@Provides
-	private static ObjectMapper provideObjectMapper() {
-		return new ObjectMapper().registerModule(new ProB2UIJacksonModule());
+	@Override
+	protected void configure() {
+		install(new MainModule());
+		install(new DataPathsModule());
+
+		bind(Application.class).toInstance(this.application);
+		bind(ProB2.class).toInstance(this.application);
+		bind(RuntimeOptions.class).toInstance(this.runtimeOptions);
+
+		bind(MagicGraphI.class).to(MagicGraphFX.class);
 	}
 }
