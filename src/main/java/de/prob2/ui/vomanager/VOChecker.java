87471c7fec457330cc05d88def132e8ca270eff7
Fabian Vu <Fabian.Vu@hhu.de>
2022-06-23 09:58:22 +0200

    Adapt to removement of AbstractVOInterpreter in probparsers/voparser

--- src/main/java/de/prob2/ui/vomanager/VOChecker.java
+++ src/main/java/de/prob2/ui/vomanager/VOChecker.java
@@ -3,9 +3,9 @@ package de.prob2.ui.vomanager;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
-import de.prob.voparser.AbstractVOInterpreter;
 import de.prob.voparser.VOParseException;
 import de.prob.voparser.VOParser;
+import de.prob.voparser.VTType;
 import de.prob.voparser.node.AAndVo;
 import de.prob.voparser.node.AEquivalentVo;
 import de.prob.voparser.node.AIdentifierVo;
@@ -21,7 +21,6 @@ import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.project.machines.Machine;
 import de.prob2.ui.simulation.SimulationItemHandler;
 import de.prob2.ui.simulation.table.SimulationItem;
-import de.prob2.ui.verifications.Checked;
 import de.prob2.ui.verifications.ltl.formula.LTLFormulaChecker;
 import de.prob2.ui.verifications.ltl.formula.LTLFormulaItem;
 import de.prob2.ui.verifications.modelchecking.ModelCheckingItem;
@@ -31,7 +30,7 @@ import de.prob2.ui.verifications.symbolicchecking.SymbolicCheckingFormulaItem;
 
 
 @Singleton
-public class VOChecker extends AbstractVOInterpreter {
+public class VOChecker {
 
 	private final CurrentProject currentProject;
 
@@ -47,11 +46,13 @@ public class VOChecker extends AbstractVOInterpreter {
 
 	private final SimulationItemHandler simulationItemHandler;
 
+	private final VOParser voParser;
+
 	@Inject
 	public VOChecker(final CurrentProject currentProject, final RequirementHandler requirementHandler, final Modelchecker modelchecker,
 					 final LTLFormulaChecker ltlChecker, final SymbolicCheckingFormulaHandler symbolicChecker,
 					 final TraceChecker traceChecker, final SimulationItemHandler simulationItemHandler) {
-		super();
+		this.voParser = new VOParser();
 		this.currentProject = currentProject;
 		this.requirementHandler = requirementHandler;
 		this.modelchecker = modelchecker;
@@ -88,85 +89,110 @@ public class VOChecker extends AbstractVOInterpreter {
 		}
 	}
 
-	public void interpretVOExpression(String VO) throws VOParseException {
-		super.interpretVOExpression(VO);
-		Start ast = voParser.parseFormula(VO);
-		interpretVOExpression(ast.getPVo());
+	public void registerTask(String id, VTType type) {
+		voParser.registerTask(id, type);
 	}
 
-	public void interpretVOExpression(PVo VO) {
-		if(VO instanceof AIdentifierVo) {
-			interpretAtomicExpression((AIdentifierVo) VO);
-		} else if(VO instanceof ANotVo) {
-			interpretNotExpression((ANotVo) VO);
-		} else if(VO instanceof AAndVo) {
-			interpretAndExpression((AAndVo) VO);
-		} else if(VO instanceof AOrVo) {
-			interpretOrExpression((AOrVo) VO);
-		} else if(VO instanceof AImpliesVo) {
-			interpretImpliesExpression((AImpliesVo) VO);
-		} else if(VO instanceof AEquivalentVo) {
-			interpretEquivalentExpression((AEquivalentVo) VO);
-		} else if(VO instanceof ASequentialVo) {
-			interpretSequentialExpression((ASequentialVo) VO);
+	public void deregisterTask(String id) {
+		voParser.deregisterTask(id);
+	}
+
+	public void checkVOExpression(ValidationObligation VO) throws VOParseException {
+		String expression = VO.getExpression();
+		Start ast = voParser.parseFormula(expression);
+		voParser.semanticCheck(ast);
+		boolean check = checkVOExpression(ast.getPVo(), VO);
+	}
+
+	public boolean checkVOExpression(PVo ast, ValidationObligation VO) {
+		VO.getTasks().clear();
+		if(ast instanceof AIdentifierVo) {
+			return checkAtomicExpression((AIdentifierVo) ast, VO);
+		} else if(ast instanceof ANotVo) {
+			return checkNotExpression((ANotVo) ast, VO);
+		} else if(ast instanceof AAndVo) {
+			return checkAndExpression((AAndVo) ast, VO);
+		} else if(ast instanceof AOrVo) {
+			return checkOrExpression((AOrVo) ast, VO);
+		} else if(ast instanceof AImpliesVo) {
+			return checkImpliesExpression((AImpliesVo) ast, VO);
+		} else if(ast instanceof AEquivalentVo) {
+			return checkEquivalentExpression((AEquivalentVo) ast, VO);
+		} else if(ast instanceof ASequentialVo) {
+			return checkSequentialExpression((ASequentialVo) ast, VO);
 		} else {
-			throw new RuntimeException("VO expression type is unknown: " + VO.getClass());
+			throw new RuntimeException("VO expression type is unknown: " + ast.getClass());
 		}
 	}
 
-	public void interpretAtomicExpression(AIdentifierVo VO) {
-
+	public boolean checkAtomicExpression(AIdentifierVo ast, ValidationObligation VO) {
+		Machine machine = currentProject.getCurrentMachine();
+		IValidationTask validationTask = machine.getValidationTasks().get(ast.getIdentifierLiteral().getText());
+		VO.getTasks().add(validationTask);
+		return checkVT(validationTask);
 	}
 
-	public void interpretNotExpression(ANotVo VO) {
-
+	public boolean checkNotExpression(ANotVo ast, ValidationObligation VO) {
+		// TODO
+		return false;
 	}
 
-	public void interpretAndExpression(AAndVo VO) {
-
+	public boolean checkAndExpression(AAndVo ast, ValidationObligation VO) {
+		boolean leftRes = checkVOExpression(ast.getLeft(), VO);
+		boolean rightRes = checkVOExpression(ast.getRight(), VO);
+		return leftRes && rightRes;
 	}
 
-	public void interpretOrExpression(AOrVo VO) {
-
+	public boolean checkOrExpression(AOrVo ast, ValidationObligation VO) {
+		// TODO
+		return false;
 	}
 
-	public void interpretImpliesExpression(AImpliesVo VO) {
-
+	public boolean checkImpliesExpression(AImpliesVo ast, ValidationObligation VO) {
+		// TODO
+		return false;
 	}
 
-	public void interpretEquivalentExpression(AEquivalentVo VO) {
-
+	public boolean checkEquivalentExpression(AEquivalentVo ast, ValidationObligation VO) {
+		// TODO
+		return false;
 	}
 
-	public void interpretSequentialExpression(ASequentialVo VO) {
-
+	public boolean checkSequentialExpression(ASequentialVo ast, ValidationObligation VO) {
+		// TODO
+		return false;
 	}
 
 
 	public void checkVO(ValidationObligation validationObligation) {
-		// checkVOExpression(validationObligation.getExpression());
-		// TODO Implement full validation task syntax (not just conjunctions)
-		for (IValidationTask validationTask : validationObligation.getTasks()) {
-			if (validationTask != null && validationTask.getChecked() != Checked.SUCCESS) {
-				checkVT(validationTask);
-			}
+		try {
+			checkVOExpression(validationObligation);
+		} catch (VOParseException e) {
+			e.printStackTrace();
 		}
+		// TODO Implement full validation task syntax (not just conjunctions)
+		//for (IValidationTask validationTask : validationObligation.getTasks()) {
+		//	if (validationTask != null && validationTask.getChecked() != Checked.SUCCESS) {
+		//		checkVT(validationTask);
+		//	}
+		//}
 	}
 
-	public void checkVT(IValidationTask validationTask) {
+	public boolean checkVT(IValidationTask validationTask) {
 		if (validationTask instanceof ModelCheckingItem) {
-			modelchecker.checkItem((ModelCheckingItem)validationTask, false, false);
+			modelchecker.checkItem((ModelCheckingItem) validationTask, false, false);
 		} else if (validationTask instanceof LTLFormulaItem) {
-			ltlChecker.checkFormula((LTLFormulaItem)validationTask);
+			ltlChecker.checkFormula((LTLFormulaItem) validationTask);
 		} else if (validationTask instanceof SymbolicCheckingFormulaItem) {
-			symbolicChecker.handleItem((SymbolicCheckingFormulaItem)validationTask, false);
+			symbolicChecker.handleItem((SymbolicCheckingFormulaItem) validationTask, false);
 		} else if (validationTask instanceof ReplayTrace) {
-			traceChecker.check((ReplayTrace)validationTask, true);
+			traceChecker.check((ReplayTrace) validationTask, true);
 		} else if (validationTask instanceof SimulationItem) {
-			simulationItemHandler.checkItem((SimulationItem)validationTask, false);
+			simulationItemHandler.checkItem((SimulationItem) validationTask, false);
 		} else {
 			throw new AssertionError("Unhandled validation task type: " + validationTask.getClass());
 		}
+		return false;
 	}
 
 }
