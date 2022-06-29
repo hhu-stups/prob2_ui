a1918c110ea5714da83a1ab2c2b2dbb6e13576a0
sebastian <sebastian.stock@jku.at>
2022-06-29 14:39:35 +0200

    Added foundations for history view

---
+++ src/main/java/de/prob2/ui/vomanager/HistoryTable.java
@@ -0,0 +1,44 @@
+package de.prob2.ui.vomanager;
+
+import com.google.inject.Singleton;
+import de.prob2.ui.internal.FXMLInjected;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableView;
+import javafx.stage.Stage;
+
+import javax.swing.text.Element;
+
+@FXMLInjected
+@Singleton
+public class HistoryTable<T> extends Stage {
+
+	@FXML
+	private  TableView<ValidationObligation> table;
+
+	@FXML
+	private  TableColumn<ValidationObligation,String> name;
+
+	@FXML
+	private  TableColumn<ValidationObligation,String>  requirement;
+
+	@FXML
+	private  TableColumn<ValidationObligation,String>  expression;
+
+
+	public HistoryTable(ValidationObligation vo) {
+		System.out.println("bla");
+	}
+
+	@FXML
+	public void initialize() {
+		initTable();
+	}
+
+	private void initTable(){
+
+	}
+
+}
