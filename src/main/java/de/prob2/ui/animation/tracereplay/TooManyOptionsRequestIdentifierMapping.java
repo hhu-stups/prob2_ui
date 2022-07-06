f9c2252ead4f2757eedbb3eb30f9e39956525113
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2022-07-06 21:12:08 +0200

    fix typo to->too

--- src/main/java/de/prob2/ui/animation/tracereplay/TooManyOptionsRequestIdentifierMapping.java
+++ src/main/java/de/prob2/ui/animation/tracereplay/TooManyOptionsRequestIdentifierMapping.java
@@ -1,22 +1,32 @@
 package de.prob2.ui.animation.tracereplay;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.google.inject.Injector;
+
 import de.prob.check.tracereplay.check.exploration.TraceExplorer;
 import de.prob.check.tracereplay.check.ui.ToManyOptionsIdentifierMapping;
+import de.prob2.ui.internal.I18n;
 import de.prob2.ui.internal.StageManager;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-import java.util.*;
-
 import static java.util.stream.Collectors.toMap;
 
-public class ToManyOptionsRequestIdentifierMapping extends Dialog<Map<String, String>> implements ToManyOptionsIdentifierMapping {
+public class TooManyOptionsRequestIdentifierMapping extends Dialog<Map<String, String>> implements ToManyOptionsIdentifierMapping {
 
 	@FXML
 	VBox oldInfos;
@@ -31,38 +41,37 @@ public class ToManyOptionsRequestIdentifierMapping extends Dialog<Map<String, St
 	AnchorPane newInfosPane;
 
 	private final StageManager stageManager;
-	private final ResourceBundle resourceBundle;
+	private final I18n i18n;
 
 	private final Map<String, SimpleStringProperty> internalView = new HashMap<>();
 
-	public ToManyOptionsRequestIdentifierMapping(final Injector injector, final StageManager stageManager){
+	public TooManyOptionsRequestIdentifierMapping(final StageManager stageManager, final I18n i18n) {
 		this.stageManager = stageManager;
-		this.resourceBundle = injector.getInstance(ResourceBundle.class);
+		this.i18n = i18n;
 
-		stageManager.loadFXML(this, "toManyOptionsChoice.fxml");
+		stageManager.loadFXML(this, "tooManyOptionsChoice.fxml");
 
-		ButtonType buttonTypeY = new ButtonType(resourceBundle.getString("toManyOptionsRequest.alert.button.ok"), ButtonBar.ButtonData.YES);
-	//	ButtonType buttonTypeN = new ButtonType(resourceBundle.getString("toManyOptionsRequest.alert.button.dontCare"), ButtonBar.ButtonData.NO);
+		ButtonType buttonTypeY = new ButtonType(i18n.translate("tooManyOptionsRequest.alert.button.ok"), ButtonBar.ButtonData.YES);
+		//	ButtonType buttonTypeN = new ButtonType(i18n.translate("tooManyOptionsRequest.alert.button.dontCare"), ButtonBar.ButtonData.NO);
 
-		this.setTitle(resourceBundle.getString("toManyOptionsRequest.alert.button.title"));
-	//	this.setHeaderText(resourceBundle.getString("toManyOptionsRequest.alert.button.text"));
+		this.setTitle(i18n.translate("tooManyOptionsRequest.alert.button.title"));
+		//	this.setHeaderText(i18n.translate("tooManyOptionsRequest.alert.button.text"));
 		this.getDialogPane().getButtonTypes().addAll(buttonTypeY);
 
-
-		this.setResultConverter(param ->
-
-			internalView.entrySet().stream().collect(toMap(Map.Entry::getKey, entry -> entry.getValue().getValue()))
+		this.setResultConverter(
+				param -> internalView.entrySet()
+						         .stream()
+						         .collect(toMap(Map.Entry::getKey, entry -> entry.getValue().getValue()))
 		);
 	}
 
-
-	void prepare(List<String> oldInfo, List<String> newInfo){
-		for(String info : oldInfo){
+	void prepare(List<String> oldInfo, List<String> newInfo) {
+		for (String info : oldInfo) {
 			Label old = new Label(info);
 			oldInfos.getChildren().add(old);
 
 			ChoiceBox<String> choiceBox = new ChoiceBox<>(FXCollections.observableArrayList(newInfo));
-			if(newInfo.contains(info)){
+			if (newInfo.contains(info)) {
 				choiceBox.getSelectionModel().select(info);
 			}
 
@@ -83,7 +92,6 @@ public class ToManyOptionsRequestIdentifierMapping extends Dialog<Map<String, St
 			internalView.put(info, selectedNew);
 		}
 
-
 		oldInfos.setAlignment(Pos.BOTTOM_RIGHT);
 		newInfos.setAlignment(Pos.BOTTOM_RIGHT);
 
@@ -91,15 +99,13 @@ public class ToManyOptionsRequestIdentifierMapping extends Dialog<Map<String, St
 		oldInfos.prefWidthProperty().bind(oldInfosPane.widthProperty());
 		newInfos.prefHeightProperty().bind(newInfosPane.heightProperty());
 		newInfos.prefWidthProperty().bind(newInfosPane.widthProperty());
-
 	}
-
 
 	@Override
 	public Map<String, String> askForMapping(List<String> oldInfo, List<String> newInfo, String operationName, TraceExplorer.MappingNames section) {
 		stageManager.register(this);
-		this.setHeaderText(String.format(resourceBundle.getString("toManyOptionsRequest.alert.button.text"), section, operationName));
+		this.setHeaderText(i18n.translate("tooManyOptionsRequest.alert.button.text", section, operationName));
 		prepare(oldInfo, newInfo);
-		return this.showAndWait().get();
+		return this.showAndWait().orElseGet(Collections::emptyMap);
 	}
 }
