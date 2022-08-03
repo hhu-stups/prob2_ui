146bac290a56bcbd03b4cea8f903528579cc2d5c
dgelessus <dgelessus@users.noreply.github.com>
2022-08-03 12:27:58 +0200

    Merge symbolic checkers into item handlers

--- src/main/java/de/prob2/ui/animation/symbolic/SymbolicAnimationItemHandler.java
+++ src/main/java/de/prob2/ui/animation/symbolic/SymbolicAnimationItemHandler.java
@@ -2,31 +2,40 @@ package de.prob2.ui.animation.symbolic;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
 
 import com.google.inject.Singleton;
 
+import de.prob.animator.command.AbstractCommand;
 import de.prob.animator.command.ConstraintBasedSequenceCheckCommand;
 import de.prob.animator.command.FindStateCommand;
 import de.prob.animator.domainobjects.ClassicalB;
 import de.prob.animator.domainobjects.FormulaExpand;
 import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
+import de.prob2.ui.internal.executor.CliTaskExecutor;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.project.machines.Machine;
 import de.prob2.ui.symbolic.SymbolicFormulaHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Singleton
 public class SymbolicAnimationItemHandler implements SymbolicFormulaHandler<SymbolicAnimationItem> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SymbolicAnimationItemHandler.class);
 
 	private final CurrentTrace currentTrace;
-
-	private final SymbolicAnimationChecker symbolicChecker;
+	private final SymbolicAnimationResultHandler resultHandler;
+	private final CliTaskExecutor cliExecutor;
 
 	@Inject
-	private SymbolicAnimationItemHandler(final CurrentTrace currentTrace, final SymbolicAnimationChecker symbolicChecker) {
+	private SymbolicAnimationItemHandler(final CurrentTrace currentTrace, final SymbolicAnimationResultHandler resultHandler, final CliTaskExecutor cliExecutor) {
 		this.currentTrace = currentTrace;
-		this.symbolicChecker = symbolicChecker;
+		this.resultHandler = resultHandler;
+		this.cliExecutor = cliExecutor;
 	}
 
 	@Override
@@ -34,16 +43,41 @@ public class SymbolicAnimationItemHandler implements SymbolicFormulaHandler<Symb
 		return machine.getSymbolicAnimationFormulas();
 	}
 
+	private void updateTrace(SymbolicAnimationItem item) {
+		List<Trace> examples = item.getExamples();
+		if(!examples.isEmpty()) {
+			currentTrace.set(examples.get(0));
+		}
+	}
+
+	private void checkItem(SymbolicAnimationItem item, AbstractCommand cmd, final StateSpace stateSpace, boolean checkAll) {
+		final CompletableFuture<AbstractCommand> future = cliExecutor.submit(() -> {
+			stateSpace.execute(cmd);
+			return cmd;
+		});
+		future.whenComplete((r, e) -> {
+			if (e == null) {
+				resultHandler.handleFormulaResult(item, r);
+			} else {
+				LOGGER.error("Exception during symbolic animation", e);
+				resultHandler.handleFormulaResult(item, e);
+			}
+			if(!checkAll) {
+				updateTrace(item);
+			}
+		});
+	}
+
 	public void handleSequence(SymbolicAnimationItem item, boolean checkAll) {
 		List<String> events = Arrays.asList(item.getCode().replace(" ", "").split(";"));
 		ConstraintBasedSequenceCheckCommand cmd = new ConstraintBasedSequenceCheckCommand(currentTrace.getStateSpace(), events, new ClassicalB("1=1", FormulaExpand.EXPAND));
-		symbolicChecker.checkItem(item, cmd, currentTrace.getStateSpace(), checkAll);
+		checkItem(item, cmd, currentTrace.getStateSpace(), checkAll);
 	}
 
 	public void findValidState(SymbolicAnimationItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		FindStateCommand cmd = new FindStateCommand(stateSpace, new ClassicalB(item.getCode(), FormulaExpand.EXPAND), true);
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 
 	@Override
--- src/main/java/de/prob2/ui/verifications/symbolicchecking/SymbolicCheckingFormulaHandler.java
+++ src/main/java/de/prob2/ui/verifications/symbolicchecking/SymbolicCheckingFormulaHandler.java
@@ -2,11 +2,13 @@ package de.prob2.ui.verifications.symbolicchecking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
 
 import com.google.inject.Singleton;
 
+import de.prob.animator.command.AbstractCommand;
 import de.prob.animator.command.CheckWellDefinednessCommand;
 import de.prob.animator.command.ConstraintBasedAssertionCheckCommand;
 import de.prob.animator.command.ConstraintBasedRefinementCheckCommand;
@@ -17,28 +19,76 @@ import de.prob.animator.domainobjects.FormulaExpand;
 import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.check.CBCDeadlockChecker;
 import de.prob.check.CBCInvariantChecker;
+import de.prob.check.IModelCheckJob;
+import de.prob.check.IModelCheckingResult;
 import de.prob.statespace.StateSpace;
+import de.prob.statespace.Trace;
+import de.prob2.ui.internal.executor.CliTaskExecutor;
 import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.project.machines.Machine;
 import de.prob2.ui.symbolic.SymbolicFormulaHandler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Singleton
 public class SymbolicCheckingFormulaHandler implements SymbolicFormulaHandler<SymbolicCheckingFormulaItem> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SymbolicCheckingFormulaHandler.class);
 	
 	private final CurrentTrace currentTrace;
-	
-	private final SymbolicFormulaChecker symbolicChecker;
+	private final SymbolicCheckingResultHandler resultHandler;
+	private final CliTaskExecutor cliExecutor;
 	
 	@Inject
-	public SymbolicCheckingFormulaHandler(final CurrentTrace currentTrace,
-											final SymbolicFormulaChecker symbolicChecker) {
+	public SymbolicCheckingFormulaHandler(final CurrentTrace currentTrace, final SymbolicCheckingResultHandler resultHandler, final CliTaskExecutor cliExecutor) {
 		this.currentTrace = currentTrace;
-		this.symbolicChecker = symbolicChecker;
+		this.resultHandler = resultHandler;
+		this.cliExecutor = cliExecutor;
 	}
 	
 	@Override
 	public List<SymbolicCheckingFormulaItem> getItems(final Machine machine) {
 		return machine.getSymbolicCheckingFormulas();
+	}
+	
+	private void updateTrace(SymbolicCheckingFormulaItem item) {
+		List<Trace> counterExamples = item.getCounterExamples();
+		if(!counterExamples.isEmpty()) {
+			currentTrace.set(counterExamples.get(0));
+		}
+	}
+	
+	private void checkItem(SymbolicCheckingFormulaItem item, AbstractCommand cmd, final StateSpace stateSpace, boolean checkAll) {
+		final CompletableFuture<AbstractCommand> future = cliExecutor.submit(() -> {
+			stateSpace.execute(cmd);
+			return cmd;
+		});
+		future.whenComplete((r, e) -> {
+			if (e == null) {
+				resultHandler.handleFormulaResult(item, r);
+			} else {
+				LOGGER.error("Exception during symbolic checking", e);
+				resultHandler.handleFormulaResult(item, e);
+			}
+			if(!checkAll) {
+				updateTrace(item);
+			}
+		});
+	}
+	
+	private void checkItem(IModelCheckJob checker, SymbolicCheckingFormulaItem item, boolean checkAll) {
+		final CompletableFuture<IModelCheckingResult> future = cliExecutor.submit(checker);
+		future.whenComplete((r, e) -> {
+			if (e == null) {
+				resultHandler.handleFormulaResult(item, r);
+			} else {
+				LOGGER.error("Exception during symbolic checking", e);
+				resultHandler.handleFormulaResult(item, e);
+			}
+			if(!checkAll) {
+				updateTrace(item);
+			}
+		});
 	}
 	
 	public void handleInvariant(SymbolicCheckingFormulaItem item, boolean checkAll) {
@@ -52,51 +102,51 @@ public class SymbolicCheckingFormulaHandler implements SymbolicFormulaHandler<Sy
 			eventNames.add(item.getCode());
 		}
 		CBCInvariantChecker checker = new CBCInvariantChecker(currentTrace.getStateSpace(), eventNames);
-		symbolicChecker.checkItem(checker, item, checkAll);
+		checkItem(checker, item, checkAll);
 	}
 		
 	public void handleRefinement(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		ConstraintBasedRefinementCheckCommand cmd = new ConstraintBasedRefinementCheckCommand();
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 	
 	public void handleStaticAssertions(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		ConstraintBasedAssertionCheckCommand cmd = new ConstraintBasedAssertionCheckCommand(ConstraintBasedAssertionCheckCommand.CheckingType.STATIC, stateSpace);
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 
 	public void handleDynamicAssertions(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		ConstraintBasedAssertionCheckCommand cmd = new ConstraintBasedAssertionCheckCommand(ConstraintBasedAssertionCheckCommand.CheckingType.DYNAMIC, stateSpace);
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 
 	public void handleWellDefinedness(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		CheckWellDefinednessCommand cmd = new CheckWellDefinednessCommand();
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 	
 	public void handleSymbolic(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		final SymbolicModelcheckCommand.Algorithm algorithm = SymbolicModelcheckCommand.Algorithm.valueOf(item.getCode());
 		SymbolicModelcheckCommand cmd = new SymbolicModelcheckCommand(algorithm);
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 		
 	}
 	
 	public void handleDeadlock(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		IEvalElement constraint = new ClassicalB(item.getCode(), FormulaExpand.EXPAND);
 		CBCDeadlockChecker checker = new CBCDeadlockChecker(currentTrace.getStateSpace(), constraint);
-		symbolicChecker.checkItem(checker, item, checkAll);
+		checkItem(checker, item, checkAll);
 	}
 	
 	public void findRedundantInvariants(SymbolicCheckingFormulaItem item, boolean checkAll) {
 		StateSpace stateSpace = currentTrace.getStateSpace();
 		GetRedundantInvariantsCommand cmd = new GetRedundantInvariantsCommand();
-		symbolicChecker.checkItem(item, cmd, stateSpace, checkAll);
+		checkItem(item, cmd, stateSpace, checkAll);
 	}
 	
 	@Override
