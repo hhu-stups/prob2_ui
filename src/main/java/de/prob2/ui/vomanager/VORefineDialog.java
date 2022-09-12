c8148c3747987c98ba680417bf4244ddd62ecda5
dgelessus <dgelessus@users.noreply.github.com>
2022-09-12 12:15:26 +0200

    Fix possible NPE when clicking VO refine button

--- src/main/java/de/prob2/ui/vomanager/VORefineDialog.java
+++ src/main/java/de/prob2/ui/vomanager/VORefineDialog.java
@@ -1,12 +1,8 @@
 package de.prob2.ui.vomanager;
 
-import de.prob2.ui.vomanager.ast.IValidationExpression;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-
 
 public class VORefineDialog extends Stage {
 
@@ -18,15 +14,15 @@ public class VORefineDialog extends Stage {
 	@FXML
 	private TextArea newVOExpression;
 
-	private final IValidationExpression oldExpression;
+	private final String oldExpression;
 
-	public VORefineDialog(IValidationExpression oldExpression){
+	public VORefineDialog(String oldExpression){
 		this.oldExpression = oldExpression;
 	}
 
 	@FXML
 	public void initialize() {
-		oldVOExpression.setText(oldExpression.toString());
+		oldVOExpression.setText(oldExpression);
 
 	}
 
