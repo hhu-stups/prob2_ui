6bc8bb11f42d67e8ab0ac01b4aae302c945d6063
Fabian Vu <Fabian.Vu@hhu.de>
2023-01-18 17:33:18 +0100

    Finish recording user interaction

--- src/main/java/de/prob2/ui/internal/UIInteraction.java
+++ src/main/java/de/prob2/ui/internal/UIInteraction.java
@@ -2,7 +2,9 @@ package de.prob2.ui.internal;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import de.prob.check.tracereplay.PersistentTrace;
 import de.prob.statespace.Transition;
+import de.prob2.ui.prob2fx.CurrentTrace;
 import de.prob2.ui.simulation.configuration.ActivationConfiguration;
 import de.prob2.ui.simulation.configuration.ActivationOperationConfiguration;
 import de.prob2.ui.simulation.configuration.SimulationConfiguration;
@@ -24,10 +26,13 @@ public class UIInteraction {
 
 	private final List<ActivationConfiguration> userInteractions;
 
+	private final CurrentTrace currentTrace;
+
 	private int interactionCounter;
 
 	@Inject
-	public UIInteraction() {
+	public UIInteraction(final CurrentTrace currentTrace) {
+		this.currentTrace = currentTrace;
 		this.uiListener = new SimpleObjectProperty<>(null);
 		this.userInteractions = new ArrayList<>();
 		this.interactionCounter = 0;
@@ -83,6 +88,7 @@ public class UIInteraction {
 		List<ActivationConfiguration> activationConfigurations = config.getActivationConfigurations();
 		List<ActivationConfiguration> activationConfigurationsForResult = new ArrayList<>();
 
+		boolean hasSetupConstants = false;
 		boolean hasInitialization = false;
 		List<String> activations = new ArrayList<>();
 		for(ActivationConfiguration activationConfiguration : activationConfigurations) {
@@ -92,6 +98,8 @@ public class UIInteraction {
 				activations = new ArrayList<>(initializationConfiguration.getActivating());
 				activations.addAll(userInteractions.stream().map(ActivationConfiguration::getId).collect(Collectors.toList()));
 				activationConfigurations.add(new ActivationOperationConfiguration("$initialise_machine", "$initialise_machine", initializationConfiguration.getAfter(), initializationConfiguration.getPriority(), initializationConfiguration.getAdditionalGuards(), initializationConfiguration.getActivationKind(), initializationConfiguration.getFixedVariables(), initializationConfiguration.getProbabilisticVariables(), activations));
+			} else if(activationConfiguration.getId().equals("$setup_constants")) {
+				hasSetupConstants = true;
 			} else {
 				activationConfigurationsForResult.add(activationConfiguration);
 			}
@@ -100,8 +108,16 @@ public class UIInteraction {
 			activations.addAll(userInteractions.stream().map(ActivationConfiguration::getId).collect(Collectors.toList()));
 			activationConfigurationsForResult.add(0, new ActivationOperationConfiguration("$initialise_machine", "$initialise_machine", null, 0, null, null, null, null, activations));
 		}
+
+		if(!hasSetupConstants) {
+			PersistentTrace persistentTrace = new PersistentTrace(currentTrace.get());
+			if("$setup_constants".equals(persistentTrace.getTransitionList().get(0).getOperationName())) {
+				activationConfigurationsForResult.add(0, new ActivationOperationConfiguration("$setup_constants", "$setup_constants", null, 0, null, null, null, null, new ArrayList<>()));
+			}
+		}
+
 		activationConfigurationsForResult.addAll(userInteractions);
-		return new SimulationConfiguration(activationConfigurationsForResult, null, SimulationConfiguration.metadataBuilder("Automatic_Simulation_with_User_Interaction").withSavedNow().withUserCreator().build());
+		return new SimulationConfiguration(activationConfigurationsForResult, new ArrayList<>(), SimulationConfiguration.metadataBuilder("Automatic_Simulation_with_User_Interaction").withSavedNow().withUserCreator().build());
 	}
 
 
