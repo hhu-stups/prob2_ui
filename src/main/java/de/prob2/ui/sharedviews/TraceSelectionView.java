5b679638632c3964f38e55653f25b9372c3ce6c3
dgelessus <dgelessus@users.noreply.github.com>
2023-02-01 16:28:06 +0100

    Refactor TraceSelectionView to use CheckingViewBase

--- src/main/java/de/prob2/ui/sharedviews/TraceSelectionView.java
+++ src/main/java/de/prob2/ui/sharedviews/TraceSelectionView.java
@@ -5,34 +5,53 @@ import com.google.inject.Inject;
 import de.prob.statespace.FormalismType;
 import de.prob2.ui.animation.tracereplay.ReplayTrace;
 import de.prob2.ui.animation.tracereplay.TraceChecker;
+import de.prob2.ui.internal.DisablePropertyController;
 import de.prob2.ui.internal.FXMLInjected;
 import de.prob2.ui.internal.I18n;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.prob2fx.CurrentTrace;
 
-import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
 import javafx.scene.input.MouseButton;
 
 @FXMLInjected
-public final class TraceSelectionView extends ScrollPane {
+public final class TraceSelectionView extends CheckingViewBase<ReplayTrace> {
+	private final class Row extends RowBase {
+		private Row() {
+			final MenuItem replayTraceItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.replayTrace"));
+			replayTraceItem.setDisable(true);
+			final MenuItem addTestsItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.editTrace"));
+			final MenuItem showDescriptionItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.showDescription"));
+			final MenuItem showStatusItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.showStatus"));
+			final MenuItem openInExternalEditorItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.openInExternalEditor"));
+			final MenuItem revealInExplorerItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.revealInExplorer"));
+
+			// Set listeners for menu items
+			traceViewHandler.initializeRow(this.getScene(), this, addTestsItem, replayTraceItem, showStatusItem, openInExternalEditorItem, revealInExplorerItem);
+			showDescriptionItem.setOnAction(event -> showDescription(this.getItem()));
+
+			contextMenu.getItems().addAll(replayTraceItem, addTestsItem, showStatusItem, new SeparatorMenuItem(), showDescriptionItem, new SeparatorMenuItem(), openInExternalEditorItem, revealInExplorerItem);
+
+			this.setOnMouseClicked(event -> {
+				ReplayTrace item = this.getItem();
+				if(item == null) {
+					return;
+				}
+				if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
+					traceChecker.check(item, true);
+				}
+			});
+		}
+	}
+
 	@FXML
-	private TableView<ReplayTrace> traceTableView;
-	@FXML
-	private TableColumn<ReplayTrace, Node> statusColumn;
-	@FXML
-	private TableColumn<ReplayTrace, String> nameColumn;
+	private TableColumn<ReplayTrace, Node> statusProgressColumn;
 	@FXML
 	private SplitPane splitPane;
 
@@ -44,7 +63,8 @@ public final class TraceSelectionView extends ScrollPane {
 	private boolean showDescription;
 
 	@Inject
-	public TraceSelectionView(final StageManager stageManager, final CurrentTrace currentTrace, final TraceChecker traceChecker, final I18n i18n, final TraceViewHandler traceViewHandler) {
+	public TraceSelectionView(final StageManager stageManager, final DisablePropertyController disablePropertyController, final CurrentTrace currentTrace, final TraceChecker traceChecker, final I18n i18n, final TraceViewHandler traceViewHandler) {
+		super(disablePropertyController);
 		this.stageManager = stageManager;
 		this.currentTrace = currentTrace;
 		this.traceChecker = traceChecker;
@@ -54,12 +74,13 @@ public final class TraceSelectionView extends ScrollPane {
 	}
 
 	@FXML
-	private void initialize() {
-		traceTableView.itemsProperty().bind(traceViewHandler.getTraces());
+	public void initialize() {
+		super.initialize();
+		items.bind(traceViewHandler.getTraces());
 		initTableColumns();
 		initTableRows();
 
-		traceTableView.getSelectionModel().selectedItemProperty().addListener((o, from, to) -> {
+		itemsTable.getSelectionModel().selectedItemProperty().addListener((o, from, to) -> {
 			if (showDescription) {
 				closeDescription();
 				if (to != null) {
@@ -69,48 +90,20 @@ public final class TraceSelectionView extends ScrollPane {
 		});
 
 		final BooleanBinding partOfDisableBinding = currentTrace.modelProperty().formalismTypeProperty().isNotEqualTo(FormalismType.B);
-		traceTableView.disableProperty().bind(partOfDisableBinding.or(currentTrace.stateSpaceProperty().isNull()));
+		itemsTable.disableProperty().bind(partOfDisableBinding.or(currentTrace.stateSpaceProperty().isNull()));
 	}
 
 	private void initTableColumns() {
-		statusColumn.setCellValueFactory(traceViewHandler.getTraceStatusFactory());
-		nameColumn.setCellValueFactory(
-				features -> new SimpleStringProperty(features.getValue().getName()));
+		statusProgressColumn.setCellValueFactory(traceViewHandler.getTraceStatusFactory());
 	}
 
 	private void initTableRows() {
-		this.traceTableView.setRowFactory(param -> {
-			final TableRow<ReplayTrace> row = new TableRow<>();
+		this.itemsTable.setRowFactory(table -> new Row());
+	}
 
-			final MenuItem replayTraceItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.replayTrace"));
-			replayTraceItem.setDisable(true);
-			final MenuItem addTestsItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.editTrace"));
-			final MenuItem showDescriptionItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.showDescription"));
-			final MenuItem showStatusItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.showStatus"));
-			final MenuItem openInExternalEditorItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.openInExternalEditor"));
-			final MenuItem revealInExplorerItem = new MenuItem(i18n.translate("animation.tracereplay.view.contextMenu.revealInExplorer"));
-
-			// Set listeners for menu items
-			traceViewHandler.initializeRow(this.getScene(), row, addTestsItem, replayTraceItem, showStatusItem, openInExternalEditorItem, revealInExplorerItem);
-			showDescriptionItem.setOnAction(event -> showDescription(row.getItem()));
-
-			row.contextMenuProperty().bind(
-					Bindings.when(row.emptyProperty())
-					.then((ContextMenu) null)
-					.otherwise(new ContextMenu(replayTraceItem, addTestsItem, showStatusItem, new SeparatorMenuItem(), showDescriptionItem, new SeparatorMenuItem(), openInExternalEditorItem, revealInExplorerItem)));
-
-			row.setOnMouseClicked(event -> {
-				ReplayTrace item = row.getItem();
-				if(item == null) {
-					return;
-				}
-				if (event.getButton().equals(MouseButton.PRIMARY) && event.getClickCount() == 2) {
-					this.traceChecker.check(item, true);
-				}
-			});
-
-			return row;
-		});
+	@Override
+	protected String configurationForItem(final ReplayTrace item) {
+		return item.getName();
 	}
 
 	public void closeDescription() {
