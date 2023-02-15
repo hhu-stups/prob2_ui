bf76e808f552a9e37da5f7744a30377c8b568326
Clayworker98 <assan.haydar@gmail.com>
2023-02-15 21:33:09 +0100

    added Documentation Strings for some representation edge cases

--- src/main/java/de/prob2/ui/documentation/TemplateUtility.java
+++ src/main/java/de/prob2/ui/documentation/TemplateUtility.java
@@ -1,5 +1,7 @@
 package de.prob2.ui.documentation;
 
+import de.prob.check.tracereplay.PersistentTransition;
+import de.prob.statespace.Transition;
 import de.prob2.ui.internal.I18n;
 import de.prob2.ui.prob2fx.CurrentProject;
 import de.prob2.ui.project.machines.Machine;
@@ -13,17 +15,44 @@ import de.prob2.ui.vomanager.IValidationTask;
 
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static de.prob2.ui.documentation.DocumentationProcessHandler.readFile;
+import static de.prob2.ui.verifications.symbolicchecking.SymbolicCheckingType.DEADLOCK;
 
 // All Methods of this class are exclusively used by the documentation Template
 public class TemplateUtility {
 
-	public static String toUIString(ModelCheckingItem item, I18n i18n) {
+	public static String transitionToUIString(PersistentTransition transition) {
+		String operationName = Transition.prettifyName(transition.getOperationName());
+		Map<String, String> parameters = transition.getParameters();
+		if(parameters.isEmpty()) {
+			return latexSafe(operationName);
+		}
+		List<String> keyValuePairs = parameters.entrySet().stream()
+				.map(entry -> entry.getKey() + "=" + entry.getValue())
+				.collect(Collectors.toList());
+		return latexSafe(operationName + "(" + String.join(", ", keyValuePairs) + ")");
+	}
+
+	public static String symbolicConfigString(SymbolicCheckingFormulaItem formula, I18n i18n) {
+		String config = formula.getCode();
+		// no configuration is internally represented as 1=1, but that is not intended in the output
+		if(formula.getType().equals(DEADLOCK) && config.equals("1=1")){
+			return latexSafe(i18n.translate("verifications.symbolicchecking.view.deadlock.noPredicate"));
+		}
+		// & is replaced because otherwise a column change takes place in the template table
+		config = config.replaceAll("&",", \\\\newline");
+		return latexSafe(config);
+	}
+	public static String modelcheckingToUIString(ModelCheckingItem item, I18n i18n) {
 		String description = item.getTaskDescription(i18n);
 		if (item.getId() != null) {
 			description = "[" + item.getId() + "] " + description;
 		}
+		//replaces comma between 2 digits. statelimit parameter(integer) breaks the line otherwise
+		description = description.replaceAll("(\\d),(\\d)", "$1$2");
 		return description.replaceAll(",", ",\\\\newline");
 	}
 	public static String getMachineCode(Machine elem, CurrentProject project) {
