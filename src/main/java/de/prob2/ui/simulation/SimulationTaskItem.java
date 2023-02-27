02ccc3b31a155a909f41a707d15e7c23f8aa690d
Fabian Vu <Fabian.Vu@hhu.de>
2023-02-27 16:33:55 +0100

    Update SimulationTaskItem to use i18n

--- src/main/java/de/prob2/ui/simulation/SimulationTaskItem.java
+++ src/main/java/de/prob2/ui/simulation/SimulationTaskItem.java
@@ -66,104 +66,104 @@ public class SimulationTaskItem extends TableCell<SimulationItem, String> {
 
 		if(information.containsKey("EXECUTIONS")) {
 
-			Label lbMonteCarloSimulation = new Label("Monte Carlo Simulation");
+			Label lbMonteCarloSimulation = new Label(i18n.translate("simulation.task.montecarlo"));
 			lbMonteCarloSimulation.getStyleClass().add("id");
 			this.itemBox.getChildren().add(lbMonteCarloSimulation);
 
-			Label lbExecutions = new Label(String.format("Executions: %s", information.get("EXECUTIONS")));
+			Label lbExecutions = new Label(i18n.translate("simulation.task.executions", information.get("EXECUTIONS")));
 			lbExecutions.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbExecutions);
 		}
 
 		if(information.containsKey("MAX_STEPS_BEFORE_PROPERTY") && (int) information.get("MAX_STEPS_BEFORE_PROPERTY") > 0) {
-			Label lbMaxStepsBeforeProperty = new Label(String.format("Max Steps before Start: %s", information.get("MAX_STEPS_BEFORE_PROPERTY")));
+			Label lbMaxStepsBeforeProperty = new Label(i18n.translate("simulation.task.stepsBeforeStart", information.get("MAX_STEPS_BEFORE_PROPERTY")));
 			lbMaxStepsBeforeProperty.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbMaxStepsBeforeProperty);
 		}
 
 		if(information.containsKey("START_AFTER_STEPS")) {
-			Label lbStartAfterSteps = new Label(String.format("Start after %s steps", information.get("START_AFTER_STEPS")));
+			Label lbStartAfterSteps = new Label(i18n.translate("simulation.task.startAfterSteps", information.get("START_AFTER_STEPS")));
 			lbStartAfterSteps.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbStartAfterSteps);
 		}
 
 		if(information.containsKey("STARTING_PREDICATE")) {
-			Label lbStartingPredicate = new Label(String.format("Starting predicate: %s", information.get("STARTING_PREDICATE")));
+			Label lbStartingPredicate = new Label(i18n.translate("simulation.task.startingPredicate", information.get("STARTING_PREDICATE")));
 			lbStartingPredicate.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbStartingPredicate);
 		}
 
 		if(information.containsKey("STARTING_PREDICATE_ACTIVATED")) {
-			Label lbStartingPredicate = new Label(String.format("Starting predicate (activated): %s", information.get("STARTING_PREDICATE_ACTIVATED")));
+			Label lbStartingPredicate = new Label(i18n.translate("simulation.task.startingPredicateActivated", information.get("STARTING_PREDICATE_ACTIVATED")));
 			lbStartingPredicate.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbStartingPredicate);
 		}
 
 		if(information.containsKey("STARTING_TIME")) {
-			Label lbStartingTime = new Label(String.format("Starting time: %s", information.get("STARTING_TIME")));
+			Label lbStartingTime = new Label(i18n.translate("simulation.task.startingTime", information.get("STARTING_TIME")));
 			lbStartingTime.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbStartingTime);
 		}
 
 
 		if(information.containsKey("STEPS_PER_EXECUTION")) {
-			Label lbStepsPerExecution = new Label(String.format("Steps per execution: %s", information.get("STEPS_PER_EXECUTION")));
+			Label lbStepsPerExecution = new Label(i18n.translate("simulation.task.stepsPerExecution", information.get("STEPS_PER_EXECUTION")));
 			lbStepsPerExecution.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbStepsPerExecution);
 		}
 
 		if(information.containsKey("ENDING_PREDICATE")) {
-			Label lbEndingPredicate = new Label(String.format("Ending predicate: %s", information.get("ENDING_PREDICATE")));
+			Label lbEndingPredicate = new Label(i18n.translate("simulation.task.endingPredicate", information.get("ENDING_PREDICATE")));
 			lbEndingPredicate.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbEndingPredicate);
 		}
 
 		if(information.containsKey("ENDING_TIME")) {
-			Label lbEndingTime = new Label(String.format("Ending time: %s", information.get("ENDING_TIME")));
+			Label lbEndingTime = new Label(i18n.translate("simulation.task.endingTime", information.get("ENDING_TIME")));
 			lbEndingTime.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbEndingTime);
 		}
 
 		if(item.getType() == SimulationType.ESTIMATION || item.getType() == SimulationType.HYPOTHESIS_TEST) {
-			Label lbProperty = new Label("Property");
+			Label lbProperty = new Label(i18n.translate("simulation.task.property"));
 			lbProperty.getStyleClass().add("id");
 			this.itemBox.getChildren().add(lbProperty);
 		}
 
 
 		if(information.containsKey("CHECKING_TYPE")) {
-			Label lbCheckingType = new Label(String.format("Checking type: %s", information.get("CHECKING_TYPE")));
+			Label lbCheckingType = new Label(i18n.translate("simulation.task.checkingType", information.get("CHECKING_TYPE")));
 			lbCheckingType.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbCheckingType);
 		}
 
 		if(information.containsKey("PREDICATE")) {
-			Label lbPredicate = new Label(String.format("Checking predicate: %s", information.get("PREDICATE")));
+			Label lbPredicate = new Label(i18n.translate("simulation.task.checkingPredicate", information.get("PREDICATE")));
 			lbPredicate.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbPredicate);
 		}
 
 		if(information.containsKey("TIME")) {
-			Label lbTime = new Label(String.format("Checking time: %s", information.get("TIME")));
+			Label lbTime = new Label(i18n.translate("simulation.task.checkingTime", information.get("TIME")));
 			lbTime.getStyleClass().add("information");
 			this.itemBox.getChildren().add(lbTime);
 		}
 
 		if(item.getType() == SimulationType.HYPOTHESIS_TEST) {
 			if(information.containsKey("HYPOTHESIS_CHECKING_TYPE")) {
-				Label lbHypothesisTest = new Label(String.format("Hypothesis Test (%s)", information.get("HYPOTHESIS_CHECKING_TYPE")));
+				Label lbHypothesisTest = new Label(i18n.translate("simulation.task.hypothesistest", information.get("HYPOTHESIS_CHECKING_TYPE")));
 				lbHypothesisTest.getStyleClass().add("id");
 				this.itemBox.getChildren().add(lbHypothesisTest);
 			}
 
 			if(information.containsKey("PROBABILITY")) {
-				Label lbProbability = new Label(String.format("Probability: %s", information.get("PROBABILITY")));
+				Label lbProbability = new Label(i18n.translate("simulation.task.probability", information.get("PROBABILITY")));
 				lbProbability.getStyleClass().add("information");
 				this.itemBox.getChildren().add(lbProbability);
 			}
 
 			if(information.containsKey("SIGNIFICANCE")) {
-				Label lbSignificance = new Label(String.format("Significance level: %s", information.get("SIGNIFICANCE")));
+				Label lbSignificance = new Label(i18n.translate("simulation.task.significance", information.get("SIGNIFICANCE")));
 				lbSignificance.getStyleClass().add("information");
 				this.itemBox.getChildren().add(lbSignificance);
 			}
@@ -172,19 +172,19 @@ public class SimulationTaskItem extends TableCell<SimulationItem, String> {
 
 		if(item.getType() == SimulationType.ESTIMATION) {
 			if (information.containsKey("ESTIMATION_TYPE")) {
-				Label lbEstimationType = new Label(String.format("Estimation type (%s)", information.get("ESTIMATION_TYPE")));
+				Label lbEstimationType = new Label(i18n.translate("simulation.task.estimationType", information.get("ESTIMATION_TYPE")));
 				lbEstimationType.getStyleClass().add("id");
 				this.itemBox.getChildren().add(lbEstimationType);
 			}
 
 			if (information.containsKey("DESIRED_VALUE")) {
-				Label lbDesiredValue = new Label(String.format("Desired value: %s", information.get("DESIRED_VALUE")));
+				Label lbDesiredValue = new Label(i18n.translate("simulation.task.desiredValue", information.get("DESIRED_VALUE")));
 				lbDesiredValue.getStyleClass().add("information");
 				this.itemBox.getChildren().add(lbDesiredValue);
 			}
 
 			if (information.containsKey("EPSILON")) {
-				Label lbEpsilon = new Label(String.format("Epsilon: %s", information.get("EPSILON")));
+				Label lbEpsilon = new Label(i18n.translate("simulation.task.epsilon", information.get("EPSILON")));
 				lbEpsilon.getStyleClass().add("information");
 				this.itemBox.getChildren().add(lbEpsilon);
 			}
