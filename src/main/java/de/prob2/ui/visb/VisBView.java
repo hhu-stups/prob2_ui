94cbabf527f9451486ef490fe082c2e0598cc307
Michael Leuschel <leuschel@uni-duesseldorf.de>
2023-04-24 12:56:10 +0200

    only create SVG objects if necessary
    and avoid have one new__%s variable per object
    
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- src/main/java/de/prob2/ui/visb/VisBView.java
+++ src/main/java/de/prob2/ui/visb/VisBView.java
@@ -299,18 +299,15 @@ public class VisBView extends BorderPane {
 		List<VisBSVGObject> svgObjects = visBVisualisation.getSVGObjects();
 		// TODO: Maybe use templates
 		if(!svgObjects.isEmpty()) {
-			StringBuilder scriptString = new StringBuilder();
+		    webView.getEngine().executeScript("var new__obj;");
 			for(VisBSVGObject svgObject : svgObjects) {
 				String id = svgObject.getId();
-				String object = svgObject.getObject();
-				scriptString.append(String.format(Locale.ROOT, "var new__%s = createElement(\"%s\")\n", id, object));
-			}
-			webView.getEngine().executeScript(scriptString.toString());
-			for(VisBSVGObject svgObject : svgObjects) {
-				String id = svgObject.getId();
+				String cmd = "new__obj = ((document.getElementById(\"" + id + "\") == null) " +
+				                               "? createElement(\"" + svgObject.getObject() + "\")" +
+				                               ": document.getElementById(\"" + id + "\") )\n";
 				Map<String, String> attributes = svgObject.getAttributes();
-				JSObject object = (JSObject) this.webView.getEngine().executeScript(String.format(Locale.ROOT, "new__%s", id));
-				object.call("setAttribute", "id", id);
+				JSObject object = (JSObject) this.webView.getEngine().executeScript(cmd);
+				object.call("setAttribute", "id", id); // not necessary if we called getElementById above
 				for(Map.Entry<String, String> entry : attributes.entrySet()) {
 					if("text".equals(entry.getKey())) {
 						this.getJSWindow().call("changeText", object, entry.getValue());
