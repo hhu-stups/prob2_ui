566019b838b82f8387a553a42c79ede00379be37
Ina <inabackes@live.de>
2023-05-11 13:44:31 +0200

    add common buttons to toolbar and replace zooming-button icons with magnifying glasses

--- src/main/java/de/prob2/ui/menu/ToolBarController.java
+++ src/main/java/de/prob2/ui/menu/ToolBarController.java
@@ -2,13 +2,18 @@ package de.prob2.ui.menu;
 
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-import de.prob2.ui.config.Config;
+import de.prob2.ui.config.FileChooserManager;
 import de.prob2.ui.internal.I18n;
 import de.prob2.ui.internal.StageManager;
 import de.prob2.ui.layout.FontSize;
 import de.prob2.ui.persistence.UIState;
 import de.prob2.ui.preferences.PreferencesStage;
+import de.prob2.ui.prob2fx.CurrentProject;
+import de.prob2.ui.project.NewProjectStage;
+import de.prob2.ui.project.ProjectManager;
+import java.nio.file.Path;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class ToolBarController {
@@ -19,22 +24,47 @@ public class ToolBarController {
 	private final I18n i18n;
 	@FXML
 	private final FontSize fontSize;
+	private final FileChooserManager fileChooserManager;
+	private final ProjectManager projectManager;
+	private final CurrentProject currentProject;
+
+	@FXML
+	private Button saveProjectButton;
 
 
 	@Inject
-	public ToolBarController(Injector injector, StageManager stageManager, UIState uiState, I18n i18n, FontSize fontsize) {
+	public ToolBarController(Injector injector, StageManager stageManager, UIState uiState, I18n i18n, FontSize fontsize, final ProjectManager projectManager, final FileChooserManager fileChooserManager, final CurrentProject currentProject) {
 		this.injector = injector;
 		this.stageManager = stageManager;
 		this.uiState = uiState;
 		this.i18n = i18n;
 		this.fontSize = fontsize;
+		this.fileChooserManager = fileChooserManager;
+		this.projectManager = projectManager;
+		this.currentProject = currentProject;
 	}
 
+	@FXML
+	private void initialize(){
+		this.saveProjectButton.disableProperty().bind(currentProject.isNull());
+	}
+
+	@FXML
+	private void saveProject(){
+			projectManager.saveCurrentProject();
+			}
 	@FXML
 	private void handlePreferences() {
 		final Stage preferencesStage = injector.getInstance(PreferencesStage.class);
 		preferencesStage.show();
 		preferencesStage.toFront();
+	}
+
+	@FXML
+	private void createNewProject() {
+		final Stage newProjectStage = injector.getInstance(NewProjectStage.class);
+		newProjectStage.showAndWait();
+		newProjectStage.toFront();
 	}
 
 	@FXML
@@ -45,5 +75,14 @@ public class ToolBarController {
 	@FXML
 	private void zoomOut() {
 		fontSize.setFontSize(fontSize.getFontSize() - 1);
+	}
+
+	@FXML
+	private void handleOpen() {
+		final Path selected = fileChooserManager.showOpenProjectOrMachineChooser(stageManager.getMainStage());
+		if (selected == null) {
+			return;
+		}
+		projectManager.openFile(selected);
 	}
 }
