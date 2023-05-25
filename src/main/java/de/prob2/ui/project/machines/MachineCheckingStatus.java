4faa47d8cf2d064e96069d27784f0e905c30f5d6
sherin.schneider <sherin.schneider@uni-duesseldorf.de>
2023-05-25 09:31:31 +0200

    Move Methods to ModelCheckingStatus

--- src/main/java/de/prob2/ui/project/machines/MachineCheckingStatus.java
+++ src/main/java/de/prob2/ui/project/machines/MachineCheckingStatus.java
@@ -1,5 +1,10 @@
 package de.prob2.ui.project.machines;
 
+import de.prob2.ui.verifications.Checked;
+import de.prob2.ui.verifications.IExecutableItem;
+
+import java.util.List;
+
 public class MachineCheckingStatus {
 
 	public enum CheckingStatus {
@@ -33,4 +38,34 @@ public class MachineCheckingStatus {
 	public int getNumberTotal() {
 		return numberTotal;
 	}
+
+	private static CheckingStatus combineCheckingStatus(final List<? extends IExecutableItem> items) {
+		boolean anyEnabled = false;
+		boolean anyUnknown = false;
+		for(IExecutableItem item : items) {
+			if(!item.selected()) {
+				continue;
+			}
+			anyEnabled = true;
+			if(item.getChecked() == Checked.FAIL) {
+				return CheckingStatus.FAILED;
+			} else if (item.getChecked() == Checked.NOT_CHECKED) {
+				anyUnknown = true;
+			}
+		}
+		return anyEnabled ? (anyUnknown? CheckingStatus.UNKNOWN :  CheckingStatus.SUCCESSFUL) : CheckingStatus.NONE;
+	}
+
+	static MachineCheckingStatus combineMachineCheckingStatus(final List<? extends IExecutableItem> items) {
+		CheckingStatus status = combineCheckingStatus(items);
+		int numberSuccess = (int) items.stream()
+			.filter(item -> item.getChecked() == Checked.SUCCESS && item.selected())
+			.count();
+		int numberTotal = (int) items.stream()
+			.filter(IExecutableItem::selected)
+			.count();
+		return new MachineCheckingStatus(status, numberSuccess, numberTotal);
+	}
+
+
 }
