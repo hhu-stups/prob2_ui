6225dbfaa339425796d999c584c6ef077b4ed3b4
dgelessus <dgelessus@users.noreply.github.com>
2023-07-07 10:19:05 +0200

    Inline DefaultPathDialog string keys that aren't variable anymore

--- src/main/java/de/prob2/ui/visb/DefaultPathDialog.java
+++ src/main/java/de/prob2/ui/visb/DefaultPathDialog.java
@@ -9,7 +9,6 @@ import de.prob2.ui.internal.StageManager;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
@@ -22,11 +21,6 @@ public final class DefaultPathDialog extends Dialog<DefaultPathDialog.Action> {
 		UNSET_DEFAULT,
 	}
 
-	private final I18n i18n;
-	private final SimpleStringProperty titleKey;
-	private final SimpleStringProperty statusWithDefaultKey;
-	private final SimpleStringProperty statusWithCurrentAsDefaultKey;
-	private final SimpleStringProperty statusWithoutDefaultKey;
 	private final SimpleObjectProperty<Path> loadedPath;
 	private final SimpleObjectProperty<Path> defaultPath;
 
@@ -39,11 +33,6 @@ public final class DefaultPathDialog extends Dialog<DefaultPathDialog.Action> {
 	public DefaultPathDialog(final StageManager stageManager, final I18n i18n) {
 		super();
 
-		this.i18n = i18n;
-		this.titleKey = new SimpleStringProperty("visb.defaultVisualisation.header");
-		this.statusWithDefaultKey = new SimpleStringProperty("visb.defaultVisualisation.text");
-		this.statusWithCurrentAsDefaultKey = new SimpleStringProperty("visb.currentAsDefaultVisualisation.text");
-		this.statusWithoutDefaultKey = new SimpleStringProperty("visb.noDefaultVisualisation.text");
 		this.loadedPath = new SimpleObjectProperty<>();
 		this.defaultPath = new SimpleObjectProperty<>();
 
@@ -54,7 +43,7 @@ public final class DefaultPathDialog extends Dialog<DefaultPathDialog.Action> {
 
 		this.getDialogPane().getButtonTypes().setAll(this.setButtonType, this.loadDefinitionsButtonType, ButtonType.CANCEL);
 
-		this.titleProperty().bind((i18n.translateBinding(this.titleKey)));
+		this.setTitle(i18n.translate("visb.defaultVisualisation.header"));
 		this.setResultConverter(buttonType -> {
 			if (buttonType == null || buttonType == ButtonType.CANCEL) {
 				return null;
@@ -72,11 +61,11 @@ public final class DefaultPathDialog extends Dialog<DefaultPathDialog.Action> {
 		});
 		this.contentTextProperty().bind(
 			Bindings.when(this.defaultPath.isNull())
-				.then(i18n.translateBinding(this.statusWithoutDefaultKey))
+				.then(i18n.translate("visb.noDefaultVisualisation.text"))
 				.otherwise(
 					Bindings.when(this.defaultPath.isEqualTo(loadedPath))
-						.then(i18n.translateBinding(this.statusWithCurrentAsDefaultKey, this.defaultPath))
-						.otherwise(i18n.translateBinding(this.statusWithDefaultKey, this.defaultPath))
+						.then(i18n.translateBinding("visb.currentAsDefaultVisualisation.text", this.defaultPath))
+						.otherwise(i18n.translateBinding("visb.defaultVisualisation.text", this.defaultPath))
 				)
 		);
 		ChangeListener<? super Path> changeListener = (observable, oldValue, newValue) -> {
