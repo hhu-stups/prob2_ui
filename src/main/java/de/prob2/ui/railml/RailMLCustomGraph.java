49bae76853c891f4f9b0263c8320841de3774343
Jan Gruteser <jan.gruteser@hhu.de>
2023-08-02 09:50:00 +0200

    delete class no longer needed

--- src/main/java/de/prob2/ui/railml/RailMLCustomGraph.java
+++
@@ -1,7 +0,0 @@
-package de.prob2.ui.railml;
-
-public class RailMLCustomGraph {
-	public static final String D4R = "D4R_customGraph";
-	public static final String RAIL_OSCOPE = "NOR_customGraph";
-	public static final String DOT = "DOT_customGraph";
-}
