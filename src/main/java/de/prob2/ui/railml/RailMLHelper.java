4aaa9a6e27a6730019e190ec2a667a28d2e34c9f
Jan Gruteser <jan.gruteser@hhu.de>
2023-10-02 16:59:22 +0200

    don't replace identical resource files

--- src/main/java/de/prob2/ui/railml/RailMLHelper.java
+++ src/main/java/de/prob2/ui/railml/RailMLHelper.java
@@ -3,28 +3,39 @@ package de.prob2.ui.railml;
 import com.google.common.io.MoreFiles;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class RailMLHelper {
 
 	protected static void replaceOldResourceFile(Path generationPath, String resourceFileName) throws IOException {
 		Path currentFile = generationPath.resolve(resourceFileName);
+		InputStream resourceFile = Objects.requireNonNull(RailMLHelper.class.getResourceAsStream(resourceFileName));
+
 		if (Files.exists(currentFile)) {
-			saveOldFile(currentFile);
+			byte[] currFile = Files.readAllBytes(currentFile);
+			byte[] resFile = resourceFile.readAllBytes();
+
+			if (!Arrays.equals(currFile,resFile)) {
+				saveOldFile(currentFile);
+				Files.copy(resourceFile, currentFile, StandardCopyOption.REPLACE_EXISTING);
+			}
+		} else {
+			Files.copy(resourceFile, currentFile, StandardCopyOption.REPLACE_EXISTING);
 		}
-		Files.copy(Objects.requireNonNull(RailMLHelper.class.getResourceAsStream(resourceFileName)),
-			currentFile, StandardCopyOption.REPLACE_EXISTING);
 	}
 
 	protected static void replaceOldFile(Path currentFile) throws IOException {
 		if (Files.exists(currentFile)) {
 			saveOldFile(currentFile);
-			Files.write(currentFile, Collections.emptyList(), StandardOpenOption.TRUNCATE_EXISTING);
+			Files.writeString(currentFile, "", StandardOpenOption.TRUNCATE_EXISTING);
+		} else {
+			Files.createFile(currentFile);
 		}
 	}
 
