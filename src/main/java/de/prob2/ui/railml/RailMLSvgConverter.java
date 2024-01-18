bbd0c321c8f4aab8686d2980b5c5706694827fba
Jan Gruteser <jan.gruteser@hhu.de>
2024-01-18 16:47:26 +0100

    adapt RailMLSvgConverter for new dasharray ext. function

--- src/main/java/de/prob2/ui/railml/RailMLSvgConverter.java
+++ src/main/java/de/prob2/ui/railml/RailMLSvgConverter.java
@@ -77,39 +77,21 @@ public class RailMLSvgConverter {
 		for (Map.Entry<String, String> entry : merged_paths.entrySet()) {
 			String id_prefix = entry.getKey();
 			String d_merged_path = entry.getValue();
-
-			String gradient_id_occ = id_prefix + "_lg_occ";
-			String gradient_id_ovl = id_prefix + "_lg_ovl";
-			String gradient_id_res = id_prefix + "_lg_res";
-			String gradient_id_tvd = id_prefix + "_lg_tvd";
-			String gradient_id_free = id_prefix + "_lg_free";
-
-			String[] path_d = d_merged_path.split(" ");
-			String[] path_begin = path_d[0].split(",");
-			String[] path_end = path_d[path_d.length - 1].split(",");
-
-			if (path_begin.length > 1 && path_end.length > 1) {
-				createAndAddGradient(doc, defs, gradient_id_occ, path_begin[0], path_begin[1], path_end[0], path_end[1], "transparent", "red");
-				createAndAddGradient(doc, defs, gradient_id_ovl, path_begin[0], path_begin[1], path_end[0], path_end[1], "transparent", "mediumvioletred");
-				createAndAddGradient(doc, defs, gradient_id_res, path_begin[0], path_begin[1], path_end[0], path_end[1], "transparent", "darkorange");
-				createAndAddGradient(doc, defs, gradient_id_tvd, path_begin[0], path_begin[1], path_end[0], path_end[1], "transparent", "blue");
-				createAndAddGradient(doc, defs, gradient_id_free, path_begin[0], path_begin[1], path_end[0], path_end[1], "black", "yellowgreen");
-			} else {
-				throw new Exception("Attribute 'd' of path " + id_prefix + " could not be processed");
-			}
 			Element merged_group = doc.createElement("g");
 
 			Element title_merged_group = doc.createElement("title");
 			title_merged_group.setAttribute("id", id_prefix + "_title");
 			title_merged_group.setTextContent(id_prefix);
 
-			Element path_free = createPath(doc, id_prefix + "_free", gradient_id_free, d_merged_path, "1.33", "yellowgreen");
-			Element path_tvd = createPathWithBlink(doc, id_prefix + "_tvd", gradient_id_tvd, d_merged_path, "1.5", "blue");
-			Element path_res = createPathWithBlink(doc, id_prefix + "_res", gradient_id_res, d_merged_path, "1.67", "darkorange");
-			Element path_ovl = createPathWithBlink(doc, id_prefix + "_ovl", gradient_id_ovl, d_merged_path, "1.85", "mediumvioletred");
-			Element path_occ = createPath(doc, id_prefix + "_occ", gradient_id_occ, d_merged_path, "2.0", "red");
+			Element path_free = createPath(doc, id_prefix + "_free", d_merged_path, "1.33", "yellowgreen");
+			Element path_branches = createPath(doc, id_prefix + "_branches", d_merged_path, "1.33", "black");
+			Element path_tvd = createPathWithBlink(doc, id_prefix + "_tvd", d_merged_path, "1.5", "blue");
+			Element path_res = createPathWithBlink(doc, id_prefix + "_res", d_merged_path, "1.67", "darkorange");
+			Element path_ovl = createPathWithBlink(doc, id_prefix + "_ovl", d_merged_path, "1.85", "mediumvioletred");
+			Element path_occ = createPath(doc, id_prefix + "_occ", d_merged_path, "2.0", "red");
 
 			merged_group.appendChild(path_free);
+			merged_group.appendChild(path_branches);
 			merged_group.appendChild(path_tvd);
 			merged_group.appendChild(path_res);
 			merged_group.appendChild(path_ovl);
@@ -155,7 +137,7 @@ public class RailMLSvgConverter {
 		transformer.transform(new javax.xml.transform.dom.DOMSource(doc), new javax.xml.transform.stream.StreamResult(fos));
 	}
 
-	private static Element createPath(Document doc, String id, String gradient_id, String d_merged_path,
+	private static Element createPath(Document doc, String id, String d_merged_path,
 									  String strokeWidth, String stroke) {
 		Element path = doc.createElement("path");
 		path.setAttribute("id", id);
@@ -167,9 +149,9 @@ public class RailMLSvgConverter {
 		return path;
 	}
 
-	private static Element createPathWithBlink(Document doc, String id, String gradient_id, String d_merged_path,
+	private static Element createPathWithBlink(Document doc, String id, String d_merged_path,
 											   String strokeWidth, String stroke) {
-		Element path_with_blink = createPath(doc, id, gradient_id, d_merged_path, strokeWidth, stroke);
+		Element path_with_blink = createPath(doc, id, d_merged_path, strokeWidth, stroke);
 		Element blink = doc.createElement("animate");
 		blink.setAttribute("id", id + "_blink");
 		blink.setAttribute("attributeName", "opacity");
@@ -178,38 +160,5 @@ public class RailMLSvgConverter {
 		blink.setAttribute("repeatCount", "indefinite");
 		path_with_blink.appendChild(blink);
 		return path_with_blink;
-	}
-
-	private static void createAndAddGradient(Document doc, Element defs, String gradientId, String x1, String y1,
-	                                                 String x2, String y2, String colorOuter, String colorInner) {
-		Element linearGradient = doc.createElement("linearGradient");
-		linearGradient.setAttribute("id", gradientId);
-		linearGradient.setAttribute("gradientUnits", "userSpaceOnUse");
-		linearGradient.setAttribute("x1", x1);
-		linearGradient.setAttribute("y1", y1);
-		linearGradient.setAttribute("x2", x2);
-		linearGradient.setAttribute("y2", y2);
-
-		if (colorOuter.equals("transparent")) {
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_1", "0%", "stop-opacity:0"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_2", "0%", "stop-color:" + colorInner + "; stop-opacity:1"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_3", "100%", "stop-color:" + colorInner + "; stop-opacity:1"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_4", "100%", "stop-opacity:0"));
-		} else {
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_1", "0%", "stop-color:" + colorOuter + "; stop-opacity:1"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_2", "0%", "stop-color:" + colorInner + "; stop-opacity:1"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_3", "100%", "stop-color:" + colorInner + "; stop-opacity:1"));
-			linearGradient.appendChild(createStopElement(doc, gradientId + "_4", "100%", "stop-color:" + colorOuter + "; stop-opacity:1"));
-		}
-
-		defs.appendChild(linearGradient);
-	}
-
-	private static Element createStopElement(Document doc, String id, String offset, String style) {
-		Element stop = doc.createElement("stop");
-		stop.setAttribute("id", id);
-		stop.setAttribute("offset", offset);
-		stop.setAttribute("style", style);
-		return stop;
 	}
 }
