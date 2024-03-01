f368e107f418e371b59e399280d2da229ff4dad9
dgelessus <dgelessus@users.noreply.github.com>
2024-03-01 14:13:33 +0100

    Document precise meanings of every Checked status

--- src/main/java/de/prob2/ui/verifications/Checked.java
+++ src/main/java/de/prob2/ui/verifications/Checked.java
@@ -1,7 +1,55 @@
 package de.prob2.ui.verifications;
 
 public enum Checked {
-	NOT_CHECKED, SUCCESS, FAIL, TIMEOUT, INTERRUPTED, PARSE_ERROR;
+	/**
+	 * The task did not run yet.
+	 * This status indicates nothing about the task's result.
+	 */
+	NOT_CHECKED,
+	
+	/**
+	 * The task ran and completed with a definitely successful result.
+	 * Re-running the task will not change this result,
+	 * unless the model or the task itself changes.
+	 */
+	SUCCESS,
+	
+	/**
+	 * The task ran and completed with a definitely failed result.
+	 * Re-running the task will not change this result,
+	 * unless the model or the task itself changes.
+	 */
+	FAIL,
+	
+	/**
+	 * The task ran,
+	 * but reached a user-defined limit before it could complete.
+	 * This is usually a time limit (timeout),
+	 * but can also be e. g. a state limit for explicit-state model checking.
+	 * This result indicates that the task has not failed <em>yet</em>,
+	 * but it also hasn't definitely succeeded,
+	 * so it may still fail if it continued running.
+	 * Re-running the task with a higher limit may yield a definitive result.
+	 */
+	TIMEOUT,
+	
+	/**
+	 * The task ran,
+	 * but was interrupted by the user before it could complete.
+	 * This result indicates that the task has not failed <em>yet</em>,
+	 * but it also hasn't definitely succeeded,
+	 * so it may still fail if it continued running.
+	 * Re-running the task without interrupting it may yield a definitive result.
+	 */
+	INTERRUPTED,
+	
+	/**
+	 * The task itself is invalid and cannot be run.
+	 * This is usually because of a parse error in a formula contained in the task.
+	 * Either the model or the task must be adjusted to make the task valid.
+	 */
+	PARSE_ERROR,
+	;
 	
 	public Checked and(final Checked other) {
 		if (this == PARSE_ERROR || this == FAIL) {
