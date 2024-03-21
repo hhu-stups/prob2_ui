bd0c619b76f217a103913291df99c5ff6ed0a51e
dgelessus <dgelessus@users.noreply.github.com>
2024-03-21 14:49:53 +0100

    Remove leftover reference to SavedProofObligationItem

--- src/main/java/de/prob2/ui/verifications/type/BuiltinValidationTaskTypes.java
+++ src/main/java/de/prob2/ui/verifications/type/BuiltinValidationTaskTypes.java
@@ -8,7 +8,6 @@ import de.prob2.ui.verifications.modelchecking.ModelCheckingItem;
 import de.prob2.ui.verifications.po.ProofObligationItem;
 import de.prob2.ui.verifications.symbolicchecking.SymbolicCheckingFormulaItem;
 import de.prob2.ui.verifications.temporal.TemporalFormulaItem;
-import de.prob2.ui.vomanager.IValidationTask;
 import de.prob2.ui.vomanager.ValidationTaskNotFound;
 
 import static de.prob2.ui.verifications.type.ValidationTaskType.register;
@@ -21,11 +20,6 @@ public final class BuiltinValidationTaskTypes {
 	public static final ValidationTaskType<SimulationItem> SIMULATION = register(new ValidationTaskType<>("SIMULATION", SimulationItem.class));
 	public static final ValidationTaskType<ReplayTrace> REPLAY_TRACE = register(new ValidationTaskType<>("REPLAY_TRACE", ReplayTrace.class));
 	public static final ValidationTaskType<ModelCheckingItem> MODEL_CHECKING = register(new ValidationTaskType<>("MODEL_CHECKING", ModelCheckingItem.class));
-	/**
-	 * Proof Obligations are saved via {@link SavedProofObligationItem},
-	 * and this class does not implement {@link IValidationTask}.
-	 * So this type is not used for (de-)serializing.
-	 */
 	public static final ValidationTaskType<ProofObligationItem> PROOF_OBLIGATION = register(new ValidationTaskType<>("PROOF_OBLIGATION", ProofObligationItem.class));
 	public static final ValidationTaskType<DotFormulaTask> DOT_FORMULA = register(new ValidationTaskType<>("DOT_FORMULA", DotFormulaTask.class));
 	public static final ValidationTaskType<TableFormulaTask> TABLE_FORMULA = register(new ValidationTaskType<>("TABLE_FORMULA", TableFormulaTask.class));
