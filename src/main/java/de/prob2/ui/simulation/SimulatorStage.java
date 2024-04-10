304fcacda1e193839ea0cf03bfdb1fec6becc2fe
Fabian Vu <Fabian.Vu@hhu.de>
2024-04-10 10:02:55 +0200

    Remove unused import in SimulatorStage

--- src/main/java/de/prob2/ui/simulation/SimulatorStage.java
+++ src/main/java/de/prob2/ui/simulation/SimulatorStage.java
@@ -78,7 +78,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
