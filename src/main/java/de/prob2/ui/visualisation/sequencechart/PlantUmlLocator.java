24d24f595ad6dc17b5b78a57b2fd32cfc133f971
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2024-04-11 20:22:31 +0200

    simplify code

--- src/main/java/de/prob2/ui/visualisation/sequencechart/PlantUmlLocator.java
+++ src/main/java/de/prob2/ui/visualisation/sequencechart/PlantUmlLocator.java
@@ -6,32 +6,21 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import de.prob2.ui.internal.ConfigDirectory;
-import de.prob2.ui.internal.StageManager;
-
-import javafx.application.Platform;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 
 @Singleton
 public final class PlantUmlLocator {
 
-	private final StageManager stageManager;
 	private final Path directory;
 	private Path cachedFile;
 
 	@Inject
-	private PlantUmlLocator(StageManager stageManager, @ConfigDirectory Path directory) {
-		this.stageManager = stageManager;
+	private PlantUmlLocator(@ConfigDirectory Path directory) {
 		this.directory = directory;
 	}
 
@@ -45,39 +34,13 @@ public final class PlantUmlLocator {
 		return name.startsWith("plantuml") && name.endsWith(".jar") && Files.isRegularFile(p);
 	}
 
+	public Path getDirectory() {
+		return this.directory;
+	}
+
 	public Optional<Path> findPlantUmlJar() {
 		if (this.cachedFile == null) {
 			this.cachedFile = this.find0();
-			if (this.cachedFile == null) {
-				Lock lock = new ReentrantLock();
-				Condition fileSet = lock.newCondition();
-				AtomicReference<Optional<Path>> localFile = new AtomicReference<>();
-				Platform.runLater(() -> {
-					lock.lock();
-					try {
-						localFile.set(Optional.ofNullable(this.askUser()));
-						fileSet.signal();
-					} finally {
-						lock.unlock();
-					}
-				});
-				while (true) {
-					lock.lock();
-					try {
-						fileSet.await();
-						Optional<Path> path = localFile.get();
-						//noinspection OptionalAssignedToNull
-						if (path != null) {
-							this.cachedFile = path.orElse(null);
-							break;
-						}
-					} catch (InterruptedException ignored) {
-						break;
-					} finally {
-						lock.unlock();
-					}
-				}
-			}
 		}
 
 		return Optional.ofNullable(this.cachedFile);
@@ -107,22 +70,6 @@ public final class PlantUmlLocator {
 		} catch (Exception e) {
 			// TODO: exception handling
 			throw new RuntimeException(e);
-		}
-	}
-
-	private Path askUser() {
-		while (true) {
-			// TODO: localization and add download URL
-			Alert alert = this.stageManager.makeAlert(Alert.AlertType.CONFIRMATION, "missing plantuml", "download pls");
-			ButtonType result = alert.showAndWait().orElse(null);
-			if (result == ButtonType.OK) {
-				Path path = this.find0();
-				if (path != null) {
-					return path;
-				}
-			} else {
-				return null;
-			}
 		}
 	}
 }
--- src/main/java/de/prob2/ui/visualisation/sequencechart/SequenceChartStage.java
+++ src/main/java/de/prob2/ui/visualisation/sequencechart/SequenceChartStage.java
@@ -14,14 +14,15 @@ import de.prob2.ui.internal.executor.BackgroundUpdater;
 import de.prob2.ui.prob2fx.CurrentTrace;
 
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 @Singleton
 public final class SequenceChartStage extends Stage {
 
+	private final StageManager stageManager;
 	private final CurrentTrace trace;
 	private final JavaLocator javaLocator;
 	private final PlantUmlLocator plantUmlLocator;
@@ -33,6 +34,7 @@ public final class SequenceChartStage extends Stage {
 	@Inject
 	private SequenceChartStage(StageManager stageManager, StopActions stopActions, CurrentTrace trace, JavaLocator javaLocator, PlantUmlLocator plantUmlLocator) {
 		super();
+		this.stageManager = stageManager;
 		this.trace = trace;
 		this.javaLocator = javaLocator;
 		this.plantUmlLocator = plantUmlLocator;
@@ -45,37 +47,60 @@ public final class SequenceChartStage extends Stage {
 	@FXML
 	@SuppressWarnings("unused")
 	private void initialize() {
-		ChangeListener<? super Trace> listener = (o, from, to) -> {
-			if (to == null) {
-				this.visualize("""
-					@startuml
-					version
-					@enduml""");
-				return;
-			}
+		this.showingProperty().addListener(o -> this.refresh());
+		this.trace.addListener(o -> this.refresh());
+		this.refresh();
+	}
 
-			this.visualize("@startuml\n" +
-				"Alice -> Bob: " + to.getCurrent().toString() + "\n" +
-				"Bob --> Alice: Authentication Response\n" +
-				"Alice -> Bob: Another authentication Request\n" +
-				"Alice <-- Bob: Another authentication Response\n" +
-				"@enduml");
-		};
-		this.trace.addListener(listener);
-		listener.changed(this.trace, null, this.trace.get());
+	private void refresh() {
+		Trace trace = this.trace.get();
+		if (trace == null || !this.isShowing()) {
+			this.showPlaceholder();
+			return;
+		}
+
+		this.visualize("@startuml\n" +
+				               "Alice -> Bob: " + trace.getCurrent().toString() + "\n" +
+				               "Bob --> Alice: Authentication Response\n" +
+				               "Alice -> Bob: Another authentication Request\n" +
+				               "Alice <-- Bob: Another authentication Response\n" +
+				               "@enduml");
+	}
+
+	private void showPlaceholder() {
+		this.visualize("""
+				@startuml
+				version
+				@enduml""");
 	}
 
 	private void visualize(String uml) {
+		String javaExecutable = this.javaLocator.getJavaExecutable();
+		Optional<Path> optPlantUmlJar = this.plantUmlLocator.findPlantUmlJar();
+		if (optPlantUmlJar.isEmpty()) {
+			Platform.runLater(() -> {
+				this.stageManager.makeAlert(
+						Alert.AlertType.ERROR,
+						"visualisation.sequenceChart.error.noPlantUml.header",
+						"visualisation.sequenceChart.error.noPlantUml.message",
+						"https://plantuml.com/download",
+						this.plantUmlLocator.getDirectory()
+				).show();
+				this.close();
+			});
+			return;
+		}
+		Path plantUmlJar = optPlantUmlJar.get();
+
 		this.updater.cancel(true);
+		if (!this.updater.isRunning()) {
+			return;
+		}
+
 		this.updater.execute(() -> {
 			String output;
 			try {
-				Optional<Path> plantUmlJar = this.plantUmlLocator.findPlantUmlJar();
-				if (plantUmlJar.isEmpty()) {
-					return;
-				}
-
-				PlantUmlCall c = new PlantUmlCall(this.javaLocator.getJavaExecutable(), plantUmlJar.get()).outputFormat(PlantUmlCall.SVG);
+				PlantUmlCall c = new PlantUmlCall(javaExecutable, plantUmlJar).outputFormat(PlantUmlCall.SVG);
 				if (this.trace.getStateSpace() != null) {
 					String dot = this.trace.getStateSpace().getCurrentPreference("DOT");
 					c.dotExecutable(dot);
