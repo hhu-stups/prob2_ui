957e2a4625ec937af1a86ed599d96554b9737562
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2024-05-10 16:34:53 +0200

    Stop editor from loading a file twice and be more aggressive with killing of file watchers

--- src/main/java/de/prob2/ui/beditor/BEditorView.java
+++ src/main/java/de/prob2/ui/beditor/BEditorView.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
@@ -34,7 +33,6 @@ import de.prob2.ui.helpsystem.HelpButton;
 import de.prob2.ui.internal.FXMLInjected;
 import de.prob2.ui.internal.I18n;
 import de.prob2.ui.internal.StageManager;
-import de.prob2.ui.internal.StopActions;
 import de.prob2.ui.internal.TextAreaState;
 import de.prob2.ui.menu.ExternalEditor;
 import de.prob2.ui.menu.MainView;
@@ -113,8 +111,8 @@ public class BEditorView extends BorderPane {
 	private final ObservableList<ErrorItem> errors;
 
 	private Thread watchThread;
-	private WatchKey key;
 	private boolean saving;
+	private boolean updatingIncludedMachines;
 
 	@Inject
 	private BEditorView(final StageManager stageManager, final I18n i18n, final CurrentProject currentProject, final CurrentTrace currentTrace, final Injector injector) {
@@ -128,7 +126,6 @@ public class BEditorView extends BorderPane {
 		this.saved = new SimpleBooleanProperty(this, "saved", true);
 		this.errors = FXCollections.observableArrayList();
 		this.watchThread = null;
-		this.key = null;
 		stageManager.loadFXML(this, "beditorView.fxml");
 	}
 
@@ -177,14 +174,13 @@ public class BEditorView extends BorderPane {
 				this.setHint();
 			} else {
 				// The correct list of included machines is available once the machine is fully loaded.
-				// Until that happens, display only the main machine and the cached selection.
-				machineChoice.getItems().setAll(currentProject.get().getAbsoluteMachinePath(currentProject.getCurrentMachine()));
-				Path selectedMachine = to.getCachedEditorState().getSelectedMachine();
-				if (selectedMachine != null) {
-					selectMachine(selectedMachine);
-				} else {
-					machineChoice.getSelectionModel().selectFirst();
+				// Until that happens, display only the cached last opened or the main machine.
+				Path toShow = to.getCachedEditorState().getSelectedMachine();
+				if (toShow == null) {
+					toShow = currentProject.get().getAbsoluteMachinePath(to);
 				}
+				machineChoice.getItems().setAll(toShow);
+				machineChoice.getSelectionModel().selectFirst();
 			}
 		});
 
@@ -225,7 +221,7 @@ public class BEditorView extends BorderPane {
 		currentProject.addListener((observable, from, to) -> resetWatching());
 
 		machineChoice.getSelectionModel().selectedItemProperty().addListener((observable, from, to) -> {
-			if (to == null) {
+			if (this.updatingIncludedMachines || to == null) {
 				return;
 			}
 			switchMachine(to);
@@ -243,11 +239,14 @@ public class BEditorView extends BorderPane {
 	}
 
 	private void updateSaved() {
-		boolean newSaved = getPath() == null;
-		if (!newSaved) {
+		boolean newSaved;
+		if (this.getPath() != null) {
 			String lastSaved = Objects.requireNonNullElse(this.lastSavedText.get(), "");
 			String editor = Objects.requireNonNullElse(this.beditor.getText(), "");
 			newSaved = lastSaved.equals(editor);
+		} else {
+			// do not show unsaved warning when there is no file open right now
+			newSaved = true;
 		}
 
 		this.saved.set(newSaved);
@@ -324,36 +323,49 @@ public class BEditorView extends BorderPane {
 	}
 
 	private void updateIncludedMachines() {
-		final AbstractModel model = currentTrace.getModel();
-		if (model instanceof ClassicalBModel) {
-			machineChoice.getItems().setAll(((ClassicalBModel) model).getLoadedMachineFiles());
-		} else if (model instanceof RulesModel) {
-			// references of main machine are not enough; referenced can reference other rmch again
-			machineChoice.getItems().setAll(((RulesModel) model).getRulesProject().getBModels().stream()
-				                                .map(IModel::getMachineReferences)
-				                                .flatMap(refs -> refs.stream()
-					                                                 .map(ref -> Paths.get(ref.getFile().getPath())))
-				                                .distinct().toList());
-			// main machine is not included in list of referenced rmch
-			machineChoice.getItems().add(currentProject.get().getAbsoluteMachinePath(currentProject.getCurrentMachine()));
-		} else {
-			// TODO: We could extract the refinement hierarchy via model.getMachines() or model.calculateDependencies on the EventBModel.
-			// Here, we have the problem that the current project might not include all of them refined machines
-			// Still, could we assume that the machine is located in the same folder as the loaded machine? If yes, then it might still be possible to implement this feature
-			machineChoice.getItems().setAll(currentProject.get().getAbsoluteMachinePath(currentProject.getCurrentMachine()));
+		this.updatingIncludedMachines = true;
+		Path prevSelected;
+		try {
+			prevSelected = this.machineChoice.getSelectionModel().getSelectedItem();
+			final AbstractModel model = currentTrace.getModel();
+			if (model instanceof ClassicalBModel) {
+				machineChoice.getItems().setAll(((ClassicalBModel) model).getLoadedMachineFiles());
+			} else if (model instanceof RulesModel) {
+				// references of main machine are not enough; referenced can reference other rmch again
+				machineChoice.getItems().setAll(((RulesModel) model).getRulesProject().getBModels().stream()
+						                                .map(IModel::getMachineReferences)
+						                                .flatMap(refs -> refs.stream()
+								                                                 .map(ref -> Path.of(ref.getFile().getPath())))
+						                                .distinct().toList());
+				// main machine is not included in list of referenced rmch
+				machineChoice.getItems().add(currentProject.get().getAbsoluteMachinePath(currentProject.getCurrentMachine()));
+			} else {
+				// TODO: We could extract the refinement hierarchy via model.getMachines() or model.calculateDependencies on the EventBModel.
+				// Here, we have the problem that the current project might not include all of them refined machines
+				// Still, could we assume that the machine is located in the same folder as the loaded machine? If yes, then it might still be possible to implement this feature
+				machineChoice.getItems().setAll(currentProject.get().getAbsoluteMachinePath(currentProject.getCurrentMachine()));
+			}
+
+			if (prevSelected != null && this.machineChoice.getItems().contains(prevSelected)) {
+				this.selectMachine(prevSelected);
+				return;
+			}
+		} finally {
+			this.updatingIncludedMachines = false;
 		}
 
-		Path selectedMachine = currentProject.getCurrentMachine().getCachedEditorState().getSelectedMachine();
-		if (selectedMachine != null) {
-			selectMachine(selectedMachine);
+		Path cachedSelected = currentProject.getCurrentMachine().getCachedEditorState().getSelectedMachine();
+		if (cachedSelected != null && this.machineChoice.getItems().contains(cachedSelected)) {
+			this.selectMachine(cachedSelected);
 		} else {
-			machineChoice.getSelectionModel().selectFirst();
+			this.machineChoice.getSelectionModel().selectFirst();
 		}
 	}
 
 	private void registerFile(Path path) {
+		this.resetWatching();
 		Path directory = Objects.requireNonNull(path, "path").getParent();
-		if (!Files.isRegularFile(path) || directory == null) {
+		if (directory == null || !Files.isRegularFile(path)) {
 			LOGGER.error("Could not register watch service for invalid file path {} with parent {}", path, directory);
 			return;
 		}
@@ -366,25 +378,34 @@ public class BEditorView extends BorderPane {
 			LOGGER.error("Could not register file: {}", path, e);
 			return;
 		}
-		watchThread = new Thread(() -> {
-			while (true) {
+
+		this.watchThread = new Thread(() -> {
+			while (!Thread.currentThread().isInterrupted()) {
+				WatchKey key;
 				try {
 					key = watcher.take();
 				} catch (InterruptedException ignored) {
-					Thread.currentThread().interrupt();
 					return;
 				}
 				for (WatchEvent<?> event : key.pollEvents()) {
+					if (event.kind() == StandardWatchEventKinds.OVERFLOW) {
+						continue;
+					}
+
 					if (path.getFileName().equals(event.context())) {
 						// Only reload on events for the file itself, not for other files in the directory.
 						Platform.runLater(() -> loadText(path));
+						// if our file changed we do not care about other changes, let's just queue the reload and wait for the next change
+						break;
 					}
 				}
-				key.reset();
+				if (!key.reset()) {
+					return;
+				}
 			}
-		}, "BEditor File Change Watcher");
-		injector.getInstance(StopActions.class).add(watchThread::interrupt);
-		watchThread.start();
+		}, "BEditor File Change Watcher for " + path.getFileName());
+		this.watchThread.setDaemon(true);
+		this.watchThread.start();
 	}
 
 	public ObjectProperty<Path> pathProperty() {
@@ -421,7 +442,7 @@ public class BEditorView extends BorderPane {
 			) {
 				// fact 1: we are loading the same file path that we have open right now
 				// fact 2: the file contents equal the last saved text
-				// conclusion: either nothing changed or the user changed the editor text
+				// conclusion: either nothing changed or the user changed the text in the editor
 				// => do not discard the changes of the user!
 				this.updateSaved();
 				return;
@@ -433,9 +454,9 @@ public class BEditorView extends BorderPane {
 			beditor.requestFollowCaret();
 			lastSavedText.set(beditor.getText());
 			beditor.setEditable(true);
+			beditor.reloadHighlighting();
 		} finally {
 			beditor.setChangingText(false);
-			beditor.reloadHighlighting();
 		}
 	}
 
@@ -493,11 +514,9 @@ public class BEditorView extends BorderPane {
 	}
 
 	private void resetWatching() {
-		if (watchThread != null) {
-			watchThread.interrupt();
-		}
-		if (key != null) {
-			key.reset();
+		if (this.watchThread != null) {
+			this.watchThread.interrupt();
+			this.watchThread = null;
 		}
 	}
 
@@ -518,7 +537,7 @@ public class BEditorView extends BorderPane {
 		}
 
 		try {
-			return Files.isSameFile(Paths.get(filename), currentPath);
+			return Files.isSameFile(Path.of(filename), currentPath);
 		} catch (IOException | InvalidPathException e) {
 			LOGGER.warn("Failed to check if file is identical to editor file", e);
 			return false;
@@ -556,12 +575,12 @@ public class BEditorView extends BorderPane {
 	}
 
 	public void jumpToErrorSource(ErrorItem.Location errorLocation) {
-		this.focusAndShowMachine(Paths.get(errorLocation.getFilename()));
+		this.focusAndShowMachine(Path.of(errorLocation.getFilename()));
 		beditor.jumpToErrorSource(errorLocation);
 	}
 
 	public void jumpToSearchResult(ErrorItem.Location searchResultLocation) {
-		this.focusAndShowMachine(Paths.get(searchResultLocation.getFilename()));
+		this.focusAndShowMachine(Path.of(searchResultLocation.getFilename()));
 		beditor.jumpToSearchResult(searchResultLocation);
 	}
 }
