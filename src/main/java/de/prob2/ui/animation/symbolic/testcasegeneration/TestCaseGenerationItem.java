9aded76a4efa96b320f5449c21ee744f94bf8aa4
dgelessus <dgelessus@users.noreply.github.com>
2024-05-13 18:03:35 +0200

    Inline TestCaseGenerator into execute method and refactor slightly

--- src/main/java/de/prob2/ui/animation/symbolic/testcasegeneration/TestCaseGenerationItem.java
+++ src/main/java/de/prob2/ui/animation/symbolic/testcasegeneration/TestCaseGenerationItem.java
@@ -1,17 +1,23 @@
 package de.prob2.ui.animation.symbolic.testcasegeneration;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import de.prob.analysis.testcasegeneration.ConstraintBasedTestCaseGenerator;
 import de.prob.analysis.testcasegeneration.Target;
 import de.prob.analysis.testcasegeneration.TestCaseGeneratorResult;
 import de.prob.analysis.testcasegeneration.TestCaseGeneratorSettings;
+import de.prob.analysis.testcasegeneration.testtrace.TestTrace;
 import de.prob.statespace.Trace;
 import de.prob2.ui.internal.I18n;
 import de.prob2.ui.verifications.AbstractCheckableItem;
+import de.prob2.ui.verifications.Checked;
+import de.prob2.ui.verifications.CheckingResultItem;
 import de.prob2.ui.verifications.ExecutionContext;
 import de.prob2.ui.vomanager.IValidationTask;
 
@@ -92,7 +98,29 @@ public abstract class TestCaseGenerationItem extends AbstractCheckableItem imple
 
 	@Override
 	public void execute(final ExecutionContext context) {
-		TestCaseGenerator.generateTestCases(this, context.stateSpace());
+		this.getExamples().clear();
+
+		ConstraintBasedTestCaseGenerator cbTestCaseGenerator = new ConstraintBasedTestCaseGenerator(context.stateSpace(), this.getTestCaseGeneratorSettings(), new ArrayList<>());
+		TestCaseGeneratorResult res = cbTestCaseGenerator.generateTestCases();
+		this.setResult(res);
+
+		List<Trace> traces = new ArrayList<>();
+		for (TestTrace trace : res.getTestTraces()) {
+			if (trace.getTrace() != null) {
+				traces.add(trace.getTrace());
+			}
+		}
+
+		if (res.isInterrupted()) {
+			this.setResultItem(new CheckingResultItem(Checked.INTERRUPTED, "animation.resultHandler.testcasegeneration.result.interrupted"));
+		} else if (traces.isEmpty()) {
+			this.setResultItem(new CheckingResultItem(Checked.FAIL, "animation.resultHandler.testcasegeneration.result.notFound"));
+		} else if (!res.getUncoveredTargets().isEmpty()) {
+			this.setResultItem(new CheckingResultItem(Checked.FAIL, "animation.resultHandler.testcasegeneration.result.notAllGenerated"));
+		} else {
+			this.setResultItem(new CheckingResultItem(Checked.SUCCESS, "animation.resultHandler.testcasegeneration.result.found"));
+		}
+		this.getExamples().addAll(traces);
 	}
 
 	@Override
