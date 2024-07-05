b923ddd4dd28f7d0bafc254b78dba05fe45546d8
dgelessus <dgelessus@users.noreply.github.com>
2024-07-05 12:04:51 +0200

    Refactor CliTaskExecutor to make .execute work as expected
    Previously, tasks started using .execute weren't tracked properly and
    couldn't be interrupted by the user.

--- src/main/java/de/prob2/ui/internal/executor/CliTaskExecutor.java
+++ src/main/java/de/prob2/ui/internal/executor/CliTaskExecutor.java
@@ -1,11 +1,16 @@
 package de.prob2.ui.internal.executor;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -16,10 +21,7 @@ import de.prob2.ui.internal.StopActions;
 import javafx.application.Platform;
 import javafx.beans.binding.BooleanExpression;
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SetProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleSetProperty;
-import javafx.collections.FXCollections;
 
 /**
  * <p>
@@ -39,35 +41,35 @@ import javafx.collections.FXCollections;
  * </p>
  */
 @Singleton
-public final class CliTaskExecutor extends CompletableThreadPoolExecutor {
+public final class CliTaskExecutor implements CompletableExecutorService {
 	private final Set<Future<?>> currentTasks;
 	private final BooleanProperty running;
+	private final CompletableExecutorService internalExecutor;
 
 	@Inject
 	private CliTaskExecutor(final DisablePropertyController disablePropertyController, final StopActions stopActions) {
-		super(
-				1, 1, 0, TimeUnit.MILLISECONDS,
-				new LinkedBlockingQueue<>(),
-				r -> new Thread(r, "probcli command executor")
-		);
-
 		this.currentTasks = new CopyOnWriteArraySet<>();
 		this.running = new SimpleBooleanProperty(this, "running", false);
+		this.internalExecutor = new CompletableThreadPoolExecutor(
+			1, 1, 0, TimeUnit.MILLISECONDS,
+			new LinkedBlockingQueue<>(),
+			r -> new Thread(r, "probcli command executor")
+		) {
+			@Override
+			protected <T> CompletableFutureTask<T> newTaskFor(final Callable<T> callable) {
+				CompletableFutureTask<T> task = super.newTaskFor(callable);
+				task.whenComplete((r, t) -> {
+					currentTasks.remove(task);
+					updateRunning();
+				});
+				currentTasks.add(task);
+				updateRunning();
+				return task;
+			}
+		};
 
 		disablePropertyController.addDisableExpression(this.runningProperty());
 		stopActions.add(this::shutdownNow);
-	}
-
-	@Override
-	protected <T> CompletableFutureTask<T> newTaskFor(final Callable<T> callable) {
-		final CompletableFutureTask<T> task = super.newTaskFor(callable);
-		task.whenComplete((r, t) -> {
-			this.currentTasks.remove(task);
-			this.updateRunning();
-		});
-		this.currentTasks.add(task);
-		this.updateRunning();
-		return task;
 	}
 
 	private void updateRunning() {
@@ -99,5 +101,79 @@ public final class CliTaskExecutor extends CompletableThreadPoolExecutor {
 	 */
 	public void interruptAll() {
 		this.currentTasks.forEach(task -> task.cancel(true));
+	}
+
+	@Override
+	public void execute(Runnable command) {
+		// Use submit instead of execute so that the Runnable is wrapped in a task.
+		// Otherwise it won't be tracked in currentTasks and the running property won't be updated,
+		// making it impossible for the user to interrupt the Runnable.
+		this.submit(command).exceptionally(exc -> {
+			Thread thread = Thread.currentThread();
+			thread.getUncaughtExceptionHandler().uncaughtException(thread, exc);
+			return null;
+		});
+	}
+
+	// All other ExecutorService methods just delegate to this.internalExecutor.
+
+	@Override
+	public <T> CompletableFuture<T> submit(Callable<T> task) {
+		return this.internalExecutor.submit(task);
+	}
+
+	@Override
+	public <T> CompletableFuture<T> submit(Runnable task, T result) {
+		return this.internalExecutor.submit(task, result);
+	}
+
+	@Override
+	public CompletableFuture<?> submit(Runnable task) {
+		return this.internalExecutor.submit(task);
+	}
+
+	@Override
+	public void shutdown() {
+		this.internalExecutor.shutdown();
+	}
+
+	@Override
+	public List<Runnable> shutdownNow() {
+		return this.internalExecutor.shutdownNow();
+	}
+
+	@Override
+	public boolean isShutdown() {
+		return this.internalExecutor.isShutdown();
+	}
+
+	@Override
+	public boolean isTerminated() {
+		return this.internalExecutor.isTerminated();
+	}
+
+	@Override
+	public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+		return this.internalExecutor.awaitTermination(timeout, unit);
+	}
+
+	@Override
+	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
+		return this.internalExecutor.invokeAll(tasks);
+	}
+
+	@Override
+	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
+		return this.internalExecutor.invokeAll(tasks, timeout, unit);
+	}
+
+	@Override
+	public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
+		return this.internalExecutor.invokeAny(tasks);
+	}
+
+	@Override
+	public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+		return this.internalExecutor.invokeAny(tasks, timeout, unit);
 	}
 }
