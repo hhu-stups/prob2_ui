02ebf98a1de053b15ac3cdd05ec029a3a6454405
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2025-01-15 17:36:00 +0100

    disable editing of simulation item while real-time simulation is running
    closes https://github.com/hhu-stups/prob-issues/issues/365

--- src/main/java/de/prob2/ui/simulation/SimulatorStage.java
+++ src/main/java/de/prob2/ui/simulation/SimulatorStage.java
@@ -475,6 +475,7 @@ public final class SimulatorStage extends Stage {
 			this.loadSimulationIntoSimulator(to);
 			uiInteractionHandler.loadUIListenersIntoSimulator(realTimeSimulator);
 		});
+		cbSimulation.disableProperty().bind(currentTrace.isNull().or(realTimeSimulator.runningProperty()).or(currentProject.currentMachineProperty().isNull()));
 
 		btAddSimulation.disableProperty().bind(currentTrace.isNull().or(disablePropertyController.disableProperty()).or(configurationPath.isNull()).or(realTimeSimulator.runningProperty()).or(currentProject.currentMachineProperty().isNull()));
 
@@ -497,7 +498,7 @@ public final class SimulatorStage extends Stage {
 		saveAsMenuItem.disableProperty().bind(disableSaveProperty);
 		saveAsItem.disableProperty().bind(disableSaveProperty);
 
-		this.simulationDiagramItems.setCellFactory(lv -> new SimulationListViewDiagramItem(stageManager, i18n, savedProperty));
+		this.simulationDiagramItems.setCellFactory(lv -> new SimulationListViewDiagramItem(stageManager, i18n, savedProperty, realTimeSimulator.runningProperty()));
 
 		machineLoader.loadingProperty().addListener((observable, from, to) -> {
 			if (to) {
@@ -526,8 +527,6 @@ public final class SimulatorStage extends Stage {
 
 
 		simulationItems.setRowFactory(table -> new SimulationItemRow(this));
-
-		this.currentTrace.addListener((observable, from, to) -> simulationDiagramItems.refresh());
 
 		simulationItems.setOnMouseClicked(e -> {
 			SimulationItem item = simulationItems.getSelectionModel().getSelectedItem();
@@ -675,7 +674,6 @@ public final class SimulatorStage extends Stage {
 
 		simulationDiagramItems.getItems().clear();
 		simulationDiagramItems.setItems(observableList);
-		simulationDiagramItems.refresh();
 	}
 
 	@FXML
@@ -686,7 +684,6 @@ public final class SimulatorStage extends Stage {
 		SimulationItem newItem = choosingStage.getResult();
 		if (newItem != null) {
 			SimulationItem toCheck = currentProject.getCurrentMachine().addValidationTaskIfNotExist(newItem);
-			simulationItems.refresh();
 			simulationItemHandler.checkItem(toCheck);
 		}
 	}
