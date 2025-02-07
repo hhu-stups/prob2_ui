de8a069522d8649d5947386a230e61ee754c8f89
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2025-02-08 00:16:52 +0100

    cleanup ConfigurationCheckingError

--- src/main/java/de/prob2/ui/simulation/configuration/ConfigurationCheckingError.java
+++ src/main/java/de/prob2/ui/simulation/configuration/ConfigurationCheckingError.java
@@ -1,13 +1,8 @@
 package de.prob2.ui.simulation.configuration;
 
-import java.io.Serial;
-
 public class ConfigurationCheckingError extends Exception {
-	@Serial
-	private static final long serialVersionUID = 1L;
 
-	public ConfigurationCheckingError(String msg) {
-		super(msg);
+	public ConfigurationCheckingError(String message) {
+		super(message);
 	}
-
 }
