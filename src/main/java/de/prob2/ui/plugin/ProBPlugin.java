f19efec3a1307b946f8cbc2f2105f2fb94eeded8
Miles Vella <673-vella@users.noreply.gitlab.cs.uni-duesseldorf.de>
2025-03-20 14:15:04 +0100

    Breaking change: use custom plugin context object instead of deprecated plugin wrapper

--- src/main/java/de/prob2/ui/plugin/ProBPlugin.java
+++ src/main/java/de/prob2/ui/plugin/ProBPlugin.java
@@ -1,10 +1,11 @@
 package de.prob2.ui.plugin;
 
+import java.util.Objects;
+
 import com.google.inject.Injector;
 
 import org.pf4j.Plugin;
 import org.pf4j.PluginManager;
-import org.pf4j.PluginWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,14 +28,12 @@ public abstract class ProBPlugin extends Plugin {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ProBPlugin.class);
 
+	private final PluginContext context;
 	private boolean started = false;
-	private final ProBPluginManager proBPluginManager;
-	private final ProBPluginHelper proBPluginHelper;
 
-	public ProBPlugin (PluginWrapper pluginWrapper, ProBPluginManager proBPluginManager, ProBPluginHelper proBPluginHelper) {
-		super(pluginWrapper);
-		this.proBPluginManager = proBPluginManager;
-		this.proBPluginHelper = proBPluginHelper;
+	public ProBPlugin(PluginContext context) {
+		super();
+		this.context = Objects.requireNonNull(context, "context");
 	}
 
 	/**
@@ -49,25 +48,33 @@ public abstract class ProBPlugin extends Plugin {
 	 * when the plug-in has not yet been started. If an exception gets thrown during the method the
 	 * {@code stopPlugin} method will be called.
 	 */
-	public abstract void startPlugin() throws Exception;
+	protected abstract void startPlugin() throws Exception;
 
 	/**
 	 * Stops the plug-in and ensures that the {@code stop} method of the plug-in is only called
 	 * when the plug-in has not yet been stopped. The stop method is also called when the {@code startPlugin}
 	 * method throws an exception so that this method should be prepared for that.
 	 */
-	public abstract void stopPlugin() throws Exception;
+	protected abstract void stopPlugin() throws Exception;
+
+	/**
+	 * Notifies the plug-in of its deletion.
+	 */
+	protected void deletePlugin() throws Exception {
+		// NO-OP
+	}
 
 	@Override
 	public final void start() {
 		if (!started) {
 			try {
 				startPlugin();
-				started = true;
 			} catch (Exception ex) {
 				LOGGER.warn("Exception while starting the plug-in {}", getName(), ex);
 				getProBPluginHelper().getStageManager()
 						.makeExceptionAlert(ex, "plugin.alerts.couldNotStartPlugin.content", getName()).show();
+			} finally {
+				started = true;
 			}
 		}
 	}
@@ -77,13 +84,37 @@ public abstract class ProBPlugin extends Plugin {
 		if (started) {
 			try {
 				stopPlugin();
-				started = false;
 			} catch (Exception ex) {
 				LOGGER.warn("Exception while stopping the plug-in {}", getName(), ex);
 				getProBPluginHelper().getStageManager()
 						.makeExceptionAlert(ex, "plugin.alerts.couldNotStopPlugin.content", getName()).show();
+			} finally {
+				started = false;
 			}
 		}
+	}
+
+	@Override
+	public final void delete() {
+		this.stop();
+		try {
+			deletePlugin();
+		} catch (Exception ex) {
+			LOGGER.warn("Exception while deleting the plug-in {}", getName(), ex);
+			getProBPluginHelper().getStageManager()
+					.makeExceptionAlert(ex, "plugin.alerts.couldNotDeletePlugin.content", getName()).show();
+		} finally {
+			started = false;
+		}
+	}
+
+	/**
+	 * Getter for this plugin's context.
+	 *
+	 * @return this plugin's context
+	 */
+	public PluginContext getContext() {
+		return this.context;
 	}
 
 	/**
@@ -94,7 +125,7 @@ public abstract class ProBPlugin extends Plugin {
 	 * @return singleton instance of the {@link ProBPluginHelper}
 	 */
 	public ProBPluginHelper getProBPluginHelper() {
-		return proBPluginHelper;
+		return this.getProBPluginManager().getProBPluginHelper();
 	}
 
 	/**
@@ -104,7 +135,7 @@ public abstract class ProBPlugin extends Plugin {
 	 * @return singleton instance of the {@link ProBPluginManager}
 	 */
 	public ProBPluginManager getProBPluginManager() {
-		return proBPluginManager;
+		return this.getContext().proBPluginManager();
 	}
 
 	/**
@@ -113,7 +144,7 @@ public abstract class ProBPlugin extends Plugin {
 	 * @return Returns the {@link ProBPluginManager} used to load this plug-in.
 	 */
 	public ProBPluginManager.ProBJarPluginManager getPluginManager() {
-		PluginManager pluginManager = getWrapper().getPluginManager();
+		PluginManager pluginManager = this.getContext().pluginWrapper().getPluginManager();
 		if (pluginManager instanceof ProBPluginManager.ProBJarPluginManager) {
 			return (ProBPluginManager.ProBJarPluginManager) pluginManager;
 		}
@@ -128,6 +159,6 @@ public abstract class ProBPlugin extends Plugin {
 	 * @return singleton instance of the {@link Injector}
 	 */
 	public Injector getInjector() {
-		return getProBPluginHelper().getInjector();
+		return this.getProBPluginHelper().getInjector();
 	}
 }
