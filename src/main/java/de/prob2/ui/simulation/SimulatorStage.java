f505c244ea2fc01e7bdfe0cb57670e90590c0e71
Fabian Vu <Fabian.Vu@hhu.de>
2025-04-25 09:57:12 +0200

    Fix a bug which avoids to add new diagram items

--- src/main/java/de/prob2/ui/simulation/SimulatorStage.java
+++ src/main/java/de/prob2/ui/simulation/SimulatorStage.java
@@ -524,7 +524,7 @@ public final class SimulatorStage extends Stage {
 		btRemoveSimulation.disableProperty().bind(cbSimulation.getSelectionModel().selectedItemProperty().isNull());
 		btAddDiagramElement.disableProperty().bind(Bindings.createBooleanBinding(() ->
 				cbSimulation.getSelectionModel().selectedItemProperty().get() == null || configurationPath.get() == null || !configurationPath.get().toString().endsWith(".json"),
-				cbSimulation.getSelectionModel().selectedItemProperty()));
+				cbSimulation.getSelectionModel().selectedItemProperty(), configurationPath));
 
 		btRemoveDiagramElement.disableProperty().bind(simulationDiagramItems.getSelectionModel().selectedItemProperty().isNull());
 
