f9a4157dc53fd0f46627e837d59ebbfb668326f4
Michael Leuschel <leuschel@uni-duesseldorf.de>
2025-10-02 17:35:41 +0200

    operations view menu item for deterministic execution
    ideally this item should be grayed out if the current state
    is not deterministic
    
    Signed-off-by: Michael Leuschel <leuschel@uni-duesseldorf.de>

--- src/main/java/de/prob2/ui/operations/OperationsView.java
+++ src/main/java/de/prob2/ui/operations/OperationsView.java
@@ -202,6 +202,8 @@ public final class OperationsView extends BorderPane {
 	@FXML
 	private MenuItem tenRandomEvents;
 	@FXML
+	private MenuItem thousandDetEvents;
+	@FXML
 	private CustomMenuItem someRandomEvents;
 	@FXML
 	private HelpButton helpButton;
@@ -567,6 +569,7 @@ public final class OperationsView extends BorderPane {
 	@FXML
 	public void random(ActionEvent event) {
 		final int operationCount;
+		final boolean stopIfNonDet;
 		if (event.getSource().equals(randomText)) {
 			final String randomInput = randomText.getText();
 			if (randomInput.isEmpty()) {
@@ -583,19 +586,27 @@ public final class OperationsView extends BorderPane {
 				alert.showAndWait();
 				return;
 			}
+			stopIfNonDet=false;
 		} else if (event.getSource().equals(oneRandomEvent)) {
 			operationCount = 1;
+			stopIfNonDet=false;
 		} else if (event.getSource().equals(fiveRandomEvents)) {
 			operationCount = 5;
+			stopIfNonDet=false;
 		} else if (event.getSource().equals(tenRandomEvents)) {
 			operationCount = 10;
+			stopIfNonDet=false;
+		} else if (event.getSource().equals(thousandDetEvents)) {
+			operationCount = 1000;
+			stopIfNonDet=true;
 		} else {
 			throw new AssertionError("Unhandled random animation event source: " + event.getSource());
 		}
 
 		Trace currentTrace = this.currentTrace.get();
 		if (currentTrace != null) {
-			this.cliExecutor.submit(() -> currentTrace.randomAnimation(operationCount)).whenCompleteAsync((res, exc) -> {
+			this.cliExecutor.submit(() -> 
+			       currentTrace.randomAnimation(operationCount,stopIfNonDet)).whenCompleteAsync((res, exc) -> {
 				if (exc != null) {
 					if (!(exc instanceof CancellationException)) {
 						LOGGER.error("error while randomly animating", exc);
