4418598f8ceec8a6a92fba1f359c04854cec9670
favu100 <Fabian.Vu@hhu.de>
2025-12-10 13:11:47 +0100

    Implement sending values from ProB to external simulation

--- src/main/java/de/prob2/ui/simulation/external/ExternalSimulationRequest.java
+++ src/main/java/de/prob2/ui/simulation/external/ExternalSimulationRequest.java
@@ -2,9 +2,12 @@ package de.prob2.ui.simulation.external;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.util.Map;
+
 @JsonPropertyOrder({
 		"finished",
 		"enabledOperations",
+		"externalValues"
 })
-public record ExternalSimulationRequest(int finished, String enabledOperations) {
+public record ExternalSimulationRequest(int finished, String enabledOperations, Map<String, String> externalValues) {
 }
--- src/main/java/de/prob2/ui/simulation/external/ExternalSimulationStep.java
+++ src/main/java/de/prob2/ui/simulation/external/ExternalSimulationStep.java
@@ -1,5 +1,6 @@
 package de.prob2.ui.simulation.external;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.StringJoiner;
 
@@ -12,7 +13,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 		"op",
 		"predicate",
 		"delta",
-		"done"
+		"done",
+		"externalFormulas"
 })
 public final class ExternalSimulationStep {
 
@@ -20,18 +22,21 @@ public final class ExternalSimulationStep {
 	private String predicate;
 	private String delta;
 	private boolean done;
+	private Map<String, String> externalFormulas;
 
 	@JsonCreator
 	public ExternalSimulationStep(
 			@JsonProperty(value = "op", required = true) String op,
 			@JsonProperty(value = "predicate", required = true) String predicate,
 			@JsonProperty(value = "delta", required = true) String delta,
-			@JsonProperty(value = "done", required = true) boolean done
+			@JsonProperty(value = "done", required = true) boolean done,
+			@JsonProperty(value = "externalFormulas", required = false) Map<String, String> externalFormulas
 	) {
 		this.op = op;
 		this.predicate = predicate;
 		this.delta = delta;
 		this.done = done;
+		this.externalFormulas = externalFormulas;
 	}
 
 	@JsonGetter("op")
@@ -70,17 +75,27 @@ public final class ExternalSimulationStep {
 		this.done = done;
 	}
 
+	@JsonGetter("externalFormulas")
+	public Map<String, String> getExternalFormulas() {
+		return externalFormulas;
+	}
+
+	public void setExternalFormulas(Map<String, String> externalFormulas) {
+		this.externalFormulas = externalFormulas;
+	}
+
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		ExternalSimulationStep that = (ExternalSimulationStep) o;
-		return done == that.done && Objects.equals(op, that.op) && Objects.equals(predicate, that.predicate) && Objects.equals(delta, that.delta);
+		return done == that.done && Objects.equals(op, that.op) && Objects.equals(predicate, that.predicate)
+				&& Objects.equals(delta, that.delta) && Objects.equals(externalFormulas, that.externalFormulas);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(op, predicate, delta, done);
+		return Objects.hash(op, predicate, delta, done, externalFormulas);
 	}
 
 	@Override
@@ -89,7 +104,8 @@ public final class ExternalSimulationStep {
 				.add("op='" + op + "'")
 				.add("predicate='" + predicate + "'")
 				.add("delta='" + delta + "'")
-				.add("done=" + done)
+				.add("done='" + done + "'")
+				.add("externalFormulas=" + externalFormulas)
 				.toString();
 	}
 }
